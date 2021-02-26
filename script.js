4935fd07e7e7605158ffb97fa38ba0423a8a16c2
mmolliss <malin.vannesjo@gmail.com>
2021-02-26 12:54:46 +0100

    basic code for text animations

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // DOM SElECTORS
 const sideNav = document.getElementById("sidenav");
+const textWrapper = document.querySelector('.typewriter');
 
 // FUNCTION DECLARATIONS
 
@@ -14,5 +15,46 @@ const hamburgerToggle = (hamburgerIcon) => {
     }
 }
 
+// Text animations
+
+
+
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+.add({
+    targets: '.typewriter .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+}).add({
+    targets: '.typewriter',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.name',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.name',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
 // EVENT LISTENERS
 
+
+// INVOKES
