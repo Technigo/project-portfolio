0ec5c814d8be14da574ee2c78a9ab5c44eaad310
Mir Albertioni <mir.al@live.se>
2021-03-01 15:02:48 +0100

    added background pic, profil pic and intro text

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// My DOMs
+const introSection = document.getElementById("intro-section-text");
+
+
+introSection.innerHTML = `<div>Hello! I’m a strategic and creative senior art director/digital designer with frontend developer skills and experience design at <3. I’m all about solving problems that matter and creating awesome, meaningful products for people to love.</div>`
