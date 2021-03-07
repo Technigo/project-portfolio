bd48a21ba7a3a6780934c253afe6d6884f1b7cc4
sirihemsley <siri.hemsley@gmail.com>
2021-03-07 11:03:06 +0100

    Completed information for each projects with links and added tags for each project

--- script.js
+++ script.js
@@ -8,47 +8,59 @@ const PROJECTS = [
     description: 'The assignment was to create a digital version of the 90s game Guess Who. I created a version with cute baby animals, beause - why not?',
     livelink: 'https://competent-newton-827cc6.netlify.app/',
     gitlink: 'https://github.com/sirimatsdotter/project-guess-who/',
+    tag: ['JAVASCRIPT','HTML','CSS', '']
   },
   {
     title: 'BUSINESS SITE',
     img: './assets/businesssite.png',
     alt: 'Business site',
-    description: 'Description description, description. Description!',
-    livelink: '#',
-    gitlink: '#',
+    description: 'In this project we learnt how to do accordions and forms on a business site, using JavaScript for the first time!',
+    livelink: 'https://gracious-jennings-2cf5eb.netlify.app/',
+    gitlink: 'https://github.com/sirimatsdotter/project-business-site',
+    tag: ['JAVASCRIPT','HTML','CSS', '']
   },
   {
     title: 'NEWS SITE',
     img: './assets/newssite.png',
     alt: 'Colourful news site',
-    description: 'Description description, description. Description!',
-    livelink: '#',
-    gitlink: '#',
+    description: 'This was one of the first projects were the focus was on making a responsive fictional news site.',
+    livelink: 'https://musing-poincare-e4efd4.netlify.app/',
+    gitlink: 'https://github.com/sirimatsdotter/project-news-site',
+    tag: ['HTML','CSS', '', '']
   },
   {
     title: 'CHAT BOT',
     img: './assets/chatbot.png',
     alt: 'Chat window with purple background',
-    description: 'Description description, description. Description!',
-    livelink: '#',
-    gitlink: '#',
+    description: 'A really fun project were me and Jennie created a chat bot to help you pick an outfit for your night out!',
+    livelink: 'https://sleepy-nobel-972874.netlify.app/',
+    gitlink: 'https://github.com/sirimatsdotter/project-chatbot',
+    tag: ['JAVASCRIPT','HTML','CSS', '']
   },
   {
     title: 'WEATHER APP',
     img: './assets/weatherapp.png',
     alt: 'Weather app with text',
-    description: 'Description description, description. Description!',
-    livelink: '#',
-    gitlink: '#',
+    description: 'In this project we were introduced to API´s and created a weather app using the Open Weather API.',
+    livelink: 'https://loving-feynman-2c374e.netlify.app/',
+    gitlink: 'https://github.com/sirimatsdotter/project-weather-app',
+    tag: ['JAVASCRIPT','APIs','HTML','CSS']
   },
 ]
 
-
+const generateProjects = () => {
+projectWrapper.innerText = projectWrapper.innerText.replace('undefined', '')
 PROJECTS.forEach((project) => {
   projectWrapper.innerHTML += `
   <div class="project-item">
     <h3 class="project-header">${project.title}</h3>
     <img class="project-img" src="${project.img}" alt="${project.alt}">
+    <div class="tags">
+      <p class="tag">${project.tag[0]}<p>
+      <p class="tag">${project.tag[1]}<p>
+      <p class="tag">${project.tag[2]}<p>
+      <p class="tag">${project.tag[3]}<p>
+    </div>
     <div class="project-description">
       <p>${project.description}</p>
       <span class="project-links">
@@ -57,6 +69,9 @@ PROJECTS.forEach((project) => {
     </div>
   </div>
   `
-})
+  })
+}
+generateProjects()
+
 
 
