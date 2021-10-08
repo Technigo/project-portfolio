5310c0d968577de671356d6e28ab46c7ac371481
Lovisa Carling <lovisa.carling@gmail.com>
2021-10-08 18:50:58 +0200

    added a button to toggle colors and display picture

--- code/script.js
+++ code/script.js
@@ -1,6 +1,11 @@
-const tagContainer = document.getElementsByClassName('filled-title')
+function myFunction() {
+    let element = document.body;
+    let lightbulb = document.getElementById('showbulb')
+    element.classList.toggle("color-toggle");
+    lightbulb.style.display = 'initial' 
+};
+ 
 
-function changeTagColor() {
-    if(tagContainer.value === 'For more' )
-    tagContainer.style.color = "red";
-}
+ 
+
+ 
