1e654caa9d41c5dd2ac835ccedbedca8824b6c70
Lovisa Carling <lovisa.carling@gmail.com>
2021-10-09 15:13:04 +0200

    Implementing darkmode

--- code/script.js
+++ code/script.js
@@ -1,8 +1,15 @@
-function myFunction() {
-    let element = document.body;
-    let lightbulb = document.getElementById('showbulb')
-    element.classList.toggle("color-toggle");
-    lightbulb.style.display = 'initial' 
+let bodyColor = document.body
+let headerImg = document.querySelector('header')
+let footerColor = document.querySelector('footer')
+let titleColor  = document.getElementById('headertext')
+
+
+
+function changeColors() { 
+    bodyColor.classList.toggle('color-toggle')
+    headerImg.classList.toggle('header-toggle')
+    footerColor.classList.toggle('footer-toggle')
+    titleColor.classList.toggle('title-toggle')
 };
  
 
