886f063ca555e58968525c9a1c6821ebc97d8902
Lovisa Carling <lovisa.carling@gmail.com>
2021-10-09 16:06:57 +0200

    darkmode finished

--- code/script.js
+++ code/script.js
@@ -2,6 +2,11 @@ let bodyColor = document.body
 let headerImg = document.querySelector('header')
 let footerColor = document.querySelector('footer')
 let titleColor  = document.getElementById('headertext')
+let gitIcon = document.getElementById('git-icon')
+let LinkedInIcon = document.getElementById('linkedin-icon')
+let StackOverflowIcon = document.getElementById('stack-icon')
+
+
 
 
 
@@ -10,6 +15,9 @@ function changeColors() {
     headerImg.classList.toggle('header-toggle')
     footerColor.classList.toggle('footer-toggle')
     titleColor.classList.toggle('title-toggle')
+    gitIcon.classList.toggle('icon-toggle')
+    LinkedInIcon.classList.toggle('icon-toggle')
+    StackOverflowIcon.classList.toggle('icon-toggle')
 };
  
 
