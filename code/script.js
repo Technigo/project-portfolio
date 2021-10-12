e99a0b105886fd13224fcfe446f0aed0cd7f7735
MarySnopok <marysnopok8@gmail.com>
2021-10-12 15:28:55 +0200

    animation and refactoring

--- code/script.js
+++ code/script.js
@@ -1,5 +1,7 @@
 const themeToggle = document.getElementById("theme-toggle");
+const containerWithDots = document.getElementById("dots");
 
 themeToggle.onchange = () => {
   document.body.classList.toggle("another-color-theme");
+  containerWithDots.classList.toggle("glowing-shadow");
 };
