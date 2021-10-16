d5a522ccfcb70ee341b11daf2e28fc93a13fc96f
Amanda Tilly <amandatilly@Amandas-2.lan>
2021-10-16 22:34:43 +0200

    styling colors, added read more button and content - intro text and project images

---
+++ code/script.js
@@ -0,0 +1,15 @@
+const dots = document.getElementById("dots");
+const moreText = document.getElementById("more");
+const btnText = document.getElementById("myBtn");
+
+const readMore = () => {
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+};
