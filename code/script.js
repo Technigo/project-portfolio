e9f0472c53ce0f44e2c17331cb000c53e9d76998
MarySnopok <marysnopok8@gmail.com>
2021-11-04 16:44:22 +0100

    adding article

--- code/script.js
+++ code/script.js
@@ -1,8 +1,10 @@
 const themeToggle = document.getElementById("theme-toggle");
 const containerWithDots = document.getElementById("dots");
+const mediumWidget = document.getElementById("medium-widget");
 
 themeToggle.onchange = () => {
   document.body.classList.toggle("another-color-theme");
   containerWithDots.classList.toggle("glowing-shadow");
   containerWithDots.classList.toggle("default-shadow");
+  mediumWidget.classList.toggle("glowing-shadow");
 };
