348b06b4e2785200b6bb4a8b6d0fc6903fab3158
Fatima-GR <fatigr2692@gmail.com>
2022-04-24 18:30:50 +0200

    added text shadow to the subtitle when the day mode is on to make it more visable

--- code/script.js
+++ code/script.js
@@ -1,9 +1,11 @@
 
 // DOM SELECTORS
-let checkbox = document.getElementById("checkbox"); //for adding to the input an event listener
-let headerText = document.querySelector(".header-text-container"); //for changing the color of all texts inside the header
-let header = document.querySelector(".header"); //for changing the background image of the header, from day to night
-let profileImage = document.querySelector(".profile-image"); //for changing the picture & border color of the profile img
+let checkbox = document.getElementById("checkbox"); //for adding to the input an event listener.
+let headerText = document.querySelector(".header-text-container"); //for changing the color of all texts inside the header.
+let header = document.querySelector(".header"); //for changing the background image of the header, from day to night.
+let profileImage = document.querySelector(".profile-image"); //for changing the picture & border color of the profile img.
+let subtitle = document.querySelector(".subtitle"); //for adding text shadow to the subtitle and this way, making it more visable.
+
 
 // EVENT LISTENER FOR THE INPUT: To change the theme of the portfolio
 checkbox.addEventListener("change", () => {
@@ -11,4 +13,5 @@ checkbox.addEventListener("change", () => {
   header.classList.toggle("dayBackground");
   headerText.classList.toggle("darkText");
   profileImage.classList.toggle("profile-image-day");
+  subtitle.classList.toggle("subtitle-day");
 });
