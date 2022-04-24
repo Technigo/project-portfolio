28f6dc0a264f095088f0877bd58131aeccba58fc
Fatima-GR <fatigr2692@gmail.com>
2022-04-24 11:35:20 +0200

    added javascript and css for making input mode button

--- code/script.js
+++ code/script.js
@@ -1,24 +1,47 @@
-
 // DOM SELECTOR
+// let btn = document.getElementById("btn-background");
 
-let btn = document.getElementById('btn-background');
-// let header = document.getElementById("myHeader");
-let header = document.querySelector('.header');
-let headerText = document.querySelector('.header-text-container'); //for changing the color of all text inside the header
-let profileImage = document.querySelector('.profile-image'); //for changing the border color of the profile img
-let profileSection = document.getElementById('profile-section')// for changin the profile section color
-let headingOne= document.getElementById('big-heading')//for adding text shadow to the heading one
-
+// let header = document.querySelector(".header");
+ //for changing the background image of the header and adding a bottom border
+let headerText = document.querySelector(".header-text-container"); //for changing the color of all text inside the header
+let profileImage = document.querySelector(".profile-image"); //for changing the border color of the profile img
+let headingOne = document.getElementById("big-heading"); //for adding text shadow to the heading one text
 
 
-//Event listeners
 
-btn.addEventListener('click', () => {
-    header.style.backgroundImage = "url('images/switzerland-mountains-daytime.webp')";
-    headerText.style.color = " #00004d";
-    profileImage.style.borderColor = "#00004d";
-    profileSection.style.background = "#D6E5FA";
-    headingOne.style.textShadow = "2px 2px #fff"
-
+let checkbox = document.getElementById('checkbox');
+checkbox.addEventListener('change', () => {
+    document.header.classList.toggle('dark')
 });
-//CHANGE FOOTER COLOR AS WELL
+
+
+
+// function changeMode() {
+//     //change the theme of the portfolio
+//     document.body.classList.toggle('dark')
+// }
+
+
+
+//Event listener
+// btn.addEventListener("click", changeMode);
+
+//Function
+// function changeMode(event) {
+//   event.preventDefault();
+
+//   header.style.backgroundImage = "url('images/switzerland-mountains-daytime.webp')";
+//   header.style.borderBottom = "1px solid #00004d";
+//   headerText.style.color = "#00004d";
+//   profileImage.style.borderColor = "#00004d";
+//   profileImage.style.backgroundImage = "url('images/profile-picture-2.jpg')";
+//   headingOne.style.textShadow = "2px 2px #fff";
+//   btn.textContent = "dark mode";
+// }
+
+
+
+// btn.onclick = show;  
+// function show() {
+// // this.classList.toggle('another');
+// }
