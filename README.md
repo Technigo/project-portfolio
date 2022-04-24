7e510b6db697cc7dfe55d299ffdbdb070ffd2bdd
Fatima-GR <fatigr2692@gmail.com>
2022-04-24 18:11:32 +0200

    updated readme file, finished input button for changing the background mode, changed background picture and added more styling

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # PORTFOLIO
 
-This week's project was to create a portfolio based on a given design from FIGMA. The website is built on HTML and CSS and the focus has been on web accessibility.
+This week's project was to create a portfolio based on a given design from FIGMA. The website is built on HTML, CSS and the focus has been on web accessibility.
+
+I have also added vanilla JavaScript manipulating the DOM and using the toggle method to make possible for the user to chose the mode of the header, between day mode or night mode.
 
 # Tech
 * HTML5
 * CSS3
-* Accessibility & accessibility testing
-* Responsive design
+* Vanilla JavaScript
+
+# Accessibility Tools
+* W3C Validator
+* WAVE Validator
+
+# Responsiveness Tools
+* Chrome dev tools
+* Responsinator
+
 
 ## View it live
-Here's the link for my portfolio: https://fatima-gamero-romero-portfolio.netlify.app
+Here's the link for viewing my portfolio: https://fatima-gamero-romero-portfolio.netlify.app
--- code/script.js
+++ code/script.js
@@ -1,47 +1,14 @@
-// DOM SELECTOR
-// let btn = document.getElementById("btn-background");
 
-// let header = document.querySelector(".header");
- //for changing the background image of the header and adding a bottom border
-let headerText = document.querySelector(".header-text-container"); //for changing the color of all text inside the header
-let profileImage = document.querySelector(".profile-image"); //for changing the border color of the profile img
-let headingOne = document.getElementById("big-heading"); //for adding text shadow to the heading one text
+// DOM SELECTORS
+let checkbox = document.getElementById("checkbox"); //for adding to the input an event listener
+let headerText = document.querySelector(".header-text-container"); //for changing the color of all texts inside the header
+let header = document.querySelector(".header"); //for changing the background image of the header, from day to night
+let profileImage = document.querySelector(".profile-image"); //for changing the picture & border color of the profile img
 
-
-
-let checkbox = document.getElementById('checkbox');
-checkbox.addEventListener('change', () => {
-    document.header.classList.toggle('dark')
+// EVENT LISTENER FOR THE INPUT: To change the theme of the portfolio
+checkbox.addEventListener("change", () => {
+    
+  header.classList.toggle("dayBackground");
+  headerText.classList.toggle("darkText");
+  profileImage.classList.toggle("profile-image-day");
 });
-
-
-
-// function changeMode() {
-//     //change the theme of the portfolio
-//     document.body.classList.toggle('dark')
-// }
-
-
-
-//Event listener
-// btn.addEventListener("click", changeMode);
-
-//Function
-// function changeMode(event) {
-//   event.preventDefault();
-
-//   header.style.backgroundImage = "url('images/switzerland-mountains-daytime.webp')";
-//   header.style.borderBottom = "1px solid #00004d";
-//   headerText.style.color = "#00004d";
-//   profileImage.style.borderColor = "#00004d";
-//   profileImage.style.backgroundImage = "url('images/profile-picture-2.jpg')";
-//   headingOne.style.textShadow = "2px 2px #fff";
-//   btn.textContent = "dark mode";
-// }
-
-
-
-// btn.onclick = show;  
-// function show() {
-// // this.classList.toggle('another');
-// }
