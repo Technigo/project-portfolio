02edbe874310e0db8e2be37c07f399c712a2afea
Linda Malm <Linda@Lindas-MacBook-Pro.local>
2022-09-14 17:22:45 +0200

    Started my portfolio by adding header, personal intro, tech-spec, projects and contact-links.

---
+++ script.js
@@ -0,0 +1,11 @@
+// const mainHeader = document.getElementById('main-header');
+
+// const setFrame = () => {
+//     mainHeader.innerHTML = `
+//         <h3>PORTFOLIO: LINDA MALM</h3>
+//         <h1>frontend developer</h1>
+//         <h2>+ digital designer</h2>    
+//         `;
+// }
+
+// setFrame();
