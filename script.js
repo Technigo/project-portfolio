ca01a417c777d2d338a2237b11ff65f6bfd296e2
Linda Malm <Linda@Lindas-MacBook-Pro.local>
2022-09-15 13:51:07 +0200

    Mobile version: Changed the structure so it is using outerwrapper and innerwrappers for a more unified look. Desktop-version unchanged.

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const linksBottom = document.getElementById('links-bottom')
 
 linksBottom.addEventListener('click', () => {
     window.scrollTo(0, document.body.scrollHeight);
+    linksBottom = '';
 })
