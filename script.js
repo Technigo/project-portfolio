ab75341d82e02ea7bead5ea786034a12be668679
Linus Eriksson <erilinus@gmail.com>
2022-09-25 23:18:40 +0200

    revamp of portfolio

--- script.js
+++
@@ -1,8 +0,0 @@
-// const $button = $('#button');
-// const isMenuOpen = false;
-
-// $button.on('click', function () {
-//     isMenuOpen = !isMenuOpen;
-    
-//     $button.attr('aria-expanded', isMenuOpen);
-// });
