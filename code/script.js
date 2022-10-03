d92ed4a6e1a41aac61b8612c4864f62d057be19d
SofieRydmark <you@example.com>
2022-10-03 13:43:26 +0200

    scrollbtn

--- code/script.js
+++ code/script.js
@@ -1,11 +1,11 @@
 // Scroll button:
 let scrollbutton = document.getElementById("scrollBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 60px from the top of the document, show the button
 window.onscroll = () => {scrollFunction()};
 
 const scrollFunction = () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     scrollbutton.style.display = "block";
   } else {
     scrollbutton.style.display = "none";
@@ -14,6 +14,6 @@ const scrollFunction = () => {
 
 // When the user clicks on the button, scroll to the top of the document
 const topFunction = () => {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 40; // For Safari
+  document.documentElement.scrollTop = 40; // For Chrome, Firefox, IE and Opera
 }
