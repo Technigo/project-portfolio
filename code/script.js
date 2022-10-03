7ec782e0683a99871b865fc715a52f5495726635
SofieRydmark <you@example.com>
2022-10-03 14:39:45 +0200

    prettier

--- code/script.js
+++ code/script.js
@@ -1,19 +1,21 @@
 // Scroll button:
-let scrollbutton = document.getElementById("scrollBtn");
+let scrollbutton = document.getElementById('scrollBtn');
 
 // When the user scrolls down 60px from the top of the document, show the button
-window.onscroll = () => {scrollFunction()};
+window.onscroll = () => {
+  scrollFunction();
+};
 
 const scrollFunction = () => {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-    scrollbutton.style.display = "block";
+    scrollbutton.style.display = 'block';
   } else {
-    scrollbutton.style.display = "none";
+    scrollbutton.style.display = 'none';
   }
-}
+};
 
 // When the user clicks on the button, scroll to the top of the document
 const topFunction = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
