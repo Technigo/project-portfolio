7fdd56d699765140dbbc6bad091f1652b4a86712
khleoth <leothunell@gmail.com>
2023-03-01 14:38:18 +0100

    page one done

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 /* const livedemoBtnFrame = document.querySelectorAll('.livedemoBtnFrame')
-const githubBtnFrame = document.querySelectorAll('.githubBtnFrame')
+const githubBtnFrame = document.querySelectorAll('.githubBtnFrame') */
+/* const linkedinIcon1 = document.querySelectorAll('.linkedinIcon1')
+const githubIcon1 = document.querySelectorAll('.githubIcon1')
+const stackoverflowIcon1 = document.querySelectorAll('.stackoverflowIcon1')
+const linkedinIcon2 = document.querySelectorAll('.linkedinIcon2')
+const githubIcon2 = document.querySelectorAll('.githubIcon2')
+const stackoverflowIcon2 = document.querySelectorAll('.stackoverflowIcon2')
 
 
-
-const mousoverDemoBtn = () => {
+const mouseoverLinkedin = () => {
+    document.querySelectorAll('.linkedinlink').addEventListener('mouseover' , () => {
+        linkedinIcon1.setAttribute('src', './assets/linkedinicon.svg')
+    })
+}
+ */
+/* const mousoverDemoBtn = () => {
     document.querySelector('.livedemoimgblack').setAttribute('src', './assets/whiteglobeicon.svg')
 }
 
