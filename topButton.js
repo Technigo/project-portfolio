d72f7ec053fa4f14920619227e5bef15abe67b04
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-02 22:13:01 +0100

    Added images and scroll

---
+++ topButton.js
@@ -0,0 +1,23 @@
+// Get the button:
+let topButton = document.getElementById("top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function () {
+  displayTopButton();
+  };
+
+const displayTopButton = () => {
+  if (document.body.scrollTop > 20 ||  document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+const scrollToTop = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  
+}
