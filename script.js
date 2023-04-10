c40f4f6ad355aeb73041714491f3e0a887466170
Bridget Mailley <bmailley42@gmail.com>
2023-04-10 10:53:53 +0200

    updated js to show correct hero image

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ const today = new Date();
 //console.log(today);
 // const month = 1;
 const month = today.getMonth();
-//console.log(month);
+// console.log(month);
 
 const imageToShow = () => {
-  if (month === 12 || month === 1){
+  if (month === 11 || month === 0){
     headerImage.innerHTML = `<img src="./assets/decjan.jpg" alt="frost leaves on the ground" class ="header-image"/>`
-  } else if (month === 2 || month === 3){
+  } else if (month === 1 || month === 2){
     headerImage.innerHTML = `<img src="./assets/febmar.jpg" alt="a small purple crocus flower sprouting through dead leaf"  class ="header-image"/>`
-  } else if (month === 4 || month === 5){
+  } else if (month === 3 || month === 4){
     headerImage.innerHTML = `<img src="./assets/aprmay.jpg" alt="single purple allium" class ="header-image"/>`
-  } else if (month === 6 || month === 7){
+  } else if (month === 5 || month === 6){
     headerImage.innerHTML = `<img src="./assets/junjul.jpg" alt="a group of yellow and orange sneezeweed flowers with a small bee" class ="header-image"/>`
-  } else if (month === 8){
+  } else if (month === 7){
     headerImage.innerHTML = `<img src="./assets/aug.jpg" alt="orange and pink zinnias" class ="header-image"/>`
-  } else if (month === 9){
+  } else if (month === 8){
     headerImage.innerHTML = `<img src="./assets/sept.jpg" alt="yellow brown and green leaves in a puddle" class ="header-image"/>`
-  } else if (month === 10 || month === 11){
+  } else if (month === 9 || month === 10){
     headerImage.innerHTML = `<img src="./assets/octnov.jpg" alt="orange grey and green pumpkins and gourds" class ="header-image"/>`
   }
 }
