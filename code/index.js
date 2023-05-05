4e40c307b7c70520a9d1e92b508b5c34c7f3e5cd
tess.aquilon <tess.aquilon@gmail.com>
2023-05-05 17:00:07 +0200

    added a js file to display the projects in a more efficient way

---
+++ code/index.js
@@ -0,0 +1,33 @@
+const projectWrapper = document.getElementById("projectWrapper");
+
+const projects = [
+    {
+    title: "Project Labyrinth",
+    projectSummary: "It's a labyrinth. It's text-based. Careful, or you might get lost!",
+    tech: "React Redux",
+    live: "https://zippy-vacherin-041127.netlify.app/",
+    code: "https://github.com/TessAquilon/project-labyrinth"
+    },
+    {
+    title: "Project Movies",
+    projectSummary: "Want to know what movies have been released recently? Well, this website displays the latest 20.",
+    tech: "React React Router API:s CSS",
+    live: "https://heartfelt-torrone-e608c2.netlify.app/",
+    code: "https://github.com/TessAquilon/project-movies"
+        },
+    {
+    title: "Project Survey",
+    projectSummary: "Ever played a game of MADLIBS? Here is my own version of it. Check it out!",
+    tech: "React",
+    live: "https://fanciful-capybara-de9924.netlify.app/",
+    code: "https://github.com/TessAquilon/project-survey"
+        },
+    {
+    title: "Project To-do",
+    projectSummary: "It's a really cute to-do list with a cat on it. A must-have for cat lovers.",
+    tech: "React Redux",
+    live: "https://gregarious-griffin-0ee914.netlify.app/",
+    code: "https://github.com/TessAquilon/project-todos"
+            }
+    
+]
