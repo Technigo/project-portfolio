985b63a9aa4d5a5541057fd44a5cef8d1d5f7956
So Youn Choi <manghatuhyo@gmail.com>
2023-10-07 20:36:36 +0100

    fakemail

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ export const App = () => {
       </div>
       <div style={{backgroundImage: `url(${kontakt})`, padding: `100px`, color: 'yellow'}}>
         <h1>contact</h1>
+        <p>fakemail@fakemail.jpg</p>
         |<a href="https://www.linkedin.com/in/so-youn-choi-703270212/">linkedin</a>|
         <a href="https://github.com/catfooo">github</a>|
         <a href="https://stackoverflowteams.com/c/technigo/users/490/?tab=profile">stackoverflow</a>|
