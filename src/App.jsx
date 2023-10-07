4c9549f9157f3073628defa3c84f03ed0751ea39
So Youn Choi <manghatuhyo@gmail.com>
2023-10-07 11:01:28 +0100

    bgimg and text

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import bild from './bild.jpg'
+
+export const App = () => {
+    return <div style={{backgroundImage: `url(${bild})`, padding: '100px'}}>
+      <h3>So Youn Choi</h3>
+      <h1>Frontend Developer</h1>
+      <h4>I make web</h4>
+      <p>..</p>
+      
+
+      </div>
+};
