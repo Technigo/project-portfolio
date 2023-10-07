88acf01d2a4884758cc1f14f17613d7b0ee0fd58
So Youn Choi <manghatuhyo@gmail.com>
2023-10-07 12:09:48 +0100

    general description

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import bild from './bild.jpg'
 
 export const App = () => {
-    return <div style={{backgroundImage: `url(${bild})`, padding: '100px', color: 'yellow'}}>
+    return <form style={{backgroundImage: `url(${bild})`, padding: '100px', color: 'yellow'}}>
       <h3>So Youn Choi</h3>
       <h1>Frontend Developer</h1>
       <h4>I make web</h4>
-      <p>..</p>
-      
+      <p style={{color: 'yellow'}}>when i was little, i played with web. kids at that time are just thrown to new virtual world called web. we were happy there and adventureous. while this journey, i wanted to remember that. i was willing to create things that i loved. we were communicating while making web and providing safe place to each other. we all had different situation and different interest, but because we could gather we could grow up together. i wanted to remember that, again, and continue my live at same mood. because my whole journey is saving myself, i wanted to make others feel same as me. we are still growing in some way, and we will do that together, with our own environment created by ourself!</p>
 
-      </div>
+      </form>
 };
