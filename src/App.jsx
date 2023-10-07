99bd1f87cc278655c3188ac325b9f91b6e0aaf70
So Youn Choi <manghatuhyo@gmail.com>
2023-10-07 13:03:43 +0100

    link

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
       <h3>So Youn Choi</h3>
       <h1>Frontend Developer</h1>
       <h4>I make web</h4>
-      <p style={{color: 'yellow'}}>when i was little, i played with web. kids at that time are just thrown to new virtual world called web. we were happy there and adventureous. while this journey, i wanted to remember that. i was willing to create things that i loved. we were communicating while making web and providing safe place to each other. we all had different situation and different interest, but because we could gather we could grow up together. i wanted to remember that, again, and continue my live at same mood. because my whole journey is saving myself, i wanted to make others feel same as me. we are still growing in some way, and we will do that together, with our own environment created by ourself!</p></div>
+      <p style={{color: 'yellow'}}>when i was little, i played with web. kids at that time are just thrown to new virtual world called web. we were happy there and adventureous. while this journey, i wanted to remember that. i was willing to create things that i loved. we were communicating while making web and providing safe place to each other. we all had different situation and different interest, but because we could gather we could grow up together. i wanted to remember that, again, and continue my life at the same mood. because my whole journey is saving myself, i wanted to make others feel same as me. we are still growing in some way, and we will do that together, with our own environment created by ourselves!</p></div>
       <div style={{backgroundImage: `url(${napkin})`, padding: `100px`, color: 'yellow'}}>
         <h1>what i want to do while we work together</h1>
         <p>HTML, CSS, JavaScript, React, mob-programming, pair-programming</p>
@@ -17,8 +17,8 @@ export const App = () => {
       <div style={{backgroundImage: `url(${vatten})`, padding: `100px`, color: 'yellow'}}>
         <h1>what i did while i learn</h1>
         <h2>project happy</h2>
-        <img src={happy} alt="screenshot from project happy" width="50%" height="50%" />
-        <p>i tried to gather happy thoughts from people. user can write things here while they are having happy moment. by doing that, user can think again about their happy moments, and get to know how others feel happy in their life. by liking each other, they can send minor signal which means they are thinking same each other, and the list of liked idea is even saving at the very last part of list, so user can easily collect happy moments!</p>
+        <a href="https://cozy-khapse-25f81c.netlify.app"><img src={happy} alt="screenshot from project happy" width="50%" height="50%" /></a>
+        <p>i tried to gather happy thoughts from people. user can write things here while they are having happy moment. by doing that, user can think again about their happy moments, and get to know how others feel happy in their life. by liking each other, they can send minor signal which means they are thinking same each other, and the list of liked idea is even saving at the very last part of list, so user can easily collect happy moments of others!</p>
       </div>
       </form>
 };
