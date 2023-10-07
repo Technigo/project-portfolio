43265532bff21cad0447a8946fc4723b8be943d1
So Youn Choi <manghatuhyo@gmail.com>
2023-10-07 11:40:47 +0100

    text color yellow

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import bild from './bild.jpg'
 
 export const App = () => {
-    return <div style={{backgroundImage: `url(${bild})`, padding: '100px'}}>
+    return <div style={{backgroundImage: `url(${bild})`, padding: '100px', color: 'yellow'}}>
       <h3>So Youn Choi</h3>
       <h1>Frontend Developer</h1>
       <h4>I make web</h4>
