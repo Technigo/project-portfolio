6fc5ca0be80df6aa304817e2ef5961e2c35aedf0
So Youn Choi <manghatuhyo@gmail.com>
2023-10-09 19:00:13 +0100

    working header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 export const Header = () => {
     return(
-        <div style={{position: 'fixed', zIndex: '3000'}}>...</div>
+        <div style={{position: 'fixed', zIndex: '3000', color: 'yellow', backgroundColor: 'black'}}>
+            So Youn Choi
+            <p>fakemail@fakemail.jpg</p>
+      |<a href="https://www.linkedin.com/in/so-youn-choi-703270212/">linkedin</a>|
+        <a href="https://github.com/catfooo">github</a>|
+        <a href="https://stackoverflowteams.com/c/technigo/users/490/?tab=profile">stackoverflow</a>|
+            </div>
     )
 }
