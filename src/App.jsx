c3045596169c857375f97d2a072078b3b32b96e1
So Youn Choi <manghatuhyo@gmail.com>
2023-10-09 20:18:35 +0100

    passed props for intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import { Intro } from './components/Intro'
-import { Tech } from './components/Tech'
-import { Project } from './components/Project'
-import  Contact  from './components/Contact'
-import { Header } from './components/Header'
+import React from 'react';
+import { Intro } from './components/Intro';
+import { Tech } from './components/Tech';
+import { Project } from './components/Project';
+import Contact from './components/Contact';
+import { Header } from './components/Header';
 
 export const App = () => {
+  // Props for Header component
   const name = "So Youn Choi";
-const email = "fakemail@fakemail.jpg";
-const linkedinLink = "https://www.linkedin.com/in/so-youn-choi-703270212/";
-const githubLink = "https://github.com/catfooo";
-const stackoverflowLink = "https://stackoverflowteams.com/c/technigo/users/490/?tab=profile";
+  const email = "fakemail@fakemail.jpg";
+  const linkedinLink = "https://www.linkedin.com/in/so-youn-choi-703270212/";
+  const githubLink = "https://github.com/catfooo";
+  const stackoverflowLink = "https://stackoverflowteams.com/c/technigo/users/490/?tab=profile";
 
-    return (
+  // Props for Intro component
+  const job = "Frontend Developer";
+  const title = "I make web";
+  const description = "when i was little, i played with web. kids at that time are just thrown to the new virtual world called web. we were happy there and adventurous. while this journey, i wanted to remember that. i was willing to create things that i loved. we were communicating while making web and providing a safe place to each other. we all had a different situation and different interest, but because we could gather we could grow up together. i wanted to remember that, again, and continue my life at the same mood. because my whole journey is saving myself, i wanted to make others feel the same as me. we are still growing in some way, and we will do that together, with our own environment created by ourselves.";
+
+  return (
     <form>
       <Header
-  name={name}
-  email={email}
-  linkedinLink={linkedinLink}
-  githubLink={githubLink}
-  stackoverflowLink={stackoverflowLink}
-/>
-      <Intro />
+        name={name}
+        email={email}
+        linkedinLink={linkedinLink}
+        githubLink={githubLink}
+        stackoverflowLink={stackoverflowLink}
+      />
+      <Intro
+        name={name}
+        job={job}
+        title={title}
+        description={description}
+      />
       <Tech />
       <Project />
       <Contact />
-      </form>
-)};
+    </form>
+  );
+};
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,15 +1,16 @@
-import 'animate.css'
+import 'animate.css';
 
-import bild from './bild.jpg'
-import mig from './mig.jpg'
+import bild from './bild.jpg';
+import mig from './mig.jpg';
 
-export const Intro = () => {
-    return(
-        <div className="animate__animated animate__zoomInDown" style={{backgroundImage: `url(${bild})`, padding: '100px', color: 'yellow'}}>
-        <img src={mig} alt="image of me" width="50%" height="50%" />
-      <h3>So Youn Choi</h3>
-      <h1>Frontend Developer</h1>
-      <h4>I make web</h4>
-      <p style={{color: 'yellow'}}>when i was little, i played with web. kids at that time are just thrown to new virtual world called web. we were happy there and adventureous. while this journey, i wanted to remember that. i was willing to create things that i loved. we were communicating while making web and providing safe place to each other. we all had different situation and different interest, but because we could gather we could grow up together. i wanted to remember that, again, and continue my life at the same mood. because my whole journey is saving myself, i wanted to make others feel same as me. we are still growing in some way, and we will do that together, with our own environment created by ourselves!</p></div>    )
-}
-
+export const Intro = (props) => {
+  return (
+    <div className="animate__animated animate__zoomInDown" style={{backgroundImage: `url(${bild})`, padding: '100px', color: 'yellow'}}>
+      <img src={mig} alt="image of me" width="50%" height="50%" />
+      <h3>{props.name}</h3>
+      <h1>{props.job}</h1>
+      <h4>{props.title}</h4>
+      <p style={{color: 'yellow'}}>{props.description}</p>
+    </div>
+  );
+};
