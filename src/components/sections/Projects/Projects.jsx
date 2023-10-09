6523e97939c86a8c82b125f6a33edf6aa2513ebb
Sakura Tanaka <sito6496t@google.com>
2023-10-09 08:58:40 +0200

    projects is done

--- src/components/sections/Projects/Projects.jsx
+++ src/components/sections/Projects/Projects.jsx
@@ -4,38 +4,37 @@ import { ImageFrame } from "../../ui/ImageFrame";
 import { Label } from "../../ui/Label";
 import { Title } from "../../ui/Title";
 import { primaryColorText } from "../../../styles/styleVariable";
+import { projectsData as data } from "../../../statics/projectsData";
 import styles from "./Projects.module.scss";
 export default function Projects() {
   return (
     <section className={styles.projects_section}>
       <Heading color={primaryColorText}>Featured Projects</Heading>
       <div className={styles.projects_section_cards}>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {data.map((project) => (
+          <ProjectCard project={project} key={project.title} />
+        ))}
       </div>
     </section>
   );
 }
 
-function ProjectCard() {
+function ProjectCard({ project }) {
+  const { imagePath, title, skills, gitUrl, demoUrl, text } = project;
   return (
     <div className={styles.project_card}>
-      <ImageFrame imagePath="/images/berlin.png" borderColor="blue" />
+      <ImageFrame imagePath={imagePath} borderColor="blue" />
       <div className={styles.text_box}>
-        <Title text="Chatbot built in JavaScript" />
-        <p className={styles.text}>
-          The chat bot app is a conversational AI-powered tool designed to enhance user experience
-          by providing instant, personalized, and automated responses to user inquiries.
-        </p>
+        <Title text={title} />
+        <p className={styles.text}>{text}</p>
         <div className={styles.labels}>
-          <Label labelText="HTML" />
-          <Label labelText="React" />
+          {skills.map((skill) => (
+            <Label labelText={skill} key={skill} />
+          ))}
         </div>
         <div className={styles.btn_box}>
-          <Button text="Live demo" iconPath="/icons/github.png" url="#" />{" "}
-          <Button text="View the code" iconPath="/icons/github.png" url="#" />
+          <Button text="Live demo" iconPath="/icons/live-demo.png" url={demoUrl} />{" "}
+          <Button text="View the code" iconPath="/icons/github.png" url={gitUrl} />
         </div>
       </div>
     </div>
