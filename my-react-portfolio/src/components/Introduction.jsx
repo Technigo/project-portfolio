12ed083783e6eed726f6ee32adca9cbfc6d999c6
olgalepisto <olga.lepisto@gmail.com>
2023-10-10 15:24:15 +0300

    practice session

---
+++ my-react-portfolio/src/components/Introduction.jsx
@@ -0,0 +1,9 @@
+import { Heading } from './Heading.jsx'
+
+export const Introduction = () => {
+    return (
+        <Heading
+            heading="Welcome to my portfolio"
+            className="introduction" />
+    )
+}
