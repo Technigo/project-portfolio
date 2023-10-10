3d43f44c0dbc3928d6d285107c6ee70e81224d31
Carolina Luna <kroluna@gmail.com>
2023-10-10 20:20:46 +0200

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
