149b6628b1a5e01c0f9ac1a3b3076a5acdcbaecd
So Youn Choi <manghatuhyo@gmail.com>
2023-10-10 08:09:15 +0100

    hide show header using hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,36 @@
+import React, { useState } from 'react';
+
 export const Header = (props) => {
-    return(
-        <div style={{position: 'fixed', zIndex: '3000', color: 'yellow', backgroundColor: 'black'}}>
-            <p>{props.name}</p>
-      <p>{props.email}</p>
-      <p>|<a href={props.linkedinLink}>linkedin</a>|
-      <a href={props.githubLink}>github</a>|<a href={props.stackoverflowLink}>stackoverflow</a>|</p>
-            </div>
-    )
-}
+  // State variable to control header visibility
+  const [showHeader, setShowHeader] = useState(false);
+
+  // Function to toggle header visibility
+  const toggleHeader = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    setShowHeader(!showHeader);
+  };
+
+  return (
+    <div style={{ position: 'fixed', zIndex: '3000', color: 'yellow', backgroundColor: 'black' }}>
+      {/* Button to toggle header */}
+      <button onClick={(e) => toggleHeader(e)}>
+        {showHeader ? 'not now' : 'contact me'}
+      </button>
+
+      {/* Conditionally render the header */}
+      {showHeader && (
+        <div>
+          <p>{props.name}</p>
+          <p>{props.email}</p>
+          <p>
+            |<a href={props.linkedinLink}>linkedin</a>|
+            <a href={props.githubLink}>github</a>|
+            <a href={props.stackoverflowLink}>stackoverflow</a>|
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 //add navigation links?
