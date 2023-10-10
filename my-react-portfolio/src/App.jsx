e6163134a63cd29a020cb868807814e535b87826
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-10 19:20:10 +0200

    changed list of repos to sort by latest pushed

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -1,7 +1,25 @@
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  //State that tracks repos by latest push
+  const [sortedByDate, setSortedByDate] = useState([]);
+  const apiCall = () => {
+    fetch(
+      `https://api.github.com/users/andrkavr/repos?sort=pushed&direction=desc`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        data.map((item) => {
+          console.log(item.name);
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    apiCall();
+  }, []);
   return (
     <div className="container">
       <Header />
