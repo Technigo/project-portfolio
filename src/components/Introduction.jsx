6129c435d0986a0fa6b41bd57f600e73d4ff5b76
So Youn Choi <manghatuhyo@gmail.com>
2023-10-10 21:32:07 +0100

    elements at introduction placed at right place

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,17 +1,21 @@
 import mig from './mig.jpg';
 
 export const Introduction = (props) => {
+    const containerStyle = {
+        marginLeft: '35%', // Adjust the percentage as needed
+    };
+
     return (
-        <div>
-            <p style={{ fontSize: '25px', textAlign: 'left', textIndent: '35%' }}>
+        <div style={containerStyle}>
+            <p style={{ fontSize: '25px', textAlign: 'left' }}>
                 <strong>Hi, I'm {props.name}</strong>
             </p>
-            <p style={{ color: 'blue', fontSize: '70px', marginTop: '-50px', textAlign: 'left', textIndent: '35%' }}>
+            <p style={{ color: 'blue', fontSize: '70px', marginTop: '-50px', textAlign: 'left' }}>
                 <strong>Frontend Developer</strong>
             </p>
             <div style={{ display: 'inline-block', marginRight: '20%', marginTop: '-80px', textAlign: 'left' }}>
                 <img src={mig} alt="picture of me" width="150px" height="150px" style={{ borderRadius: '50%', objectFit: 'cover', float: 'left', marginRight: '10px' }} />
-                <p style={{ fontSize: '10px'}}>{props.description}</p>
+                <p style={{ fontSize: '10px' }}>{props.description}</p>
             </div>
         </div>
     );
