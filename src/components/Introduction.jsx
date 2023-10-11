7216fc792f92b4f4b60e143ca1f65f72433c8c5c
So Youn Choi <manghatuhyo@gmail.com>
2023-10-11 07:59:47 +0100

    downside arrow from design

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,6 +1,11 @@
+import React, { useState } from 'react'
+
 import mig from './mig.jpg';
+import up from './up.png'
+import down from './down.png'
 
 export const Introduction = (props) => {
+    const [isHovered, setIsHovered] = useState(false)
     const containerStyle = {
         marginLeft: '35%', // Adjust the percentage as needed
     };
@@ -17,7 +22,13 @@ export const Introduction = (props) => {
                 <img src={mig} alt="picture of me" width="150px" height="150px" style={{ borderRadius: '50%', objectFit: 'cover', float: 'left', marginRight: '10px' }} />
                 <p style={{ fontSize: '10px' }}>{props.description}</p>
             </div>
-            <div style={{color: 'rgb(235, 85, 118)'}}>⬇</div>
+            <div
+                style={{ color: 'rgb(235, 85, 118)', cursor: 'pointer' }}
+                onMouseEnter={() => setIsHovered(true)} // Set hover state to true on mouse enter
+                onMouseLeave={() => setIsHovered(false)} // Set hover state to false on mouse leave
+            >
+                {isHovered ? <img src={down} alt="down" /> : <img src={up} alt="up" />}
+            </div>
         </div>
     );
 };
