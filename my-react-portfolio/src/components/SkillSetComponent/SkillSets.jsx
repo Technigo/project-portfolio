05d99f7c36bf99609d675b0714bfbd9f8bbe6074
veronica astrom <v.aastrom@gmail.com>
2023-10-11 12:51:31 +0200

    added skill sets component

--- my-react-portfolio/src/components/SkillSetComponent/SkillSets.jsx
+++ my-react-portfolio/src/components/SkillSetComponent/SkillSets.jsx
@@ -4,37 +4,58 @@ import { Textbox } from "../Textbox";
 
 
 export const SkillSets = () => {
-  const skillCategories = [
-    {
-      mediumSizeHeader: 'Technologies',
-      text: ['HTML5', 'CSS3', 'Javascript ES6', 'React', 'GitHub']
-    },
-    {
-      mediumSizeHeader: 'Toolbox',
-      text: ['Slack', 'Postman']
-    },
-    {
-        mediumSizeHeader: 'Upcoming',
-        text: ['Node.js']
-      },
-    {
-        mediumSizeHeader: 'More',
-        text: ['Agile Methodology', 'Project Management', 'Process Design']
-      }
-  ];
 
-  return (
-    <>
-      {skillCategories.map((category, index) => (
-        <div key={index}>
-          <MediumHeadline mediumSizeHeader={category.mediumSizeHeader} />
-          <ul className="list-without-bullets">
-            {category.text.map((text, itemIndex) => (
-              <li key={itemIndex}><Textbox text={text} /></li>
+    const skillCategories = [
+        {
+          id: "1",
+          mediumSizeHeader: 'Technologies',
+          text: [
+            { id: "101", name: 'HTML5' },
+            { id: "102", name: 'CSS3' },
+            { id: "103", name: 'Javascript ES6' },
+            { id: "104", name: 'React' },
+            { id: "105", name: 'GitHub' }
+          ]
+        },
+        {
+          id: "2",
+          mediumSizeHeader: 'Toolbox',
+          text: [
+            { id: "201", name: 'Slack' },
+            { id: "202", name: 'Postman' }
+          ]
+        },
+        {
+          id: "3",
+          mediumSizeHeader: 'Upcoming',
+          text: [
+            { id: "301", name: 'Node.js' }
+          ]
+        },
+        {
+          id: "4",
+          mediumSizeHeader: 'More',
+          text: [
+            { id: "401", name: 'Agile Methodology' },
+            { id: "402", name: 'Project Management' },
+            { id: "403", name: 'Process Design' }
+          ]
+        }
+      ];
+
+
+    return (
+        <>
+            {skillCategories.map((category) => (
+                <div key={category.id}>
+                    <MediumHeadline mediumSizeHeader={category.mediumSizeHeader} />
+                    <ul className="list-without-bullets">
+                        {category.text.map((skill) => (
+                            <li key={skill.id}><Textbox text={skill.name} /></li>
+                        ))}
+                    </ul>
+                </div>
             ))}
-          </ul>
-        </div>
-      ))}
-    </>
-  );
+        </>
+    );
 };
