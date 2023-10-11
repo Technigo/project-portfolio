91c573700b981a45d0628b783b1112500e03dde0
Sandra <sandragustafsson@Sandras-MacBook-Pro.local>
2023-10-11 11:31:41 +0200

    2 components done

--- my-react-portfolio/src/components/presentation/Presentation.jsx
+++ my-react-portfolio/src/components/presentation/Presentation.jsx
@@ -3,9 +3,12 @@ import './Presentation.css';
 
 export const Presentation = () => {
   return (
+    <div className='presentation-wrapper'>
     <div className='presentation'>
+      <div className='presentation-titles-container'>
         <h3>Hi, I'm Sandra Gustafsson</h3>
-        <h1>Frontend Developer</h1>
+        <h1 className='presentation-bigtitle'>Frontend Developer</h1>
+        </div>
         <div className="profile-section">
             <img 
             src="public/profilimg.jpeg" 
@@ -13,7 +16,7 @@ export const Presentation = () => {
             className='profile-image' />
             <p className='presentation-text'>Sandra, a Frontend Developer, excels in problem-solving and streamlining tasks. Her drive to make processes efficient is matched by her goal-oriented approach. With each project, she seeks perfection and effectiveness. Passionate about continuous growth, she embraces challenges, always eager to learn, adapt, and set higher standards for herself.</p>
         </div>
-
+    </div>
     </div>
   );
 };
--- my-react-portfolio/src/components/tech/Tech.jsx
+++ my-react-portfolio/src/components/tech/Tech.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/prop-types */
+import './Tech.css'
 import React from 'react'
 
 export const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='tech-wrapper'>
+      <h1 className='tech-title'>Tech</h1>
+      <p className='tech-text'>HTML, CSS, Flexbox, Grid, Javascript, JSX, React, React Hooks, Node.js, Web Accessibility, APIs, Branches, Pair-programming, Github, Figma, Netlify, Developer tools</p>
+    </div>
   )
 }
