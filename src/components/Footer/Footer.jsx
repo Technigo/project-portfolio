1236bc6240921fc213d33564067db588bf18124c
jennylarsen <jennymarikalarsen@gmail.com>
2023-10-12 13:46:15 +0200

    styling for footer with animation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import "./footer.css";
 export const Footer = () => {
     return (
         <div className="footer-wrapper">
-        <h1 className="h1-footer">Jenny Larsen </h1>
+        <h1 className="h1-footer">Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer · Jenny Larsen · Frontend Developer  </h1>
         </div>
     )
 }
+
