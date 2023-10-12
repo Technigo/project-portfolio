f3c44d8b8d98834f11b075f4a93cea53371b26e2
LauraLyckholm <laura.lyckholm@gmail.com>
2023-10-12 15:20:13 +0200

    Trying to deploy to Netlify

--- src/ReusableComponents/ImageElements/ImageComponent.jsx
+++ src/ReusableComponents/ImageElements/ImageComponent.jsx
@@ -3,8 +3,8 @@ import "./image.css";
 
 export const ImageComponent = ({ sectionClassName, elementClassName, link, ImageAltText }) => {
     return (
-        <section className={`${sectionClassName}`}>
-            <img className={`${elementClassName}`} src={`${link}`} alt={`${ImageAltText}`} />
+        <section className={sectionClassName}>
+            <img className={elementClassName} src={link} alt={ImageAltText} />
         </section>
     )
 }
