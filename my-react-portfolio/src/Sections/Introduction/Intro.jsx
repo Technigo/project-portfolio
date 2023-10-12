e032d9332940e9099652d0951cdd3574f868b803
hamdiolad <oladhamdi@gmail.com>
2023-10-12 12:10:33 +0200

    intro css

--- my-react-portfolio/src/Sections/Introduction/Intro.jsx
+++ my-react-portfolio/src/Sections/Introduction/Intro.jsx
@@ -2,6 +2,7 @@ import "./intro.css";
 import { CircleImage } from "../../ReusableComponents/Images/CircleImage";
 import { MainTitle } from "../../ReusableComponents/Texts/MainTitle";
 import { NormalText } from "../../ReusableComponents/Texts/NormalText";
+import { SubTitle } from "../../ReusableComponents/Texts/SubTitle";
 
 const introText =
   "Hamdi is an exceptional developer known for her innovative solutions and exceptional coding abilities. She creates user-friendly applications and solves complex issues with ease. Her drive for excellence makes her a valuable asset to any project and a standout in the technology field.";
@@ -9,11 +10,14 @@ const introText =
 export const Intro = () => {
   return (
     <header>
-      <section className="profile-image">
+      <section className="circle-image">
         <CircleImage />
       </section>
+      <section className="sub-title">
+        <SubTitle text={"Hi, Im Hamdi Olad"} />
+      </section>
       <section className="main-title">
-        <MainTitle text={"Fronend Dev"} />
+        <MainTitle text={"Frontend Dev."} />
       </section>
       <section className="normal-text">
         <NormalText text={introText} />
