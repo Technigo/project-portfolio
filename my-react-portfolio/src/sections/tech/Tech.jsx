aac62c77a49f0822149b5e22c8146d5d74da426a
Sheryll-Ann <shey.pc@gmail.com>
2023-10-13 12:41:31 +0200

    creating Tech.jsx section

--- my-react-portfolio/src/sections/tech/Tech.jsx
+++ my-react-portfolio/src/sections/tech/Tech.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { SectionTitle } from "../../reusableComponents/texts/SectionTitle"
+import { NormalText } from "../../reusableComponents/texts/NormalText"
 
-export const Tech = () => {
+export const Tech = ({ sectionTitletext, text }) => {
+
+    const lhstext = 'Tech'
+    const rhstext = 'HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub'
     return (
-        <div>Tech</div>
+        <>
+            <SectionTitle sectionTitletext={lhstext} />
+            <NormalText text={rhstext} />
+        </>
     )
 }
