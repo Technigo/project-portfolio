9eba7d49d65b9530ffad6902d7d620e019752d16
Anna Robertsson <robertsson_anna@hotmail.com>
2023-10-13 12:11:09 +0200

    tech section finished styling

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -1,5 +1,19 @@
+import { MainHeading } from "../Headings/MainHeading/MainHeading";
 import "./Tech.css";
 
 export const Tech = () => {
-  return <p>Here you will see my tech</p>;
+  return (
+    <div className="tech-container">
+      <div className="tech-wrapper">
+        <MainHeading className={"tech-h2"} text={"Tech"} />
+        <div className="techstack-wrapper">
+          <p>
+            HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks,
+            Node.js, Mongo DB, Web Accessibility, API:s, mob-programming,
+            pair-programming, Github.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
