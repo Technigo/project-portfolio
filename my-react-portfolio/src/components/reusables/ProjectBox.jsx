7b66b30c7a944634e8904e5093527af735f90473
olgalepisto <olga.lepisto@gmail.com>
2023-10-13 13:49:37 +0300

    continued building component 'ProjectBox'

--- my-react-portfolio/src/components/reusables/ProjectBox.jsx
+++ my-react-portfolio/src/components/reusables/ProjectBox.jsx
@@ -10,12 +10,28 @@ export const ProjectBox = () => {
         <section className="box-wrapper">
             <ProjectImage />
             <div className="project-info">
-                <SubHeading />
-                <Paragraph />
-                <InfoButton />
-                <LinkButton
-                    className="project-button"
-                    buttonText="testing" />
+                <SubHeading 
+                    subHeading="Project name"
+                    className="project-sub" />
+                <Paragraph 
+                    text="Here is the project info"
+                    className="project-text" />
+                    <div className="info-buttons">
+                        <InfoButton 
+                            buttonText="HTML"
+                            className="info-button" />
+                        <InfoButton
+                            buttonText="CSS"
+                            className="info-button" />
+                    </div>
+                    <div className="link-buttons">
+                        <LinkButton
+                            className="live-demo"
+                            buttonText="Live demo" />
+                        <LinkButton 
+                            className="view-live"
+                            buttonText="View the code" />
+                    </div>
             </div>
         </section>
     )
