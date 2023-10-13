467e4a545bce3ac18c8f3c79f8dbdf28c3a2278f
Julia Holm <juliaholm@julias-mbp.home>
2023-10-13 21:28:21 +0200

    added array data

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h1 align="center">
   <a href="">
-    <img src="/react-p.svg" alt="Project Banner Image">
+    <img src="/technigo-assets/react-p.svg" alt="Project Banner Image">
   </a>
 </h1>
 
@@ -9,6 +9,7 @@
 Congratulations on completing Sprint 2 of the boot camp! For the final phase, we will now take your skills in React to the next level by building your very own portfolio website using the concepts learned in class
 
 ## Instructions
+
 [Check this projects instructions here](https://github.com/Technigo/project-portfolio/blob/main/instructions.md)
 
 ## Set up your own version of your portfolio
---
+++ src/Data.jsx
@@ -0,0 +1,170 @@
+export const navLinks = [
+  {
+    page: "Profile",
+    path: "profile",
+  },
+  {
+    page: "Tech",
+    path: "tech",
+  },
+  {
+    page: "Projects",
+    path: "projects",
+  },
+  {
+    page: "Articles",
+    path: "articles",
+  },
+  {
+    page: "Skills",
+    path: "skills",
+  },
+  {
+    page: "Contact",
+    path: "contact",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Happy Thoughts",
+    description:
+      "A React project focused on state management, API interactions for fetching and posting data. Features include a design-aligned UI, sorted thought display by recency, like count for each thought, a form for new thoughts, and a like functionality.",
+    image: "public/assets/happy-thoughts-image.png",
+    github_url: "https://github.com/JuliaHolm/project-happy-thoughts-vite",
+    demo_url: "https://what-makes-you-happy.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Adopt a Cat",
+    description:
+      "A React project to create a customizable survey form. Focus on React state and form control. After submission, users see a summary. Design is flexible but should prioritise accessibility.",
+    image: "public/assets/adopt-a-cat-image.png",
+
+    github_url: "https://github.com/JuliaHolm/project-survey-vite",
+    demo_url: "https://willowy-meerkat-5ea81b.netlify.app/",
+  },
+  //   {
+  //     id: 3,
+  //     title: "",
+  //     description: "",
+  //     image: "",
+  //     urls: [
+  //       {
+  //         github: "",
+  //         demo: "",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "",
+  //     description: "",
+  //     image: "",
+  //     urls: [
+  //       {
+  //         github: "",
+  //         demo: "",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "",
+  //     description: "",
+  //     image: "",
+  //     urls: [
+  //       {
+  //         github: "",
+  //         demo: "",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "",
+  //     description: "",
+  //     image: "",
+  //     urls: [
+  //       {
+  //         github: "",
+  //         demo: "",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "",
+  //     description: "",
+  //     image: "",
+  //     urls: [
+  //       {
+  //         github: "",
+  //         demo: "",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "",
+  //     description: "",
+  //     image: "",
+  //     urls: [
+  //       {
+  //         github: "",
+  //         demo: "",
+  //       },
+  //     ],
+  //   },
+];
+
+export const skills = [
+  {
+    id: 1,
+    bg_color: "#0B24F5",
+    category: "Code",
+    items: [
+      "HTML5",
+      "CSS3",
+      "Javascript ES6",
+      "React",
+      "Styled Components",
+      "GitHub",
+    ],
+  },
+  {
+    id: 2,
+    bg_color: "#2483E0",
+    category: "Toolbox",
+    items: [
+      "Atom",
+      "Postman",
+      "Adobe Photoshop",
+      "Adobe Illustrator",
+      "Figma",
+      "Keynote",
+      "Slack",
+    ],
+  },
+  {
+    id: 3,
+    bg_color: "#6DB486",
+    category: "Uppcoming",
+    items: ["Node.js"],
+  },
+  {
+    id: 4,
+    bg_color: "#EB5577",
+    category: "More",
+    items: [
+      "Design Lead",
+      "Branding",
+      "Strategy",
+      "Process design",
+      "Concept development",
+      "Agile methodology",
+      "Hyper Island toolbox",
+    ],
+  },
+];
