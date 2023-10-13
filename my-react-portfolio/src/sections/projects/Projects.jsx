adc9acacf485825af35ee96e69f353356663f034
mariateresepettersson <mariateresepettersson>
2023-10-13 09:48:53 +0100

    Added classes and styling to projects component

--- my-react-portfolio/src/sections/projects/Projects.jsx
+++ my-react-portfolio/src/sections/projects/Projects.jsx
@@ -5,9 +5,11 @@ import { ListProjectCards } from './ListProjectCards.jsx';
 export const Projects = () => {
 
     return (
-        <div className="projects-container">
-            <Heading level={1} text="Featured Projects" className="featured-projects" />
-            <ListProjectCards />
-        </div>
+        <section className="projects-container">
+            <section className='projects-wrapper'>
+                <Heading level={1} text="Featured Projects" className="projects-heading" />
+                <ListProjectCards />
+            </section>
+        </section>
     );
 };
