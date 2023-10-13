f2b94f4f3fcaa273881db253944ae7b40a17a618
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-13 14:08:46 +0200

    minor changes

--- my-react-portfolio/src/sections/letstalk/LetsTalk.jsx
+++ my-react-portfolio/src/sections/letstalk/LetsTalk.jsx
@@ -1,3 +1,25 @@
+import "./letstalk.css";
+import profilePic from "../../assets/Bild.jpg";
+
 export const LetsTalk = () => {
-  return <div>LetsTalk</div>;
+  return (
+    <div className="letstalk-container">
+      <div className="lets-talk">
+        <h3 className="lets-talk-title">LetsTalk</h3>
+        <img
+          src={profilePic}
+          alt="profile-picture"
+          className="profile-picture"
+        />
+
+        <div className="contact-info">
+          <p className="contact-name">Andrej Kavrzic</p>
+          <a href="tel:+46737332619" className="phone-number">
+            +46(0)737332619
+          </a>
+          <a href="mailto:andrej.kavrzic@gmail.com">andrej.kavrzic@gmail.com</a>
+        </div>
+      </div>
+    </div>
+  );
 };
