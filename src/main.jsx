19268fa3b01fceb3ee45f32aafead36c1dc077cc
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-10-13 20:04:41 +0700

    update my-react-portfolio
    update my-react-portfolio

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.createRoot(document.getElementById('Root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+
+)
+
+
