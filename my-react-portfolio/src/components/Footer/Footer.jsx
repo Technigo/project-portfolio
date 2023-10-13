57ca144cacafa745e715d4b6f6c517054dfacc4e
majoh23 <139242342+majoh23@users.noreply.github.com>
2023-10-13 16:08:37 +0200

    More css.

--- my-react-portfolio/src/components/Footer/Footer.jsx
+++ my-react-portfolio/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 export const Footer = () => {
     return (
       <footer className="footer-container">
-          <h3 className="rolling-text">Manne Johansson · Fullstack Developer</h3>
+          <h3 className="rolling-text">Manne Johansson · Frontend Developer</h3>
       </footer>
     );
   }
--- my-react-portfolio/src/components/Tech/Tech.jsx
+++ my-react-portfolio/src/components/Tech/Tech.jsx
@@ -3,11 +3,11 @@ import { Headline } from '../Headline/Headline';
 import './Tech.css';
 
 export const Tech = () => {
-    return (
-      <div className="tech-container">
-        <Headline className="headline" text="Tech" level={1} />
-          <h3>HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, API:s, 
-mob-programming, pair-programming, Github. </h3>
-      </div>
-    );
-  }
+  return (
+    <div className="tech-container">
+      <Headline className="headline" text="Tech" level={1} />
+      <h3>HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, API:s, 
+mob-programming, pair-programming, Github.</h3>
+    </div>
+  );
+}
