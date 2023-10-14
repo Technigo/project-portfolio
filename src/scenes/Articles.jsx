7bacd09791fa68f441059d71cbc2862723facf73
Julia Holm <juliaholm@julias-mbp.home>
2023-10-14 22:19:24 +0200

    -

--- src/scenes/Articles.jsx
+++ src/scenes/Articles.jsx
@@ -4,8 +4,7 @@ import { ArticleCard } from "../components/ArticleCard";
 export const Articles = () => {
   return (
     <section className="articles" id="articles">
-      <div className="wavy-line-wrapper" >
-      </div>
+      <div className="wavy-line-wrapper"></div>
       <div className="page-wrapper">
         <div className="page-container bg-page-container">
           <h2 className="page-title">My words</h2>
