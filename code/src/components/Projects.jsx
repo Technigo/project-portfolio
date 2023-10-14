638883da9e11c8386111f90098ad6b10b232c16a
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-14 20:35:07 +0200

    changed direction of projects.json

--- code/src/components/Projects.jsx
+++ code/src/components/Projects.jsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ViewGithubBtn from "./ViewGithubBtn";
 import LiveDemoBtn from "./LiveDemoBtn";
-import projectsData from "../path/to/projects.json"; // Use the correct relative path
-
 
 const Projects = () => {
-    const projects = projectsData.projects; // Access the projects array from the imported JSON data
+    const [repos, setRepos] = useState([]);
+    const apiUrl = "https://api.github.com/users/lisawh0/repos";
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                // Extract relevant information from the API response
+                const projectData = data.map((repo) => ({
+                    id: repo.id,
+                    name: repo.name,
+                    description: repo.description,
+                    url: repo.html_url,
+                }));
+                setRepos(projectData);
+            })
+            .catch((error) => console.error("Error fetching data:", error));
+    }, []);
 
     return (
         <div className="projects">
-            <h1>Featured Projects</h1>
-            {projects.map((project, index) => (
-                <div key={index} className="project-child">
+            <h1>Feautred Projects</h1>
+            {repos.map((project) => (
+                <div key={project.id} className="project-child">
                     <h3>{project.name}</h3>
                     <p>{project.description}</p>
-                    <a href={project.github} target="_blank" rel="noopener noreferrer">
+                    <a href={project.url} target="_blank" rel="noopener noreferrer">
                         <ViewGithubBtn />
                     </a>
-                    <a href={project.netlify} target="_blank" rel="noopener noreferrer">
+                    <a href={project.url} target="_blank" rel="noopener noreferrer">
                         <LiveDemoBtn />
                     </a>
                 </div>
             ))}
         </div>
     );
+
 };
 
 export default Projects;
