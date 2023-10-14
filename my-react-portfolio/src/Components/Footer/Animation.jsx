64150c53544c9d51b3459cfa8f50c1dd07e6ea25
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-10-14 12:01:39 +0200

    added footer

---
+++ my-react-portfolio/src/Components/Footer/Animation.jsx
@@ -0,0 +1,8 @@
+import "./Animation.css"
+
+export const Animation = () => {
+  return (
+    <h3 className="rolling-text">Klaudia Wróblewska· Frontend Developer</h3>
+  )
+}
+
--- my-react-portfolio/src/Components/Footer/Footer.jsx
+++ my-react-portfolio/src/Components/Footer/Footer.jsx
@@ -1,17 +1,23 @@
 import { CardHeading } from "../../ReusableComponents/CardHeading"
 import { Contact } from "./Contact"
 import { MyPhoto } from "../../ReusableComponents/MyPhoto.jsx"
+import { SocialLinks } from "./SocialLinks"
+import { Animation } from "./Animation"
+import "./Footer.css"
 
 
 export const Footer = ()  =>{
   return (
+    <>
     <footer>
       <CardHeading className="blue-heading" text="Let’s talk" />
       <MyPhoto />
       <Contact />
       <div className="icons">
-        
+        <SocialLinks />
       </div>
     </footer>
+      <Animation />
+      </>
   )
 }
