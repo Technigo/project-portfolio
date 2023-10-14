650780987317b8c0f76d7bedb0ed326877cedcb2
mariateresepettersson <mariateresepettersson>
2023-10-14 11:18:41 +0100

    Added content and styled contact component

--- my-react-portfolio/src/sections/contact/Contact.jsx
+++ my-react-portfolio/src/sections/contact/Contact.jsx
@@ -1,35 +1,39 @@
+import './contact.css'
 import { Heading } from '/src/components/typography/Heading.jsx';
 import { Image } from '/src/components/images/Image.jsx';
 
 export const Contact = () => {
 
     return (
-        <section className="contact">
-            <Heading
-                level={1} text="Let's talk"
-                aria-label="This is the main heading"
-                className="contact-heading"
-            />
-            <Image
-                sectionClassName={"profile-img"}
-                elementClassName={"circle-img"}
-                link={"/src/assets/profile-image.png"}
-                ImageAltText={"Profile Image of Maria"}
-            />
-            <Heading
-                level={3}
-                text="Maria Pettersson\n+46(0)730 30 07 66\nmariateresepettersson@gmail.com"
-                aria-label="This is the main heading"
-                className="contact-heading"
-            />
+        <footer className="contact-container">
+            <section className='contact-wrapper'>
+                <Heading
+                    level={1} text="Let's talk"
+                    aria-label="This is the main heading"
+                    className="contact-heading"
+                />
+                <Image
+                    sectionClassName={"profile-img"}
+                    elementClassName={"circle-img"}
+                    link={"/src/assets/profile-image.png"}
+                    ImageAltText={"Profile Image of Maria"}
+                />
+                <address className='contact-info'>
+                    <ul>
+                        <li>Maria Pettersson</li>
+                        <li>+46(0)730 30 07 66</li>
+                        <li>mariateresepettersson@gmail.com</li>
+                    </ul>
+                </address>
+            </section>
             <div className="banner">
-            <Heading
-                level={3}
-                text="Maria Pettersson · Fullstack Developer · Maria Pettersson · Fullstack Developer · Maria Pettersson · Fullstack Developer"
-                aria-label="This is the main heading"
-                className="scroll-text"
-            />
+                <Heading
+                    level={3}
+                    text="Maria Pettersson · Fullstack Developer · Maria Pettersson · Fullstack Developer · Maria Pettersson · Fullstack Developer"
+                    aria-label="This is the main heading"
+                    className="scroll-text"
+                />
             </div>
-        </section>
+        </footer>
     );
 };
