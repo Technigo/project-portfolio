012e2d8cc8a1140f7b90573bfc585857ad552046
Sandra Gustafsson <sandragustafsson@mac-mini.home>
2023-10-14 21:01:14 +0200

    flex and grid

--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -9,14 +9,18 @@ export const Presentation = () => {
             src="images/profilimg.jpeg" 
             alt="image of Sandra" 
             className='profile-image' />
-      
+      <div className='presentation-content'> 
       <div className='presentation-titles-container'>
         <h3>Hi, I'm Sandra Gustafsson</h3>
         <h1 className='presentation-bigtitle'>Frontend Developer</h1>
         </div>
             <p className='presentation-text'>Sandra, a Frontend Developer, excels in problem-solving and streamlining tasks. Her drive to make processes efficient is matched by her goal-oriented approach. With each project, she seeks perfection and effectiveness. Passionate about continuous growth, she embraces challenges, always eager to learn, adapt, and set higher standards for herself.</p>
+            
+         <div className='arrow-wrapper'>
         <img src={Arrow} alt='Arrow pointing down'></img>
         </div>
+        </div>
+      </div>
     </div>
     
   );
