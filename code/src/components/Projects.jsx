d647e2ea3d6696b40f3fd633e6b1897f72f40493
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-14 21:40:28 +0200

    styling the project part

--- code/src/components/Projects.jsx
+++ code/src/components/Projects.jsx
@@ -3,7 +3,6 @@ import ViewGithubBtn from "./ViewGithubBtn";
 import LiveDemoBtn from "./LiveDemoBtn";
 import projectsData from "../data/projects.json"; // Adjust the path as needed
 
-
 const Projects = () => {
     const [projects, setProjects] = useState([]);
 
@@ -17,15 +16,28 @@ const Projects = () => {
             <h1>Featured Projects</h1>
             {projects.map((project, index) => (
                 <div key={index} className="project-child">
-                    <h3>{project.name}</h3>
-                    <img src={project.image} alt={project.name} />
-                    <p>{project.description}</p>
-                    <a href={project.github} target="_blank" rel="noopener noreferrer">
-                        <ViewGithubBtn />
-                    </a>
-                    <a href={project.netlify} target="_blank" rel="noopener noreferrer">
-                        <LiveDemoBtn />
-                    </a>
+                    <div className="project-details">
+                        <img className="projectimg" src={project.image} alt={project.name} />
+                        <div className="text-details">
+                            <h2>{project.name}</h2>
+                            <p>{project.description}</p>
+                            <div className="tags">
+                                {project.tags.map((tag, index) => (
+                                    <span key={index} className="tag">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="project-actions">
+                        <a href={project.github} target="_blank" rel="noopener noreferrer">
+                            <ViewGithubBtn />
+                        </a>
+                        <a href={project.netlify} target="_blank" rel="noopener noreferrer">
+                            <LiveDemoBtn />
+                        </a>
+                    </div>
                 </div>
             ))}
         </div>
