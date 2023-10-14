c804fdbfdc17f51ce4f121f7e481d77483fc01b5
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-14 22:46:04 +0200

    skills css

--- code/src/components/Mywords.jsx
+++ code/src/components/Mywords.jsx
@@ -4,7 +4,6 @@ const Mywords = () => {
     return (
         <div className="mywords">
             <div className="path">{/*path-pic here*/}
-
                 <h1>My Words</h1>
             </div>
         </div>
