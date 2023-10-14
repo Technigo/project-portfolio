8e2529e3374b560f8bbd25b6a5bb3d2ac05fc739
verawitting <135651734+verawitting@users.noreply.github.com>
2023-10-14 23:11:24 +0200

    resolving links again

--- src/sections/components/SocialMedia.jsx
+++ src/sections/components/SocialMedia.jsx
@@ -1,5 +1,9 @@
 import "./SocialMedia.css"
 
+import linkedinLight from "/src/assets/social/linkedinlight.svg"
+import gitHubLight from "/src/assets/social/githublight.svg"
+import instaLight from "/src/assets/social/instagramlight.svg"
+
 export const SocialMedia = () => {
 
 
@@ -8,17 +12,17 @@ export const SocialMedia = () => {
             <a  href="https://www.linkedin.com/in/vera-witting-ba51b1b6"
                 target="_blank"
                 rel="noreferrer noopener">
-                <img className="linkedin-icon" src="src/assets/social/linkedinlight.svg" alt="Linked in icon." />
+                <img className="linkedin-icon" src={linkedinLight} alt="Linked in icon." />
             </a>
             <a  href="https://github.com/verawitting"
                 target="_blank"
                 rel="noreferrer noopener">
-                <img className="github-icon" src="src/assets/social/githublight.svg" alt="Git Hub icon." />
+                <img className="github-icon" src={gitHubLight} alt="Git Hub icon." />
             </a>
             <a  href="https://www.instagram.com/verawitting/"
                 target="_blank"
                 rel="noreferrer noopener">
-                <img className="instagram-icon" src="src/assets/social/instagramlight.svg" alt="Instagram icon" />
+                <img className="instagram-icon" src={instaLight} alt="Instagram icon" />
             </a>
         </div>
     )
