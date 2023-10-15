1bcd6cd48404674515f90cc328a29c6d20b2c9b6
Emmy Jansson <emmybengtsson@live.se>
2023-10-15 14:22:21 +0200

    added the article cards

--- src/Components.jsx/ArticleCard.jsx
+++ src/Components.jsx/ArticleCard.jsx
@@ -1,25 +1,63 @@
 // import { Photo } from "./components/Photo"
 import { LinkButton } from "./LinkButton";
-import { Photo } from "./Photo"; // Import the 'Photo' component
+import "./ArticleCard.css";
 
 export const ArticleCard = () => {
   const publishedDate = "Oct 2023";
   const articleTitle = "From Healthcare Worker to Frontend Developer";
   const articleText = `"That's why I like tech as a community. Because it's helpful and inviting, especially to newbies."`;
   const url =
-    "https://www.technigo.io/stories/antonia-from-healthcare-to-development";
+    "https://www.technigo.io/stories/antonia-from-healhtcare-to-development";
 
   return (
-    <div className="article-card">
-      <Photo />
-      <h2>{articleTitle}</h2>
-      <p>{articleText}</p>
-      <a href={url} target="_blank" rel="noopener noreferrer"></a>
-      <LinkButton
-        buttonText=""
-        url={url}
-        icon="./src/Icons/Read-On-Medium.png"
-      />
+    <div className="article-card-container">
+      <div className="article-card">
+        <img src="./src/pictures/article.jpg" alt="" />
+        <h2>{articleTitle}</h2>
+        <p>{articleText}</p>
+        <a href={url} target="_blank" rel="noopener noreferrer"></a>
+        <LinkButton
+          buttonText=""
+          url={url}
+          icon="./src/Icons/Read-On-Medium.png"
+        />
+      </div>
+
+      <div className="article-card">
+        <img src="./src/pictures/article.jpg" alt="" />
+        <h2>{articleTitle}</h2>
+        <p>{articleText}</p>
+        <a href={url} target="_blank" rel="noopener noreferrer"></a>
+        <LinkButton
+          buttonText=""
+          url={url}
+          icon="./src/Icons/Read-On-Medium.png"
+        />
+      </div>
+
+      <div className="article-card">
+        <img src="./src/pictures/article.jpg" alt="" />
+        <h2>{articleTitle}</h2>
+        <p>{articleText}</p>
+        <a href={url} target="_blank" rel="noopener noreferrer"></a>
+        <LinkButton
+          buttonText=""
+          url={url}
+          icon="./src/Icons/Read-On-Medium.png"
+        />
+      </div>
+
+      <div className="article-card">
+        <img src="./src/pictures/article.jpg" alt="" />
+        <h2>{articleTitle}</h2>
+        <p>{articleText}</p>
+        <a href={url} target="_blank" rel="noopener noreferrer"></a>
+        <LinkButton
+          buttonText=""
+          url={url}
+          icon="./src/Icons/Read-On-Medium.png"
+        />
+      </div>
     </div>
   );
 };
