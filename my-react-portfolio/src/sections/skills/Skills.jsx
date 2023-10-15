740b1d62a8df6303b261ea7e54710db0110ae61d
Sheryll-Ann <shey.pc@gmail.com>
2023-10-15 23:04:51 +0200

    styling Skills component

--- my-react-portfolio/src/sections/skills/Skills.jsx
+++ my-react-portfolio/src/sections/skills/Skills.jsx
@@ -9,55 +9,57 @@ export const Skills = () => {
 
         <section className="skills-section">
             <SectionTitle sectionTitletext={skillsTitle} />
-            <div className="code">
-                <MediumsizedTitle mediumHeader={'Code'} />
-                <p>
-                    <ul>
-                        <li>HTML5</li>
-                        <li>CSS3</li>
-                        <li>Javascript ES6</li>
-                        <li>React</li>
-                        <li>Styled Components</li>
-                        <li>GitHub</li>
+            <div className="skills-flex">
+                <div className="code">
+                    <MediumsizedTitle mediumHeader={'Code'} />
+                    <p>
+                        <ul>
+                            <li>HTML5</li>
+                            <li>CSS3</li>
+                            <li>Javascript ES6</li>
+                            <li>React</li>
+                            <li>Styled Components</li>
+                            <li>GitHub</li>
 
-                    </ul>
-                </p>
-            </div>
-            <div className="toolbox">
-                <MediumsizedTitle mediumHeader={'Toolbox'} />
-                <p>
-                    <ul>
-                        <li>Figma</li>
-                        <li>Slack</li>
-                        <li>Postman</li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </p>
-            </div>
-            <div className="upcoming">
-                <MediumsizedTitle mediumHeader={'Upcoming'} />
-                <p>
-                    <ul>
-                        <li>Node.js</li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </p>
-            </div>
-            <div className="more">
-                <MediumsizedTitle mediumHeader={'More'} />
-                <p>
-                    <ul>
-                        <li>Agile methology</li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </p>
+                        </ul>
+                    </p>
+                </div>
+                <div className="toolbox">
+                    <MediumsizedTitle mediumHeader={'Toolbox'} />
+                    <p>
+                        <ul>
+                            <li>Figma</li>
+                            <li>Slack</li>
+                            <li>Postman</li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="upcoming">
+                    <MediumsizedTitle mediumHeader={'Upcoming'} />
+                    <p>
+                        <ul>
+                            <li>Node.js</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="more">
+                    <MediumsizedTitle mediumHeader={'More'} />
+                    <p>
+                        <ul>
+                            <li>Agile methology</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </p>
+                </div>
             </div>
         </section>
     )
