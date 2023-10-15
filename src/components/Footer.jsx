46ee1d1c1bd27305c156904638e2b8cb265ad234
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-16 01:08:02 +0200

    adding rollerbanner

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './footer.css';
+import RollingBanner from './RollingBanner';
 
 function Footer() {
     return (
         <footer className="footer-banner">
             <div className="container">
-                <h3>Lisa Dahlkar · Front End Developer · Lisa Dahlkar · Front End Developer · Lisa Dahlkar · Front End Developer · Lisa Dahlkar · Front End Developer · Lisa Dahlkar · Front End Developer · Lisa Dahlkar · Front End Developer · </h3>
+                <RollingBanner />
             </div>
         </footer>
     );
