14e3871f087879fa8b65198c8e54e93c5897da09
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-10-15 22:54:19 +0200

    add contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,36 @@
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
 import data from "./data.json";
+import Container from "../container/Container";
 
 const Contact = () => {
     return (
-        <div className="container mx-auto py-40 px-8 md:px-40">
-            <h2 className="text-6xl font-bold text-blue-700 text-center mb-16">{data.title}</h2>
-        </div>
+        <Container>
+            <h2 className="text-6xl font-bold text-technigo-blue text-center mb-16">
+                {data.title}
+            </h2>
+            <img
+                className="rounded-full w-40 mx-auto mb-8"
+                src={data.imageUrl}
+                alt={data.imageAlt}
+            />
+            <p className="text-center font-semibold text-xl mb-8">
+                {data.name}
+            </p>
+            <div className="flex gap-4 justify-center">
+                <a
+                    aria-label="LinkedIn"
+                    href={data.linkedinUrl}
+                >
+                    <IconBrandLinkedin className="text-gray-400 hover:text-gray-600" />
+                </a>
+                <a
+                    aria-label="Github"
+                    href={data.githubUrl}
+                >
+                    <IconBrandGithub className="text-gray-400 hover:text-gray-600" />
+                </a>
+            </div>
+        </Container>
     );
 }
 
