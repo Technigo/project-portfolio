8c20f66ee51887990255f4bd5de2850a10471236
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-10-16 00:01:19 +0200

    Changing media queries

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -21,16 +21,16 @@ export const Intro = () => {
           <p>
             Susanne is a budding developer with a genuine interest in innovative
             solutions and a strong desire to refine her coding skills. With her
-            extensive experience as a manager in customer service and a
-            Bachelor's degree in social work, she brings a unique perspective to
-            her work. In addition, Susanne is particularly enthusiastic about
-            incorporating accessibility features into her projects, ensuring
-            inclusivity and usability for all users. In her current role, she
-            serves as a vital link between customers and technology, utilizing
-            her skills to develop user-friendly applications and tackle complex
-            challenges. Her unwavering commitment to maintaining high standards
-            underscores her value as a contributing member of any project,
-            distinguishing her in the technology field.
+            extensive experience as a manager in customer service she brings a
+            unique perspective to her work. In addition, Susanne is particularly
+            enthusiastic about incorporating accessibility features into her
+            projects, ensuring inclusivity and usability for all users. In her
+            current role, she serves as a vital link between customers and
+            technology, utilizing her skills to develop user-friendly
+            applications and tackle complex challenges. Her unwavering
+            commitment to maintaining high standards underscores her value as a
+            contributing member of any project, distinguishing her in the
+            technology field.
           </p>
         </div>
       </div>
