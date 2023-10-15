f3794c4d9a5b70b57835f164001bd3cff80bfde0
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-10-15 15:07:10 +0700

    update components
    update components

--- my-react-portfolio/components/featuredproj.jsx
+++ my-react-portfolio/components/featuredproj.jsx
@@ -1,15 +1,284 @@
-import React from "react";
+// import React, { useState, useEffect } from "react";
+
+// export const Featuredproj = () => {
+//   const [repos, setRepos] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchRepos() {
+//       try {
+//         const response = await fetch('https://api.github.com/users/stenlisuryadinata/repos');
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         setRepos(data);
+//       } catch (error) {
+//         console.error('Error fetching repos:', error);
+//       }
+//     }
+
+//     fetchRepos();
+//   }, []);
+
+//   return (
+//     <div className="featured_container">
+//       <div className="featured_text">
+//         <h3 className="featured_myname">Featured Projects</h3>
+
+//         {repos.map((repo) => (
+//           <div key={repo.id} className="repo-item">
+//             <p>{repo.name}</p>
+//             <p>{repo.description}</p>
+//             <p>Language: {repo.language}</p>
+//             <p>Stars: {repo.stargazers_count}</p>
+//             <p>Forks: {repo.forks_count}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// import React, { useState, useEffect } from "react";
+
+// export const Featuredproj = () => {
+//   const [repos, setRepos] = useState([]);
+//   const selectedRepoNames = ["Digital-Business-Card"];  // Add the project name you want to showcase here
+//   const selectedRepoNames = ["project-happy-thoughts-vite"];
+//   const selectedRepoNames = ["Digital-Business-Card"];
+//   const selectedRepoNames = ["Digital-Business-Card"];
+  
+//   useEffect(() => {
+//     async function fetchRepos() {
+//       try {
+//         const response = await fetch('https://api.github.com/users/stenlisuryadinata/repos');
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+
+//         // Filter and select the project with the specified name
+//         const selectedRepos = data.filter((repo) =>
+//           selectedRepoNames.includes(repo.name)
+//         );
+
+//         setRepos(selectedRepos);
+//       } catch (error) {
+//         console.error('Error fetching repos:', error);
+//       }
+//     }
+
+//     fetchRepos();
+//   }, [selectedRepoNames]);
+
+//   return (
+//     <div className="featured_container">
+//       <div className="featured_text">
+//         <h3 className="featured_myname">Featured Projects</h3>
+
+//         {repos.map((repo) => (
+//           <div key={repo.id} className="repo-item">
+//             <h4>{repo.name}</h4>
+//             <p>{repo.description}</p>
+//             <p>Language: {repo.language}</p>
+//             <p>Stars: {repo.stargazers_count}</p>
+//             {/* You can add more details from the GitHub API response */}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// import React, { useState, useEffect } from "react";
+
+// export const Featuredproj = () => {
+//   const [repos, setRepos] = useState([]);
+//   const selectedRepoNames = [
+//     "Digital-Business-Card",
+//     "project-happy-thoughts-vite",
+//     "Another-Project-Name"
+//   ];  // Add the project names you want to showcase here
+
+//   useEffect(() => {
+//     async function fetchRepos() {
+//       try {
+//         const response = await fetch('https://api.github.com/users/stenlisuryadinata/repos');
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+
+//         // Filter and select the projects with the specified names
+//         const selectedRepos = data.filter((repo) =>
+//           selectedRepoNames.includes(repo.name)
+//         );
+
+//         setRepos(selectedRepos);
+//       } catch (error) {
+//         console.error('Error fetching repos:', error);
+//       }
+//     }
+
+//     fetchRepos();
+//   }, [selectedRepoNames]);
+
+//   return (
+//     <div className="featured_container">
+//       <div className="featured_text">
+//         <h3 className="featured_myname">Featured Projects</h3>
+
+//         {repos.map((repo) => (
+//           <div key={repo.id} className="repo-item">
+//             <h4>{repo.name}</h4>
+//             <p>{repo.description}</p>
+//             <p>Language: {repo.language}</p>
+//             <p>Stars: {repo.stargazers_count}</p>
+//             {/* You can add more details from the GitHub API response */}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// import React, { useState, useEffect } from "react";
+// import projectsData from "./projects.json";
+
+// export const Featuredproj = () => {
+//   const [combinedData, setCombinedData] = useState([]);
+//   const selectedRepoNames = [
+//     "Digital-Business-Card",
+//     "project-happy-thoughts-vite",
+//     "Another-Project-Name"
+//   ];
+
+//   useEffect(() => {
+//     async function fetchGitHubData() {
+//       try {
+//         const response = await fetch('https://api.github.com/users/stenlisuryadinata/repos');
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         const gitHubData = await response.json();
+
+//         // Filter and select the GitHub repositories with the specified names
+//         const selectedGitHubRepos = gitHubData.filter((repo) =>
+//           selectedRepoNames.includes(repo.name)
+//         );
+
+//         // Combine GitHub data with local project data
+//         // const combinedData = selectedGitHubRepos.concat(projectsData.projects);
+//         const combinedData = projectsData.projects.concat(selectedGitHubRepos);
+//         //projectsData.projects.concat(selectedGitHubRepos);
+//         setCombinedData(combinedData);
+//       } catch (error) {
+//         console.error('Error fetching GitHub repos:', error);
+//       }
+//     }
+
+//     fetchGitHubData();
+//   }, [selectedRepoNames]);
+
+//   return (
+//     <div className="featured_container">
+//       <div className="featured_text">
+//         <h3 className="featured_myname">Featured Projects</h3>
+
+//         {combinedData.map((project) => (
+//           <div key={project.name} className="repo-item">
+//             <h4>{project.name}</h4>
+//             <img src={project.image} alt={project.name} />
+//             {Array.isArray(project.tags) && project.tags.length > 0 && (
+//               <p>Tags: {project.tags.join(', ')}</p>
+//             )}
+//             {project.netlify && (
+//               <p>Netlify: <a href={project.netlify}>{project.netlify}</a></p>
+//             )}
+//             {project.github && (
+//               <p>GitHub: <a href={project.github}>{project.github}</a></p>
+//             )}
+//             {/* You can add more details from the project objects */}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+
+import React, { useState, useEffect } from "react";
+import projectsData from "./projects.json";
 
 export const Featuredproj = () => {
-    return (
-        <div className="featured_container">
-                <div className="featured_text">
-                    <h3 className="featured_myname">Featured Project</h3>
-                    <h1 className="featured_desc">HTML, CSS, Flexbox, JavaScript, ES6, 
-                        JSX, React, React Hooks, Node.js, 
-                        Mongo DB, Web Accessibility, APIs, 
-                        mob-programming, pair-programming, GitHub. </h1>
-                </div>
-        </div>
-    );
-    };
+  const [combinedData, setCombinedData] = useState([]);
+  const selectedRepoNames = [
+    "Digital-Business-Card",
+    "project-happy-thoughts-vite",
+    "Another-Project-Name"
+  ];
+
+  useEffect(() => {
+    async function fetchGitHubData() {
+      try {
+        const response = await fetch('https://api.github.com/users/stenlisuryadinata/repos');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const gitHubData = await response.json();
+
+        // Filter and select the GitHub repositories with the specified names
+        const selectedGitHubRepos = gitHubData.filter((repo) =>
+          selectedRepoNames.includes(repo.name)
+        );
+
+        // Combine GitHub data with local project data
+        // const combinedData = selectedGitHubRepos.concat(projectsData.projects);
+        const combinedData = projectsData.projects.concat(selectedGitHubRepos);
+
+        setCombinedData(combinedData);
+      } catch (error) {
+        console.error('Error fetching GitHub repos:', error);
+      }
+    }
+
+    fetchGitHubData();
+  }, [selectedRepoNames]);
+
+  return (
+    <div className="featured_container">
+      <div className="featured_text">
+        <h3 className="featured_myname">Featured Projects</h3>
+
+        {combinedData.map((project) => (
+          <div key={project.name} className="repo-item">
+            <h4>{project.name}</h4>
+            {project.description && (
+              <p>{project.description}</p>
+            )}
+            {project.language && (
+              <p>Language: {project.language}</p>
+            )}
+            {project.stargazers_count && (
+              <p>Stars: {project.stargazers_count}</p>
+            )}
+            <img src={project.image} alt={project.name} />
+            {Array.isArray(project.tags) && project.tags.length > 0 && (
+              <p>Tags: {project.tags.join(', ')}</p>
+            )}
+            {project.netlify && (
+              <p>Netlify: <a href={project.netlify}>{project.netlify}</a></p>
+            )}
+            {project.github && (
+              <p>GitHub: <a href={project.github}>{project.github}</a></p>
+            )}
+            {/* You can add more details from the project objects */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
