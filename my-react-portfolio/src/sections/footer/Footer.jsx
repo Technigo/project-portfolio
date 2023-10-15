2676f415230655627552204574d3b75b9619c1a2
Sheryll-Ann <shey.pc@gmail.com>
2023-10-15 23:05:49 +0200

    styled Footer component + scroll-text

--- my-react-portfolio/src/sections/footer/Footer.jsx
+++ my-react-portfolio/src/sections/footer/Footer.jsx
@@ -1,7 +1,9 @@
 import { CircleImage } from "../../reusableComponents/images/CircleImage"
 import { SectionTitle } from "../../reusableComponents/texts/SectionTitle"
 import BtnGithub from "/icons/BtnGithub.png"
+import BtnGithubHover from "/icons/BtnGithubHover.png"
 import BtnLinkedin from "/icons/BtnLinkedin.png"
+import BtnLinkedinHover from "/icons/BtnLinkedinHover.png"
 
 import "./Footer.css"
 
@@ -9,7 +11,7 @@ export const Footer = () => {
     const footerTitle = "Let's talk"
 
     return (
-        <div>
+        <div className="contact-info">
             <SectionTitle sectionTitletext={footerTitle} />
             <CircleImage />
             <h2>
@@ -19,10 +21,38 @@ export const Footer = () => {
                     <li>sheryll@mail.com</li>
                 </ul>
             </h2>
-            <img src={BtnGithub} alt="" />
-            <img src={BtnLinkedin} alt="" />
+            <div className="footer-links">
+                <a
+                    href='https://github.com/Sheryll-Ann'
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="link to github account"
+                >
+                    <button type="button">
+                        <img className="btn-github" src={BtnGithub} alt="github image" />
+                        <img className="btn-github-hover" src={BtnGithubHover} alt="github image hovered" />
+                        <span></span>
+                    </button>
+                </a>
+                <a
+                    href='#'
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="link to Linkedin"
+                >
+                    <button type="button">
+                        <img className="btn-linkedin" src={BtnLinkedin} alt="linkedin image" />
+                        <img className="btn-linkedin-hover" src={BtnLinkedinHover} alt="linkedin image hovered" />
+                        <span></span>
+                    </button>
+                </a>
+            </div>
             <nav>
-                <p>Sheryll Poo-Cheong . Front-end Developer . </p>
+                <div className="scroll-text">
+                    <p>Sheryll PC · Front-End Developer · </p>
+                    <p>Sheryll PC · Front-End Developer · </p>
+                    <p>Sheryll PC · Front-End Developer  </p>
+                </div>
             </nav>
         </div>
     )
