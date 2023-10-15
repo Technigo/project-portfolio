c4d2135d8ad6e5e8c50c01064bc9589f0f4750f8
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-10-15 22:55:04 +0200

    add projects, skills and tech

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,20 +3,21 @@ import Button from "../button/Button";
 import Photo from "../photo/Photo";
 import Tag from "../tag/Tag";
 import data from "./data.json";
+import Container from "../container/Container";
 
 const Projects = () => {
     return (
-        <div className="container mx-auto py-40 px-8">
-            <h2 className="text-center md:text-left text-6xl font-bold mb-16 text-blue-700">{data.title}</h2>
+        <Container className="bg-white">
+            <h2 className="text-center md:text-left text-6xl font-bold mb-16 text-technigo-blue">{data.title}</h2>
             {data.projects.map((project, index) => (
-                <div key={index} className="flex gap-8 flex-col md:flex-row">
+                <div key={index} className="flex gap-8 flex-col md:flex-row mb-8 md:items-center">
                     <Photo
                         src={project.imageUrl}
                         alt={project.imageAlt}
-                        className="w-full md:w-40"
-                        color="blue"
+                        className="w-full md:w-1/3"
+                        color="border-technigo-blue"
                     />
-                    <div>
+                    <div className="md:w-2/3">
                         <h3 className="text-3xl font-semibold mb-4">{project.name}</h3>
                         <p className="mb-4">{project.description}</p>
                         <div className="mb-8">
@@ -26,20 +27,28 @@ const Projects = () => {
                                 </Tag>
                             ))}
                         </div>
-                        <div className="flex place-content-between">
-                            <Button href={project.demoUrl} className="bg-gray-100">
-                                <IconWorld className="my-2 sh-8 w-8 inline-block" />
+                        <div className="flex flex-col md:flex-row gap-4">
+                            <Button
+                                href={project.demoUrl}
+                                ariaLabel="Live Demo"
+                                className="w-fit bg-gray-100 hover:bg-technigo-blue hover:text-white"
+                            >
+                                <IconWorld className="h-8 w-8 inline-block mr-2" />
                                 <span className="font-semibold align-middle">Live Demo</span>
                             </Button>
-                            <Button href={project.githubUrl} className="bg-gray-100">
-                                <IconBrandGithub className="my-2 sh-8 w-8 inline-block" />
+                            <Button
+                                href={project.githubUrl}
+                                ariaLabel="View the code"
+                                className="w-fit bg-gray-100 hover:bg-technigo-light-pink hover:text-white"
+                            >
+                                <IconBrandGithub className="h-8 w-8 inline-block mr-2" />
                                 <span className="font-semibold align-middle">View the code</span>
                             </Button>
                         </div>
                     </div>
                 </div>
             ))}
-        </div>
+        </Container>
     );
 }
 
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,22 +1,35 @@
+import Container from "../container/Container";
 import data from "./data.json";
 
 const Skills = () => {
+    const colors = [
+        "bg-technigo-light-pink",
+        "bg-technigo-light-blue",
+        "bg-technigo-green",
+        "bg-technigo-light-yellow"
+    ];
+
+    const text = [
+        "text-white",
+        "text-white",
+        "text-white",
+        "text-technigo-blue",
+    ];
+
     return (
-        <div className="bg-blue-700">
-            <div className="container mx-auto py-40 px-8 md:px-40">
-                <h2 className="text-6xl font-bold text-white text-center mb-16">{data.title}</h2>
-                <div className="grid gap-8 text-center md:text-left grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
-                    {data.categories.map((category) => (
-                        <div key={category.title}>
-                            <h3 className="text-xl font-semibold inline-block px-1 text-white mb-4 bg-red-400">{category.title}</h3>
-                            {category.skills.map((skill) => (
-                                <p key={skill} className="text-white mb-1 pl-1">{skill}</p>
-                            ))}
-                        </div>
-                    ))}
-                </div>
+        <Container className="bg-technigo-blue">
+            <h2 className="text-6xl font-bold text-white text-center mb-16">{data.title}</h2>
+            <div className="grid gap-8 text-center md:text-left grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+                {data.categories.map((category, index) => (
+                    <div key={category.title}>
+                        <h3 className={`text-2xl font-medium inline-block px-1 ${text[index % 4]} mb-4 ${colors[index % 4]}`}>{category.title}</h3>
+                        {category.skills.map((skill) => (
+                            <p key={skill} className="text-white mb-1 pl-1">{skill}</p>
+                        ))}
+                    </div>
+                ))}
             </div>
-        </div>
+        </Container>
     );
 }
 
