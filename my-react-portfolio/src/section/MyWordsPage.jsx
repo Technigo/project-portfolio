a95d7d403b77c4b3b9b7d32091418b4294d019a4
veronica astrom <v.aastrom@gmail.com>
2023-10-15 10:56:02 +0200

    added a wavy divider to the MYWordsPage component

--- my-react-portfolio/src/section/MyWordsPage.jsx
+++ my-react-portfolio/src/section/MyWordsPage.jsx
@@ -1,5 +1,6 @@
 import "./myWordsPage.css";
 import "../components/headline.css";
+import waveDivider from "../assets/wave-divider/waveDivider.png";
 
 import { Headline } from "../components/Headline";
 import { MyWords } from "../components/MyWordsComponent/MyWords";
@@ -10,11 +11,14 @@ import { MyWords } from "../components/MyWordsComponent/MyWords";
 export const MyWordsPage = () => {
     let headerText = "My words";
 
-
     return (
         <section className="my-words-page-wrapper">
-            <Headline className="big-heading pink-h1" headerText={headerText} />
-            <MyWords />
+            {/* // Leaving the alt text since the divider isn't important" */}
+            <img src={waveDivider} alt="" className="wavy-divider" />
+            <div className="my-words-article-content">
+                <Headline className="big-heading pink-h1" headerText={headerText} />
+                <MyWords />
+            </div>
         </section>
     )
 }
