31f4fc14b2293eb002df46fa69fc0070b9ae6581
LauraLyckholm <laura.lyckholm@gmail.com>
2023-10-15 17:25:32 +0200

    Testing to change icon in css instead of via js for better functionality on mobile

--- src/Sections/Projects/ProjectCard.jsx
+++ src/Sections/Projects/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./projects.css";
 import repoImages from "./repoImages.json";
 import { SubHeading } from "../../ReusableComponents/Typography/SubHeading.jsx";
@@ -10,8 +10,8 @@ import { NormalText } from "../../ReusableComponents/Typography/NormalText";
 // Component for each individual project-card. Takes in data as a prop from its parent the FeaturedProjects.jsx where the fetch from Github is made.
 export const ProjectCard = ({ repositories }) => {
     // State that checks if buttons are in hover-state or not. Default is false. 
-    const [isDemoButtonHovered, setIsDemoButtonHovered] = useState(false);
-    const [isGithubButtonHovered, setIsGithubButtonHovered] = useState(false);
+    // const [isDemoButtonHovered, setIsDemoButtonHovered] = useState(false);
+    // const [isGithubButtonHovered, setIsGithubButtonHovered] = useState(false);
 
     // Declaring a variable that uses the images to find the relevant repositories. If the name in the image-json is in the list of repos in the Fetch it will be saved in the filteredRepos. 
     const filteredRepos = repositories.filter((repo) => {
@@ -20,7 +20,7 @@ export const ProjectCard = ({ repositories }) => {
 
     return (
         <>
-            {filteredRepos.map((repo, index) => {
+            {filteredRepos.map((repo) => {
 
                 const repoImage = repoImages.find((img) => img.repoName === repo.name);
                 const topics = repo.topics || []; // Saving repo.topics in a variable and sets an empty array as a default
@@ -51,24 +51,26 @@ export const ProjectCard = ({ repositories }) => {
                                     btnURL={repo.homepage}
                                     imgAlt={"Web icon"}
                                     /* Checks if the state of the button is hovered, and then change the icon url, otherwise leaves the one that is default */
-                                    iconURL={isDemoButtonHovered[index] ? '/icons/globe-white.svg' : '/icons/globe.svg'}
+                                    // iconURL={isDemoButtonHovered[index] ? '/icons/globe-white.svg' : '/icons/globe.svg'}
+                                    iconURL={"/icons/globe.svg"}
                                     btnText={"Live demo"}
-                                    /* The methods onMouseEnter and onMouseLeave states to true and false depending on the pointer*/
-                                    onTouchStart={() => setIsDemoButtonHovered(index, true)} // onTouchStart for mobile devices where there is no hover
-                                    onTouchEnd={() => setIsDemoButtonHovered(index, false)}   // onTouchEnd for mobile devices where there is no hover
-                                    onMouseEnter={() => setIsDemoButtonHovered({ ...isDemoButtonHovered, [index]: true })}
-                                    onMouseLeave={() => setIsDemoButtonHovered({ ...isDemoButtonHovered, [index]: false })}
+                                /* The methods onMouseEnter and onMouseLeave states to true and false depending on the pointer*/
+                                // onTouchStart={() => setIsDemoButtonHovered(index, true)} // onTouchStart for mobile devices where there is no hover
+                                // onTouchEnd={() => setIsDemoButtonHovered(index, false)}   // onTouchEnd for mobile devices where there is no hover
+                                // onMouseEnter={() => setIsDemoButtonHovered({ ...isDemoButtonHovered, [index]: true })}
+                                // onMouseLeave={() => setIsDemoButtonHovered({ ...isDemoButtonHovered, [index]: false })}
                                 />
                                 <Button
                                     className={"github-btn"}
                                     btnURL={repo.html_url}
                                     imgAlt={"Github icon"}
-                                    iconURL={isGithubButtonHovered[index] ? '/icons/github-white.svg' : '/icons/github-darkgray.svg'}
+                                    // iconURL={isGithubButtonHovered[index] ? '/icons/github-white.svg' : '/icons/github-darkgray.svg'}
+                                    iconURL={"/icons/github-darkgray.svg"}
                                     btnText={"View the code"}
-                                    onTouchStart={() => setIsGithubButtonHovered(index, true)} // Touch start event
-                                    onTouchEnd={() => setIsGithubButtonHovered(index, false)}   // Touch end event
-                                    onMouseEnter={() => setIsGithubButtonHovered({ ...isGithubButtonHovered, [index]: true })}
-                                    onMouseLeave={() => setIsGithubButtonHovered({ ...isGithubButtonHovered, [index]: false })}
+                                // onTouchStart={() => setIsGithubButtonHovered(index, true)} // Touch start event
+                                // onTouchEnd={() => setIsGithubButtonHovered(index, false)}   // Touch end event
+                                // onMouseEnter={() => setIsGithubButtonHovered({ ...isGithubButtonHovered, [index]: true })}
+                                // onMouseLeave={() => setIsGithubButtonHovered({ ...isGithubButtonHovered, [index]: false })}
                                 />
                             </div>
                         </div>
