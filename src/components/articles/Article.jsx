086ca550aac6abbc7e499cb8e35b42952ab2c970
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-10-15 22:54:06 +0200

    Try fixing squiggle on articles

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -16,15 +16,19 @@ const Article = ({
             <Photo
                 src={imageUrl}
                 alt={imageAlt}
-                color="#FF4575"
+                color="border-technigo-pink"
                 className="mb-6"
             />
             <Tag>{createdAt}</Tag>
             <h3 className="mt-2 mb-4 text-2xl font-semibold">{title}</h3>
             <p className="mb-6">{preview}</p>
             <div className="flex">
-                <Button href={articleUrl} className="bg-gray-100">
-                    <IconArticle className="my-2 sh-8 w-8 inline-block" />
+                <Button
+                    href={articleUrl}
+                    ariaLabel="Read more"
+                    className="bg-white hover:bg-technigo-yellow"
+                >
+                    <IconArticle className="h-8 w-8 inline-block mr-2" />
                     <span className="font-semibold align-middle">Read more</span>
                 </Button>
             </div>
