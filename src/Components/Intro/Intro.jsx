faa82d5560fa509e3147321f597d129df89e3408
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-10-15 23:31:13 +0200

    Changing media queries

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -7,27 +7,32 @@ import "./Intro.css";
 
 export const Intro = () => {
   return (
-    <section className="intro-wrapper">
-      <ProfilePicture />
-      <div className="info-text">
-        <div>
+    <section className="intro-section">
+      <div className="intro-wrapper">
+        <div className="photo-wrapper">
+          <ProfilePicture />
+        </div>
+
+        <div className="header-wrapper">
           <Heading3 text={"Hi, I'm Susanne Ekenheim"} />
           <Heading1 className={"info-h1"} text={"Frontend Developer"} />
         </div>
-        <p>
-          Susanne is a budding developer with a genuine interest in innovative
-          solutions and a strong desire to refine her coding skills. With her
-          extensive experience as a manager in customer service and a Bachelor's
-          degree in social work, she brings a unique perspective to her work. In
-          addition, Susanne is particularly enthusiastic about incorporating
-          accessibility features into her projects, ensuring inclusivity and
-          usability for all users. In her current role, she serves as a vital
-          link between customers and technology, utilizing her skills to develop
-          user-friendly applications and tackle complex challenges. Her
-          unwavering commitment to maintaining high standards underscores her
-          value as a contributing member of any project, distinguishing her in
-          the technology field.
-        </p>
+        <div className="info-text">
+          <p>
+            Susanne is a budding developer with a genuine interest in innovative
+            solutions and a strong desire to refine her coding skills. With her
+            extensive experience as a manager in customer service and a
+            Bachelor's degree in social work, she brings a unique perspective to
+            her work. In addition, Susanne is particularly enthusiastic about
+            incorporating accessibility features into her projects, ensuring
+            inclusivity and usability for all users. In her current role, she
+            serves as a vital link between customers and technology, utilizing
+            her skills to develop user-friendly applications and tackle complex
+            challenges. Her unwavering commitment to maintaining high standards
+            underscores her value as a contributing member of any project,
+            distinguishing her in the technology field.
+          </p>
+        </div>
       </div>
       <div className="arrow">
         <img src="/arrow_down.svg" />
