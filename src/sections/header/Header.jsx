ae61f7df7c3a6f432b326bb5cc7d143e0734d6ec
mariateresepettersson <mariateresepettersson>
2023-10-16 20:44:22 +0100

    Debugging case sensitive header css

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,7 @@
-import './Header.css'
+import './header.css'
 import { Heading } from '/src/components/typography/Heading.jsx';
 import { Image } from '/src/components/images/Image.jsx';
 import { Paragraph } from '/src/components/typography/Paragraph.jsx';
-import arrowImage from '/src/assets/arrow.svg'
 
 const introText = "Maria is a versatile professional at the intersection of UX design and frontend development. Her diverse background in finance, coupled with international experience, equips her with a unique perspective. She has a deep-rooted passion for technology with the mission to create seamless, user-centric digital solutions that bridge the gap between finance and innovation.";
 
