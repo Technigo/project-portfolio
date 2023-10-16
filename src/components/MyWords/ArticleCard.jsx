c812ac349157596ada55f26f26f0a04bf925d7d3
jennylarsen <jennymarikalarsen@gmail.com>
2023-10-16 09:26:01 +0200

    test for deploy

--- src/components/MyWords/ArticleCard.jsx
+++ src/components/MyWords/ArticleCard.jsx
@@ -6,7 +6,6 @@ import "./articlecard.css";
 
 // Import an array of article data from the JSON file.
 //import articles from "../data/articles.json";
-//import articles from "../data/articles.json";
 import articles from "../data/articles.json";
 
 export const ArticleCard = () => {
