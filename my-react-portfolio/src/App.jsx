bcdcb7d0e76b81dae9d375f882800ba5d13c0a07
mariateresepettersson <mariateresepettersson>
2023-10-16 10:37:23 +0100

    Testing to deploy

---
+++ my-react-portfolio/src/App.jsx
@@ -0,0 +1,7 @@
+import { Body } from './sections/Body';
+
+export const App = () => {
+  return <div className='app-container'>
+    <Body />
+  </div>;
+};
---
+++ my-react-portfolio/src/sections/articles/Article.jsx
@@ -0,0 +1,27 @@
+import './article.css';
+import { Heading } from '/src/components/typography/Heading.jsx';
+import { ArticleCard } from './ArticleCard.jsx';
+import { articles } from './articlesData'; // Import the articles array
+
+export const Article = () => {
+  return (
+
+    <article className="article-container">
+      <article className="article-wrapper">
+        <Heading level={1} text="My Words" className="article-heading" />
+        <div className="article-list">
+          {articles.map((article, index) => (
+            <ArticleCard
+              key={index}
+              title={article.title}
+              imageUrl={article.imageUrl}
+              tagText={article.tagText}
+              description={article.description}
+              link={article.link}
+            />
+          ))}
+        </div>
+      </article>
+    </article>
+  );
+};
