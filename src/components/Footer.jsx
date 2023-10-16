fb349b261107b3214f33463be9676ac815990bcc
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-16 12:36:40 +0200

    removed rollerbanner due to deploy issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './footer.css';
-import RollingBanner from './RollingBanner.jsx';
 
 function Footer() {
     return (
         <footer className="footer-banner">
             <div className="container">
-                <RollingBanner />
             </div>
         </footer>
     );
