830f8ab9c45a324cb89523b19198daaa2280ecf9
mirelcac <141237662+mirelcac@users.noreply.github.com>
2023-10-16 03:10:27 +0200

    trying to deploy

--- my-react-portfolio/vite.config.js
+++ my-react-portfolio/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Specify the MIME type for JavaScript modules
+    mimeTypes: {
+      'application/javascript': ['js', 'jsx']
+    }
+  }
 })
