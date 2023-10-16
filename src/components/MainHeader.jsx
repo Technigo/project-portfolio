e1f1a98aa05697c357da5fbcc6720e849de7d6bc
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-16 22:49:11 +0200

    header styling

--- src/components/MainHeader.jsx
+++
@@ -1,5 +0,0 @@
-import "./mainheader.css";
-
-export const mainheader = ({ className, text }) => {
-    return <h2 className={`${className}`}>{text}</h2>;
-};
--- src/components/SubHeader.jsx
+++
@@ -1,5 +0,0 @@
-import "./subheader.css";
-
-export const subheader = ({ className, text }) => {
-    return <h3 className={`${className}`}>{text}</h3>;
-};
