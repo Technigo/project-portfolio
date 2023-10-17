45b160356b70e9f65d46c3c2c3e461afe4ffe0c6
mariateresepettersson <mariateresepettersson>
2023-10-17 18:33:21 +0100

    Styled skill section to be responsive

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,8 +1,20 @@
+import { Heading } from '/src/components/typography/Heading.jsx';
+import { SkillCard } from './SkillCard.jsx';
+import './skills.css'
+
 export const Skills = () => {
 
     return (
-        <section>
-            <h2>Skills</h2>
+        <section className='skills-container'>
+            <Heading
+                level={1}
+                text="Skills"
+                aria-label="Skills section label"
+                className="skills-heading"
+            />
+            <section className='skills-wrapper'>
+                <SkillCard />
+            </section>
         </section>
     );
 };
