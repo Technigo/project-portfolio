c4d9a39c8ae1343b88272321bbae73b4f8be59a0
Lisa Dahlkar <lisadahlkar@macbook-pro.home>
2023-10-19 13:53:24 +0200

    intro update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
     return (
         <div className="intro-content">
             <img className="img-me" src="Lisa.JPG" alt="A picture of Lisa Dahlkar" />
-            <p><br ></br>As a soon-to-be 30-year-old junior Front End Developer with a diverse background in logistics, optics, and retail, I am excited to embark on a journey back to my childhood and teenage passion: coding. Despite my professional experience in logistics, optics, and retail, I have always harbored a deep fascination for the world of programming. Now, I am committed to turning this passion into a fulfilling career as a front-end developer.</p>
+            <p><br ></br>As a soon-to-be 30-year-old junior Frontend Developer with a diverse background in logistics, optics, and retail, I am excited to embark on a journey back to my teenage passion: coding. Despite my professional experience in logistics, optics, and retail, I have always harbored a deep fascination for the world of programming. Now, I am committed to turning this passion into a fulfilling career as a front-end developer.</p>
         </div>
 
     );
