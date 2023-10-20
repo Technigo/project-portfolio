7e63457a23cf374ad1a5575fc0a47fcb8ebc1148
FridafridaL <fridus.lindskog@gmail.com>
2023-10-20 17:13:14 +0200

    css

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -2,13 +2,35 @@ import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>
+    <div className="footer">
+      {/* <span className="rolling-name primary">
+        Andrej Kavržić · Frontend Developer · Andrej Kavržić · Frontend
+        Developer ·
+      </span> */}
+      <div className="scrolling-text primary">
+        <p className="scroll-text ">
           Frida Lindskog · Frontend Developer · Frida Lindskog · Frontend
-          Developer ·
+          Developer · Frida Lindskog · Frontend Developer · Frida Lindskog ·
+          Frontend Developer · Frida Lindskog · Frontend Developer · Frida
+          Lindskog · Frontend Developer · Frida Lindskog · Frontend Developer ·
+          Frida Lindskog · Frontend Developer · Frida Lindskog · Frontend
+          Developer · Frida Lindskog · Frontend Developer · Frida Lindskog ·
+          Frontend Developer · Frida Lindskog · Frontend Developer · Frida
+          Lindskog · Frontend Developer ·
         </p>
       </div>
-    </footer>
+      <div className="scrolling-text secondary">
+        <p className="scroll-text ">
+          Frida Lindskog · Frontend Developer · Frida Lindskog · Frontend
+          Developer · Frida Lindskog · Frontend Developer · Frida Lindskog ·
+          Frontend Developer · Frida Lindskog · Frontend Developer · Frida
+          Lindskog · Frontend Developer · Frida Lindskog · Frontend Developer ·
+          Frida Lindskog · Frontend Developer · Frida Lindskog · Frontend
+          Developer · Frida Lindskog · Frontend Developer · Frida Lindskog ·
+          Frontend Developer · Frida Lindskog · Frontend Developer · Frida
+          Lindskog · Frontend Developer ·
+        </p>
+      </div>
+    </div>
   );
 };
