727e39957cfa25d3953b93533c6f53aec0f50ae1
jennylarsen <jennymarikalarsen@gmail.com>
2023-10-21 21:45:27 +0200

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Articles } from './Sections/Articles/Articles'
 import { FeaturedProjects } from './Sections/FeaturedProjects/FeaturedProjects'
+import { Footer } from './Sections/Footer/Footer'
+import { NameBanner } from './Sections/Footer/NameBanner'
 import { Header } from './Sections/Header/Header'
 import { Skills } from './Sections/Skills/Skills'
 import { Tech } from './Sections/Tech/Tech'
@@ -13,6 +15,8 @@ return (
     <FeaturedProjects />
     <Articles />
     <Skills />
+    <Footer />
+    <NameBanner />
      
 
     
---
+++ src/Sections/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import "./footer.css";
+import { ImageComponent } from "../../ReusableComp/ImageElements/ImageComp";
+import { MainHeading } from "../../ReusableComp/Typo/MainHeading";
+import { SubHeading } from "../../ReusableComp/Typo/SubHeading";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <MainHeading text={"Let's talk"} />
+            <ImageComponent sectionClassName={"profile-image"} elementClassName={"circle-img"} link={"/profilepic.jpg"} ImageAltText={"Image of Jenny"} />
+            <div className="contact-info">
+                <SubHeading text={"Jenny Larsen"} />
+                <a aria-label="Phone number" href="tel:+0727402484">
+                    <SubHeading text={"+46(0)72 74 02 484"} />
+                </a>
+                <a aria-label="E-mail adress" href="mailto:jennymarikalarsen@gmail.com">
+                    <SubHeading text={"jennymarikalarsen@gmail.com"} />
+                </a>
+            </div>
+            <div className="icons">
+                <a href="https://www.linkedin.com/in/jenny-larsen-b51140219/">
+                    <ImageComponent sectionClassName={"icon-box"} elementClassName={"icon"} link={"/icons/linkedin=default.svg"} ImageAltText={"Link to LinkedIn"} />
+                </a>
+                <a href="https://github.com/jennylarsen">
+                    <ImageComponent sectionClassName={"icon-box"} elementClassName={"icon"} link={"/icons/github=default.svg"} ImageAltText={"Link to GitHub"} />
+                </a>
+            </div>
+        </footer>
+    )
+}
