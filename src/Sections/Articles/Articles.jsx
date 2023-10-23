1ee28b264d489dc3de2216ec6178b2cfc99dcb3c
jennylarsen <jennymarikalarsen@gmail.com>
2023-10-23 09:27:23 +0200

    wavy-line completed and styled

--- src/Sections/Articles/Articles.jsx
+++ src/Sections/Articles/Articles.jsx
@@ -5,12 +5,15 @@ import { ArticleCard } from "./ArticleCard";
 
 export const Articles = () => {
     return (
+        <>
+        
         <section id="articles-section" className="articles-section">
-             <div className="wavy-line-wrapper"></div>
+        <div className="wavy-line-wrapper"></div>
             <MainHeading className={"articles-heading"} text={"My words"} />
             <div className="article-cards-section">
                 <ArticleCard data={data} />
             </div>
         </section>
+        </>
     )
 }
