cb35e69800874e702bcdd1c01faf335a8e11ba1f
Carolina Luna <kroluna@gmail.com>
2023-10-23 13:29:59 +0200

    cffgh

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { Header } from "./sections/Header";
 import { Tech } from "./sections/Tech";
 import { Projects } from "./sections/Projects";
-import { Button } from "./components/Button";
+
 import { Footer } from "./sections/Footer";
 
 export const App = () => {
