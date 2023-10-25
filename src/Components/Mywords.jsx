08e5030550c00297f214f0992e702c7988498ec3
Idah Collin <idah_collin@hotmail.com>
2023-10-25 19:49:40 +0200

    Font on My words,minor styling

--- src/Components/Mywords.jsx
+++ src/Components/Mywords.jsx
@@ -11,7 +11,7 @@ export const Mywords = () => {
                 <div className="article1">
                     <img src="/assets/coding.jpg" alt="Photo of coding" />
                     <div className="date">Okt 2023</div>
-                    <h3>AI-Generated Insights: The Future of Coding</h3>
+                    <h3>AI-Generated</h3>
                     <p>In this AI-generated article, we explore the cutting-edge advancements in artificial intelligence and how they are revolutionizing the world of coding. From automated code generation to intelligent code optimization, we delve into how AI is shaping the future of software development and making coding more efficient and accessible.</p>
                     <button type="button">Read article</button>
                 </div>
@@ -19,19 +19,19 @@ export const Mywords = () => {
                 <div className="article2">
                     <img src="/assets/coding.jpg" alt="Photo of coding" />
                     <div className="date">Okt 2023</div>
-                    <h3>AI-Generated Insights: The Future of Coding</h3>
+                    <h3>AI-Generated</h3>
                     <p>In this AI-generated article, we explore the cutting-edge advancements in artificial intelligence and how they are revolutionizing the world of coding. From automated code generation to intelligent code optimization, we delve into how AI is shaping the future of software development and making coding more efficient and accessible.</p><button type="button">Read article</button></div>
 
                 <div className="article3">
                     <img src="/assets/coding.jpg" alt="Photo of coding" />
                     <div className="date">Okt 2023</div>
-                    <h3>AI-Generated Insights: The Future of Coding</h3>
+                    <h3>AI-Generated</h3>
                     <p>In this AI-generated article, we explore the cutting-edge advancements in artificial intelligence and how they are revolutionizing the world of coding. From automated code generation to intelligent code optimization, we delve into how AI is shaping the future of software development and making coding more efficient and accessible.</p><button type="button">Read article</button></div>
 
                 <div className="article4">
                     <img src="/assets/coding.jpg" alt="Photo of coding" />
                     <div className="date">Okt 2023</div>
-                    <h3>AI-Generated Insights: The Future of Coding</h3>
+                    <h3>AI-Generated</h3>
                     <p>In this AI-generated article, we explore the cutting-edge advancements in artificial intelligence and how they are revolutionizing the world of coding. From automated code generation to intelligent code optimization, we delve into how AI is shaping the future of software development and making coding more efficient and accessible.</p><button type="button">Read article</button></div>
             </div>
         </div>
