6bdbdca6ebf05f8cf816fbae1bd6a76167af9df2
FridafridaL <fridus.lindskog@gmail.com>
2023-11-06 18:06:54 +0100

    styling

--- src/Sections/Intro.jsx
+++ src/Sections/Intro.jsx
@@ -1,20 +1,24 @@
 import "./Intro.css";
 //import { Headline } from "./Headline";
-import jag from "../assets/jag1.png";
+import jag from "../assets/jag5.png";
 import arrow from "../assets/Position=down.png";
 
 export const Intro = () => {
   return (
-    <div className="intro-container">
-      <img src={jag} alt="Image" className="centered"></img>
-      <h2>Hi, I'm Frida Lindskog</h2>
-      <h1>Frontend Developer</h1>
-      <p>
-        some text bla vla bla bla bla bla bla bla blsome text bla vla bla bla
-        bla bla bla bla bsome text bla vla bla bla bla bla bla bla bsome text
-        bla vla bla bla bla bla bla bla b
-      </p>
-      <img src={arrow} alt="arrowicon" className="arrow-icon"></img>
-    </div>
+    <>
+      <div className="intro-container">
+        <img src={jag} alt="Image" className="centered" />
+        <h2>Hi, I'm Frida Lindskog</h2>
+        <h1>Frontend Developer</h1>
+        <p>
+          some text bla vla bla bla bla bla bla bla blsome text bla vla bla bla
+          bla bla bla bla bsome text bla vla bla bla bla bla bla bla bsome text
+          bla vla bla bla bla bla bla bla b
+        </p>
+      </div>
+      <div className="arrow-icon">
+        <img src={arrow} alt="arrowicon" />
+      </div>
+    </>
   );
 };
--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,19 +1,6 @@
 import "./LinkButton.css";
 import React, { useState } from "react";
 
-// export const LinkButton = ({ buttonText, url, icon }) => {
-//   const handleClick = () => {
-//     console.log(`Clicked ${buttonText}`);
-//     window.open(url, "_blank");
-//   };
-
-//   return (
-//     <button onClick={handleClick} className="link-button">
-//       {icon && <img src={icon} alt="Icon" />}
-//       {buttonText}
-//     </button>
-//   );
-// };
 export const LinkButton = ({ buttonText, url, icon, hoverIcon, alt }) => {
   const [isHovered, setIsHovered] = useState(false);
 
