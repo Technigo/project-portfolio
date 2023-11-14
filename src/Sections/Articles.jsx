cdf7ea593401ecb419eaa999474989495297f56a
FridafridaL <fridus.lindskog@gmail.com>
2023-11-14 08:25:33 +0100

    projects

--- src/Sections/Articles.jsx
+++ src/Sections/Articles.jsx
@@ -2,11 +2,19 @@ import { Headline } from "./Headline";
 import articles from "../../articles.json";
 import { ArticleCard } from "../components/ArticleCard";
 import "./Articles.css";
+import dividerPath from "../assets/divider.svg";
 
 export const Articles = ({ name, photo, tags, text }) => {
+  const imgStyle = {
+    fill: "blue", // Set your desired color here
+    width: "100%",
+  };
+
   return (
     <>
-      <div
+      <img src={dividerPath} style={imgStyle} alt="Divider" />
+
+      {/* <div
         className="wave"
         style={{
           width: "100%",
@@ -14,10 +22,10 @@ export const Articles = ({ name, photo, tags, text }) => {
           padding: "14px",
           marginTop: "10px",
         }}
-      />
+      /> */}
 
       <div className="articles">
-        {/* <img src="/src/assets/path.png" className="path"></img> */}
+        {/* <img src={divider} className="divider"></img> */}
         <Headline text="My words" level={1} />
         <div className="article-grid">
           {articles.articles.map((articles) => (
