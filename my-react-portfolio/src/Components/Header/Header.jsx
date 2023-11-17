f5184195d5f2454e2bcb1af30c5c63cc7a997ff5
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-11-17 09:39:39 +0100

    improved the styled

--- my-react-portfolio/src/Components/Header/Header.jsx
+++ my-react-portfolio/src/Components/Header/Header.jsx
@@ -7,30 +7,29 @@ import "./Header.css"
 // Component for the introduction section.
 
 export const Header = () => {
-  
-const aboutMe = "After completing my studies and gaining experience as a steel construction technologist with SolidWorks and CAD design, I decided to change my career and started learning web development. Currently studying at Technigo for Web Development Bootcamp."
+
+  const aboutMe = "After completing my studies and gaining experience as a steel construction technologist with SolidWorks and CAD design, I decided to change my career and started learning web development. Currently studying at Technigo for Web Development Bootcamp."
   return (
-  <>
+    <>
       <header>
         <div className="header-wrapper">
-               <div className="image-content">
-                    <div className="image-cropper">
-                    <MyPhoto className="person-img"/>
-                    </div>
+          <div className="image-content">
+            <div className="image-cropper">
+              <MyPhoto className="person-img" />
+            </div>
           </div>
           <section className="heading-wrapper">
-      <p className="header-paragraph">Hi, I &apos; m Klaudia Wróblewska </p>
-          <CardHeading className="green-heading" text="Frontend Developer" />
-        </section>
-            <Description className="header-description" text={aboutMe} />
+            <p className="header-paragraph">Hi, I &apos; m Klaudia Wróblewska </p>
+            <CardHeading className="green-heading" text="Frontend Developer" />
+          </section>
+          <Description className="header-description" text={aboutMe} />
         </div>
         <div className="arrow">
-        <img src="./arrow.svg" alt="arrow" className="arrow" />
+          <img src="./arrow.svg" alt="arrow" className="arrow" />
         </div>
       </header>
-    
+
     </>
   )
 }
 
- 
