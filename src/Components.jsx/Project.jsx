406da07fbc192293139bf502cb6c83d28a58bf2c
Emmy Jansson <emmybengtsson@live.se>
2023-12-04 19:27:40 +0100

    some changes

--- src/Components.jsx/Project.jsx
+++ src/Components.jsx/Project.jsx
@@ -4,37 +4,38 @@ import { Tag } from "./Tag";
 import projectsData from "./projects.json";
 import "./Project.css";
 
-export const Project = ({ project }) => {
+export const Project = ({ projects }) => {
+  console.log(projectsData);
   const { name, image, description, tags, github_url, deployed_page_url } =
-    project;
+    projects;
   return (
     <>
       <div className="project-container">
-        {/* <img className="project-pic" src={image} alt={name} /> */}
+        <h1>Projects</h1>
 
-        <div className="project-text">
-          <h4>{name}</h4>
-          <p>{description}</p>
-
-          {/* <div className="tag-container">
-            {tags.map((item) => (
-              <Tag key={item} tag={item} />
-            ))}
-          </div> */}
-
-          <div className="button-container">
-            <LinkButton
-              url={deployed_page_url}
-              text="Live demo"
-              hovColor="netlify"
-            />
-            <LinkButton
-              url={github_url}
-              text="View the code"
-              hovColor="github"
-            />
+        {projects.map((project, index) => (
+          <div className="project-text" key={index}>
+            <p>{name}</p>
+            <p>{description}</p>
+            {/* <div className="tag-container">
+              {tags.map((item) => (
+                <Tag key={item} tag={item} />
+              ))}
+            </div> */}
+            <div className="button-container">
+              <LinkButton
+                url={deployed_page_url}
+                text="Live demo"
+                hovColor="netlify"
+              />
+              <LinkButton
+                url={github_url}
+                text="View the code"
+                hovColor="github"
+              />
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
