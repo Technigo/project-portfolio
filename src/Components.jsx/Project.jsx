190dce44b9cdec36f8da02893703bac633978847
Emmy Jansson <emmybengtsson@live.se>
2023-12-07 14:45:21 +0100

    finally got my projects into my portfolio!

--- src/Components.jsx/Project.jsx
+++ src/Components.jsx/Project.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import { LinkButton } from "./LinkButton";
 import { Tag } from "./Tag";
-import projectsData from "./projects.json";
+import projectsData from "./projects";
 import "./Project.css";
 
-export const Project = ({ projects }) => {
-  console.log(projectsData);
-  const { name, image, description, tags, github_url, deployed_page_url } =
-    projects;
+export const Project = () => {
   return (
     <>
+      <h1>Projects</h1>
       <div className="project-container">
-        <h1>Projects</h1>
+        {projectsData.projects.map((project, index) => {
+          const {
+            name,
+            image,
+            description,
+            tags,
+            github_url,
+            deployed_page_url,
+          } = project;
 
-        {projects.map((project, index) => (
-          <div className="project-text" key={index}>
-            <p>{name}</p>
-            <p>{description}</p>
-            {/* <div className="tag-container">
-              {tags.map((item) => (
-                <Tag key={item} tag={item} />
-              ))}
-            </div> */}
-            <div className="button-container">
+          return (
+            <div className="project-card" key={index}>
+              <h3>{name}</h3>
+              <p>{description}</p>
+              {/* Assuming you have an 'image' property in your project data */}
+              <img src={image} alt={name} />
+              {/* Uncomment the following lines if you have a 'tags' property in your project data */}
+              {/* <div className="tag-container">
+                {tags.map((item) => (
+                  <Tag key={item} tag={item} />
+                ))}
+              </div> */}
+              {/* <div className="button-container"> */}
               <LinkButton
                 url={deployed_page_url}
                 text="Live demo"
@@ -33,9 +42,10 @@ export const Project = ({ projects }) => {
                 text="View the code"
                 hovColor="github"
               />
+              {/* </div> */}
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );
