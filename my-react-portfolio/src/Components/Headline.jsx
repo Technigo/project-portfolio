77ed5a35416ae7ff70895d9a287475c9eecb764f
Linnea Johansson <Linneajoh@gmail.com>
2023-12-28 18:59:14 +0100

    adjust position of headline

--- my-react-portfolio/src/Components/Headline.jsx
+++ my-react-portfolio/src/Components/Headline.jsx
@@ -5,26 +5,24 @@ import './Headline.css';
 const Headline = () => {
   return (
     <div className="headline">
-      <div className="header-and-profile">
-        <div className="header-section">
-          <h1>Hi, I'm Linnea Johansson</h1>
-          <h2>Frontend Developer</h2>
-        </div>
-        <div className="profile-section">
-          <img src={linneaImage} alt="Linnea Johansson" className="profile-image" />
-          <p className="profile-description">
-            Linnea is an exceptional developer known for her innovative solutions and
-            exceptional coding abilities. She creates user-friendly applications and solves
-            complex issues with ease. Her drive for excellence makes her a valuable asset
-            to any project and a standout in the technology field.
-          </p>
-        </div>
+      <h1>Hi, I'm Linnea Johansson</h1>
+      <h2>Frontend Developer</h2>
+      <div className="profile-container">
+        <img src={linneaImage} alt="Linnea Johansson" className="profile-image" />
+        <p className="profile-description">
+          Linnea is an exceptional developer known for her innovative solutions and
+          exceptional coding abilities. She creates user-friendly applications and solves
+          complex issues with ease. Her drive for excellence makes her a valuable asset
+          to any project and a standout in the technology field.
+        </p>
       </div>
     </div>
   );
 };
 
 export default Headline;
+
+
 
 
 
