beab0398c67c495bd44a6c699c0d70caee04cc14
Linnea Johansson <Linneajoh@gmail.com>
2023-12-29 12:47:40 +0100

    arrow

--- my-react-portfolio/src/Components/Headline.jsx
+++ my-react-portfolio/src/Components/Headline.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import linneaImage from '../assets/Linnea.jpg';
+import arrowImage from '../assets/arrow.png';
 import './Headline.css';
 
 const Headline = () => {
@@ -8,19 +9,23 @@ const Headline = () => {
       <h1>Hi, I'm Linnea Johansson</h1>
       <h2>Frontend Developer</h2>
       <div className="profile-container">
-        <img src={linneaImage} alt="Linnea Johansson" className="profile-image" />
-        <p className="profile-description">
-          Linnea is an exceptional developer known for her innovative solutions and
-          exceptional coding abilities. She creates user-friendly applications and solves
-          complex issues with ease. Her drive for excellence makes her a valuable asset
-          to any project and a standout in the technology field.
-        </p>
+        <div className="profile-content"> 
+          <img src={linneaImage} alt="Linnea Johansson" className="profile-image" />
+          <p className="profile-description">
+            Linnea is an exceptional developer known for her innovative solutions and
+            exceptional coding abilities. She creates user-friendly applications and solves
+            complex issues with ease. Her drive for excellence makes her a valuable asset
+            to any project and a standout in the technology field.
+          </p>
+        </div>
+        <img src={arrowImage} alt="Arrow" className="arrow-image" />
       </div>
     </div>
   );
 };
 
 export default Headline;
+
 
 
 
