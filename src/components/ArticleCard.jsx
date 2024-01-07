49e9e860a35eb0214876dcbf54ef856e7971a567
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2024-01-07 11:08:31 +0100

    -m

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './ArticleCard.css';
 
-function ArticleCard({ title, description, imageUrl }) {
+function ArticleCard({ title, description, imageUrl, date }) { // Include 'date' in the props
   return (
     <div className="article-card">
-      <img src={imageUrl} alt="Article" className="article-image" />
+      <img src={imageUrl} alt={title} className="article-image" />
+      <p className="date-label">{date}</p> {/* Display the passed date */}
       <h3 className="article-title">{title}</h3>
       <p className="article-description">{description}</p>
       <button className="read-article-button">Read article</button>
@@ -13,3 +14,4 @@ function ArticleCard({ title, description, imageUrl }) {
 }
 
 export default ArticleCard;
+
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Presentation.css';
 import ArticleCard from './ArticleCard';
-import Article1 from '../assets/Article1.jpg'; 
-import Article2 from '../assets/Article2.jpg'; 
 import SquigglyLine from './squigglyline'; 
+import Article1 from '../assets/Article1.jpg'; // Path to your image
+import Article2 from '../assets/Article2.jpg'; // Path to your image
 
 function Presentation() {
-  const [presentationText, setPresentationText] = useState(
-  );
+  // Define the articles array here
+  const articles = [
+    {
+      id: 1,
+      title: "Article 1",
+      description: "Content to come",
+      imageUrl: Article1,
+      date: "July 2024"
+    },
+    {
+      id: 2,
+      title: "Article 2",
+      description: "Content to come",
+      imageUrl: Article2,
+      date: "August 2024"
+    },
+    // ... more articles
+  ];
 
   return (
     <div className="presentation">
-       <SquigglyLine />
-      <h2 className="presentationh2">My words</h2>
+      <SquigglyLine />
+      <h2 className="presentation-h2">My words</h2>
       <div className="articles-grid">
-        <ArticleCard title="Article 1" description="Content to come" imageUrl={Article1} />
-        {/* Replace the path/to/image2.jpg with the appropriate image URL */}
-        <ArticleCard title="Article 2" description="Content to come" imageUrl={Article2} />
-        {/* Add more ArticleCard components as necessary */}
+        {articles.map(article => (
+          <ArticleCard
+            key={article.id}
+            title={article.title}
+            description={article.description}
+            imageUrl={article.imageUrl}
+            date={article.date} // Pass the date to each ArticleCard
+          />
+        ))}
       </div>
     </div>
   );
 }
 
 export default Presentation;
+
