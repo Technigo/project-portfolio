1e749ce4ed7d59552f43244ba3f1f148d0344c24
Emmy Jansson <emmybengtsson@live.se>
2024-01-14 06:51:09 +0100

    images

--- src/Components.jsx/Contact.jsx
+++ src/Components.jsx/Contact.jsx
@@ -9,7 +9,7 @@ export const Contact = () => {
     <div className="contact">
       <h1>Let's talk!</h1>
       <div className="image">
-        <img src="./assets/me.jpg" alt="" />
+        <img src="me.jpg" alt="" />
       </div>
       <h2>Emmy Jansson</h2>
       <h3>Mobile: +4673-719 77 49</h3>
@@ -18,22 +18,22 @@ export const Contact = () => {
         <LinkButton
           buttonText=""
           url="https://www.instagram.com/emmy.linneaa/"
-          icon="./assets/instagram.png"
+          icon="instagram.png"
         />
         <LinkButton
           buttonText=""
           url="https://www.linkedin.com/in/emmy-jansson-2104a3293/"
-          icon="./assets/linkedin.png"
+          icon="linkedin.png"
         />
         <LinkButton
           buttonText=""
           url="https://stackoverflowteams.com/c/technigo/users/520/?tab=profile"
-          icon="./assets/stackoverflow.png"
+          icon="stackoverflow.png"
         />
         <LinkButton
           buttonText=""
           url="https://github.com/EmmyLJ/"
-          icon="./assets/github.png"
+          icon="github.png"
         />
       </div>
     </div>
--- src/Components.jsx/Links.jsx
+++ src/Components.jsx/Links.jsx
@@ -7,17 +7,17 @@ export const Links = () => {
       <LinkButton
         buttonText="Instagram"
         url="https://www.instagram.com/emmy.linneaa/"
-        icon="./assets/instagram(1).png"
+        icon="instagram(1).png"
       />
       <LinkButton
         buttonText="Linkedin"
         url="https://www.linkedin.com/in/emmy-jansson-2104a3293/"
-        icon="./assets/linkedin.png"
+        icon="linkedin.png"
       />
       <LinkButton
         buttonText=""
         url="https://stackoverflowteams.com/c/technigo/users/520/?tab=profile"
-        icon="./assets/stackoverflow.png"
+        icon="stackoverflow.png"
       />
     </div>
   );
