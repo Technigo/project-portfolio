c5d9476b4e646f93af561cc04a33588621fb25af
BeckieMorton <beckam11@gmail.com>
2024-01-15 13:56:21 +1100

    tech section styling

--- my-react-portfolio/src/Components/TechSection/TechSection.jsx
+++ my-react-portfolio/src/Components/TechSection/TechSection.jsx
@@ -1,13 +1,20 @@
-import "./TechSection.css";
+import styles from "./TechSection.module.css";
 
 export const TechSection = () => {
   return (
-    <div className="tech-section-container">
-      <h1 className="tech-section-heading">Tech</h1>
-      <p className="tech-section-text">
-        HTML, CSS, JavaScript, ES6, JSX, React, React Hooks, Node.js, Express
-        API, Web Accessibility, APIs, mob-programming, pair-programming, GitHub.
-      </p>
-    </div>
+    <>
+      <div className={styles.techContainer}>
+        <div className={styles.leftBox}>
+          <h1>Tech</h1>
+        </div>
+        <div className={styles.rightBox}>
+          <p>
+            HTML, CSS, JavaScript, ES6, JSX, React, React Hooks, Node.js,
+            Express API, Web Accessibility, APIs, mob-programming,
+            pair-programming, GitHub.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
