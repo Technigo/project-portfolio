f2d4cd4f396d68ae3566f79e04e1e67085207409
BeckieMorton <beckam11@gmail.com>
2024-01-16 12:46:22 +1100

    featured projects styling and thumbnails

--- my-react-portfolio/src/Components/FeaturedProjects/FeaturedProjects.jsx
+++ my-react-portfolio/src/Components/FeaturedProjects/FeaturedProjects.jsx
@@ -1,5 +1,7 @@
 import styles from "./FeaturedProjects.module.css";
 
+import { ProjectInfo as data } from "../Data/ProjectInfo";
+
 import { useEffect, useState } from "react";
 
 export const FeaturedProjects = () => {
@@ -45,51 +47,108 @@ export const FeaturedProjects = () => {
   console.log(`myGits`, myGits);
 
   return (
-    <>
-      <div className={styles.projectsHeader}>
-        <h1>Featured Projects</h1>
-      </div>
-      <div className={styles.projectsContainer}>
-        <div className={styles.projectsBox}>
-          <img
-            src="./assets/happy-thoughts.jpg"
-            className={styles.projectsThumbnail}
-          />
+    <div className={styles.outer_containerWrapper}>
+      <div className={styles.wrapper_projects}>
+        <div className={styles.projectsHeader}>
+          <h1>Featured Projects</h1>
         </div>
-        <div className={styles.projectsBox}>
-          <h2>Project Name</h2>
-          <p>info about project</p>
-          <p>
-            <span style={{ backgroundColor: "black", color: "white" }}>
-              &nbsp; HTML5 &nbsp;
-            </span>
-            &nbsp;
-            <span style={{ backgroundColor: "black", color: "white" }}>
-              &nbsp; CSS3 &nbsp;
-            </span>
-            &nbsp;
-            <span style={{ backgroundColor: "black", color: "white" }}>
-              &nbsp; React &nbsp;
-            </span>
-          </p>
-          <p>
-            <a
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.liveDemoButton}
-            >
-              <img src=".\assets\Live-Demo-Button.png" alt="live demo" />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.viewCodeButton}
-            >
-              <img src=".\assets\View-Code-Button.png" alt="view code" />
-            </a>
-          </p>
-        </div>
+        {data.map((project) => (
+          <div key={project.repoName} className={styles.projectContainer}>
+            <img
+              src={project.imagePath}
+              alt={project.displayName}
+              className={styles.projectImage}
+            />
+
+            <div className={styles.textContainer}>
+              <h2 className={styles.displayName}>{project.displayName}</h2>
+
+              <p className={styles.text}>{project.text}</p>
+
+              <ul className={styles.skillsList}>
+                {project.skills.map((skill) => (
+                  <li key={skill}>{skill}</li>
+                ))}
+              </ul>
+
+              <div className={styles.iconContainer}>
+                <a
+                  className={styles.liveDemo}
+                  href={project.gitUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="./assets/Live-Demo-Button.png"
+                    alt="live demo button"
+                  />
+                </a>
+
+                <a
+                  className={styles.viewCode}
+                  href={project.netlifyUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="./assets/View-Code-Button.png"
+                    alt="view code button"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
+
+  // return (
+  //   <>
+  //     <div className={styles.projectsHeader}>
+  //       <h1>Featured Projects</h1>
+  //     </div>
+  //     <div className={styles.projectsContainer}>
+  //       <div className={styles.projectsBox}>
+  //         <img
+  //           src="./assets/happy-thoughts.jpg"
+  //           className={styles.projectsThumbnail}
+  //         />
+  //       </div>
+  //       <div className={styles.projectsBox}>
+  //         <h2>Project Name</h2>
+  //         <p>info about project</p>
+  //         <p>
+  //           <span style={{ backgroundColor: "black", color: "white" }}>
+  //             &nbsp; HTML5 &nbsp;
+  //           </span>
+  //           &nbsp;
+  //           <span style={{ backgroundColor: "black", color: "white" }}>
+  //             &nbsp; CSS3 &nbsp;
+  //           </span>
+  //           &nbsp;
+  //           <span style={{ backgroundColor: "black", color: "white" }}>
+  //             &nbsp; React &nbsp;
+  //           </span>
+  //         </p>
+  //         <p>
+  //           <a
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.liveDemoButton}
+  //           >
+  //             <img src=".\assets\Live-Demo-Button.png" alt="live demo" />
+  //           </a>
+  //           <a
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.viewCodeButton}
+  //           >
+  //             <img src=".\assets\View-Code-Button.png" alt="view code" />
+  //           </a>
+  //         </p>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 };
