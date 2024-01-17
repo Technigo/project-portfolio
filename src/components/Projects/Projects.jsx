c9ed669e91e88d6b768a97216e3526dbee7f8cb9
majoh23 <139242342+majoh23@users.noreply.github.com>
2024-01-17 15:42:48 +0100

    CSS updates

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Headline } from '../Headline/Headline';
-import ProjectsData from '../../../projects.json';
-import { ProjectCard } from '../ProjectCard/ProjectCard';
-import './Projects.css';
+import React from "react";
+import { Headline } from "../Headline/Headline";
+import ProjectsData from "../../../projects.json";
+import { ProjectCard } from "../ProjectCard/ProjectCard";
+import "./Projects.css";
 
 export const Projects = () => {
   return (
     <div className="projects-container">
-    <Headline className="headline" text="Featured projects" level={1} />
-      {ProjectsData.projects.map(project => (
+      <Headline className="headline" text="Featured Projects" level={1} />
+      {ProjectsData.projects.map((project) => (
         <ProjectCard key={project.name} project={project} />
       ))}
     </div>
   );
-}
+};
