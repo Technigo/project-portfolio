e195c16bd8dec188ca45de8d2cf8e868e4f47589
Sandra Gustafsson <sandragustafsson@Mac-mini.local>
2024-01-17 10:19:58 +0100

    updated material

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,74 +1,82 @@
-import './Articles.css';
-import { Buttons } from '../Buttons/Buttons';
-import ReadMoreSVG from '../../assets/note-rounded-square-interface-symbol-svgrepo-com.svg'
-import waveLine from '../../assets/dividerLarge.png';
+import "./Articles.css";
+import { Buttons } from "../Buttons/Buttons";
+import ReadMoreSVG from "../../assets/note-rounded-square-interface-symbol-svgrepo-com.svg";
+import waveLine from "../../assets/dividerLarge.png";
 
 const exampleArticles = [
   {
-      imageUrl: "images/img.png",
-      title: "Emotions during a bootcamp!",
-      preview: "Attending a web developer bootcamp is an emotional rollercoaster. It's a constant....",
-      link: "https://www.linkedin.com/in/sandra-gustafsson-3665061a5/recent-activity/all/",
-      publishDate: "November 24, 2023"
+    imageUrl: "images/img.png",
+    title: "Emotions during a bootcamp!",
+    preview:
+      "Attending a web developer bootcamp is an emotional rollercoaster. It's a constant....",
+    link: "https://www.linkedin.com/posts/sandra-gustafsson-3665061a5_frontend-developer-bootcamp-activity-7130534507249606656-mJbW/?utm_source=share&utm_medium=member_desktop",
+    publishDate: "November 24, 2023",
   },
   {
-      imageUrl: "images/img-2.png",
-      title: "Journey through React",
-      preview: "Dive into React's ecosystem and its paradigms to build robust web applications...",
-      link: "https://www.google.se",
-      publishDate: "October 1, 2023"
+    imageUrl: "images/img-2.png",
+    title: "Exited to share our final project",
+    preview:
+      "After weeks of hard work and collaboration, our team  consisting of myself, Frida, Caro, and Emmy is thrilled to present our.... ",
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7152998899408871425/",
+    publishDate: "January 16, 2024",
   },
   {
-      imageUrl: "images/img-3.png",
-      title: "The Future of Web",
-      preview: "A look into the future trends and technologies that are shaping the web...",
-      link: "https://www.google.se",
-      publishDate: "October 1, 2023"
+    imageUrl: "images/img-3.png",
+    title: "The Future of Web",
+    preview:
+      "A look into the future trends and technologies that are shaping the web...",
+    link: "https://www.google.se",
+    publishDate: "October 1, 2023",
   },
   {
-      imageUrl: "images/img-4.png",
-      title: "CSS Secrets and Tips",
-      preview: "Unravel the secrets of CSS to build beautiful and responsive designs...",
+    imageUrl: "images/img-4.png",
+    title: "CSS Secrets and Tips",
+    preview:
+      "Unravel the secrets of CSS to build beautiful and responsive designs...",
 
-      link: "https://www.google.se",
-      publishDate: "October 1, 2023"
-  }
+    link: "https://www.google.se",
+    publishDate: "October 1, 2023",
+  },
 ];
 
 const ArticleCard = ({ imageUrl, title, preview, link, publishDate }) => {
   return (
-      <div className="article-card">
-          <img src={imageUrl} alt={title} className="article-image" />
-          <span className="publish-date">{publishDate}</span>
-              <h3>{title}</h3>
-              <p>{preview}</p>
-              <Buttons type="read-more" text="Read More" icon={<img src={ReadMoreSVG} alt="Icon" />}href={link} />
-      </div>
+    <div className="article-card">
+      <img src={imageUrl} alt={title} className="article-image" />
+      <span className="publish-date">{publishDate}</span>
+      <h3>{title}</h3>
+      <p>{preview}</p>
+      <Buttons
+        type="read-more"
+        text="Read More"
+        icon={<img src={ReadMoreSVG} alt="Icon" />}
+        href={link}
+      />
+    </div>
   );
 };
 
 export const Articles = () => {
-
-
   return (
-<>
-     <div className='divider-wrapper'>
-        <img src={waveLine} alt='dividing line between slides'></img>
-      </div> 
-      <div className='articles-wrapper'>
-              <h1>My Words</h1>
-              <div className="articles-grid">
-                  {exampleArticles.map(article => <ArticleCard
-                      key={article.title}
-                      imageUrl={article.imageUrl}
-                      publishDate={article.publishDate}
-                      title={article.title}
-                      preview={article.preview}
-                      link={article.link} />
-                  )}
-
-              </div>
-          </div>
-          </>  
-  ); 
+    <>
+      <div className="divider-wrapper">
+        <img src={waveLine} alt="dividing line between slides"></img>
+      </div>
+      <div className="articles-wrapper">
+        <h1>My Words</h1>
+        <div className="articles-grid">
+          {exampleArticles.map((article) => (
+            <ArticleCard
+              key={article.title}
+              imageUrl={article.imageUrl}
+              publishDate={article.publishDate}
+              title={article.title}
+              preview={article.preview}
+              link={article.link}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/prop-types */
-import './Tech.css'
+import "./Tech.css";
 
 export const Tech = () => {
   return (
-    <div className='tech-wrapper'>
-      <div className='tech-content'>
-      <h1 className='tech-title'>Tech</h1>
-      <p className='tech-text'>HTML, CSS, Flexbox, Grid, Javascript, JSX, React, React Hooks, Node.js, Web Accessibility, APIs, Zustand, Branches, Pair-programming, Github, Figma, Netlify, Developer tools</p>
+    <div className="tech-wrapper">
+      <div className="tech-content">
+        <h1 className="tech-title">Tech</h1>
+        <p className="tech-text">
+          HTML, CSS, Flexbox, Grid, Javascript, JSX, React, React Hooks, React
+          librarys, Web Accessibility, APIs, Zustand, json, Node.js, MongoDB,
+          Mongoose, Express.js, Branches, Pair-programming, Github, Figma,
+          Netlify, Developer tools
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
