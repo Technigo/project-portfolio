7c57966e8222a2cb6902d04c8e9adf1887e65223
Carolina Luna <kroluna@gmail.com>
2024-01-17 09:16:59 +0100

    changes

--- src/components/Photo.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/prop-types */
-import "./Photo.css";
-
-export const Photo = ({ mode, color, source }) => {
-  console.log(source);
-  return <img src={source} className={`photo ${color} ${mode}`} />;
-};
