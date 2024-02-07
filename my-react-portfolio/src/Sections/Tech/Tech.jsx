f97c8f5f4a1f8a9faf20b84f2ca429991ebe7c33
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-02-07 11:22:50 +0100

    ground ready, now banner + arrow

--- my-react-portfolio/src/Sections/Tech/Tech.jsx
+++ my-react-portfolio/src/Sections/Tech/Tech.jsx
@@ -1,6 +1,6 @@
 import "./tech.css";
 import { LargeText } from "../../ReusableComponents/Texts/LargeText";
-import { SubTitle } from "../../ReusableComponents/Texts/SubTitle";
+import { NormalText } from "../../ReusableComponents/Texts/NormalText";
 
 const techText = "HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, API:s, mob-programming, pair programming, Github."
 
@@ -8,8 +8,8 @@ export const Tech = () => {
     return (
         <section className="tech">
             <section className="tech-wrapper">
-                <LargeText text={"Tech"} />
-                <SubTitle text={techText} />
+                <LargeText className="tech-heading" text={"Tech"} />
+                <NormalText className="tech-paragraph" text={techText} />
             </section>
         </section>
     )
