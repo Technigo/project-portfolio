5f17e8429a8997df03bf0e44850e3a62a1cf23cf
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-02-13 13:18:08 +0100

    the banner is in place

--- my-react-portfolio/src/ReusableComponents/Animation/Footerbanner/FooterBanner.jsx
+++ my-react-portfolio/src/ReusableComponents/Animation/Footerbanner/FooterBanner.jsx
@@ -1,27 +1,11 @@
-
-import React, { useRef, useEffect } from "react";
-import { gsap } from "gsap";
-import bannerSVG from "/src/assets/icon/banner.svg"; // Adjust the import path to your SVG file
+import "./footerbanner.css";
+import { SubTitle } from "../../Texts/SubTitle";
 
 export const FooterBanner = () => {
-    const bannerRef = useRef(null);
-
-    useEffect(() => {
-        // Ensure GSAP animation only initializes once the component is mounted
-        const banner = bannerRef.current;
-        const bannerWidth = banner.offsetWidth;
-
-        gsap.to(banner, {
-            x: -bannerWidth, // Move the banner to the left by its own width
-            ease: "none",
-            repeat: -2, // Infinite loop
-            duration: 20, // Duration to complete one loop, adjust as needed for speed
-        });
-    }, []);
-
     return (
-        <div ref={bannerRef} style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>
-            <img src={bannerSVG} alt="Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · " />
+        <div className="banner-container">
+            <SubTitle className="scrolltext" text={" Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer · Evelyn Del Carmen · Fullstack Developer ·"} />
         </div>
-    );
+    )
 };
+
