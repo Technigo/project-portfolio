484e5ec6fc4b8a4e3f39e6c83a145dff0b395c66
Mattias Lindström <mattiaslindstrom@MacBook-Air-som-tillhor-Mattias.local>
2024-02-15 20:53:24 +0000

    reset links

--- my-react-portfolio/src/Sections/Footer/Footer.jsx
+++ my-react-portfolio/src/Sections/Footer/Footer.jsx
@@ -2,11 +2,7 @@ import "./footer.css";
 import { CircleImage } from "../../ReusableComponents/Images/CircleImage";
 import { LargeText } from "../../ReusableComponents/Texts/LargeText";
 import { SmallSubTitle } from "../../ReusableComponents/Texts/SmallSubTitle";
-import LinkedInIcon from '../../assets/icon/linkedin.svg';
-import GithubIcon from '../../assets/icon/github.svg';
-import StackOverflowIcon from '../../assets/icon/stackoverflow.svg';
-import InstagramIcon from '../../assets/icon/instagram.svg';
-
+import { ProjectImage } from "../../ReusableComponents/Images/ProjectImage";
 import evelynImage from "../../assets/cvimage/evelyn.png";
 
 
@@ -32,16 +28,16 @@ export const Footer = () => {
                 </div>
                 <div className="contact-icons" >
                     <a href="https://www.linkedin.com/in/evelyn-del-carmen-736552175/">
-                        <LinkedInIcon className="icons"/>
+                        <ProjectImage className="icons" src={"./src/assets/icon/linkedin.svg"} />
                     </a>
                     <a href="https://github.com/EvelynDelCarmen">
-                    <GithubIcon className="icons"/>
+                        <ProjectImage className="icons" src={"./src/assets/icon/github.svg"} />
                     </a>
                     <a href="https://stackoverflowteams.com/c/technigo/users/510/?tab=profile">
-                        <StackOverflowIcon className="icons" />
+                        <ProjectImage className="icons" src={"./src/assets/icon/stackoverflow.svg"} />
                     </a>
                     <a href="https://www.instagram.com/evelyn._delcarmen/">
-                        <InstagramIcon className="icons" />
+                        <ProjectImage className="icons" src={"/instagram.svg"} />
                     </a>
                 </div>
             </section>
