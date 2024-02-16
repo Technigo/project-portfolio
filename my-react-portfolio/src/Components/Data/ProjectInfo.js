b4c6a2aa1de18e3f5e9ae56815ae723104cde30e
BeckieMorton <beckam11@gmail.com>
2024-02-16 13:54:42 +1100

    fix styling and spacing

--- my-react-portfolio/src/Components/Data/ProjectInfo.js
+++ my-react-portfolio/src/Components/Data/ProjectInfo.js
@@ -18,7 +18,7 @@ export const ProjectInfo = [
     gitUrl: "https://github.com/BeckieMorton/project-design-handoff-vite",
     className: "YogaDesign",
     skills: ["HTML", "CSS", "JavaScript", "react-i18next"],
-    text: "A colloboration week with the UX Design students. I was tasked with bringing to life my peers design in code. Uses the react-i18next internationalisation framework to translate the page into English and German.",
+    text: "A colloboration week with the UX Design students. I was tasked with bringing to life my peers design in code. Implimented react-i18next internationalisation.",
   },
 
   {
@@ -29,7 +29,7 @@ export const ProjectInfo = [
     gitUrl: "https://github.com/BeckieMorton/project-happy-thoughts-vite",
     className: "HappyThoughts",
     skills: ["HTML", "CSS", "React", "APIs"],
-    text: "A React project focused on state management, and API interactions for fetching and posting data. Features include a design-aligned UI, sorted thought display by recency, 'like' count and functionality, and a form for new thoughts.",
+    text: "A React project focused on state management, and API interactions for fetching and posting data. Features include a sorted thought display, 'like' count and functionality, and a form for new thoughts.",
   },
 
   {
@@ -41,7 +41,7 @@ export const ProjectInfo = [
       "https://github.com/BeckieMorton/project-custom-hooks-qr-code-generator-vite",
     className: "QrCode",
     skills: ["HTML", "CSS", "React", "Custom Hooks"],
-    text: "React & Vite Boiler Plate project to Generate QR codes from URLs, utilising custom hooks for efficient state management.",
+    text: "React & Vite project to Generate QR codes from URLs, utilising custom hooks for efficient state management.",
   },
   {
     imagePath: "./assets/projects/weatherapp.png",
@@ -50,7 +50,7 @@ export const ProjectInfo = [
     netlifyUrl: "https://wk4-weatherapp-api-a735bf.netlify.app/",
     gitUrl: "https://github.com/BeckieMorton/project-weather-app",
     className: "WeatherApp",
-    skills: ["HTML", "CSS", "JavaScript"],
+    skills: ["HTML", "CSS", "JavaScript", "APIs"],
     text: "A SPA crafted for mobile that uses a weather API to tell today's weather and give a 5-day forecast. A paried project to practice working in branches.",
   },
   {
@@ -61,7 +61,7 @@ export const ProjectInfo = [
     gitUrl: "https://github.com/BeckieMorton/project-chatbot",
     className: "ChatBot",
     skills: ["HTML", "CSS", "JavaScript"],
-    text: "A functional chatbot interface. The user can interact, and the bot can ask questions, display messages, and respond with precision. Book a flight with Myhem Mystery flights today!",
+    text: "A functional chatbot interface. The user can interact, and the bot can ask questions, display messages, and respond. Book a flight with Myhem Mystery flights today!",
   },
   {
     imagePath: "./assets/projects/moviesite.png",
@@ -71,7 +71,7 @@ export const ProjectInfo = [
     gitUrl: "https://github.com/BeckieMorton/project-movies-vite",
     className: "MovieSite",
     skills: ["HTML", "CSS", "JavaScript"],
-    text: "Utilising themoviedb.org's API to showcase movies and their details. Fetures fetching popular movie specifics, and managing image URLS. Using react router functionality, error handling, and loading states.",
+    text: "Utilising themoviedb.org's API to showcase movies and their details. Fetches movie specifics, manages image URLS. Uses react router functionality, error handling, and loading states.",
   },
 
   {
@@ -82,7 +82,7 @@ export const ProjectInfo = [
     gitUrl: "https://github.com/BeckieMorton/project-guess-who",
     className: "Guesswho",
     skills: ["HTML", "CSS", "JavaScript"],
-    text: "A digital version of the classic board game 'Guess Who'. Applying concepts like DOM manipulation, event handling, function definitions, conditional statements, delayed execution, variable assignment, and object and array manipulation.",
+    text: "A digital version of 'Guess Who'. Applying concepts like DOM manipulation, event handling, function definitions, conditional statements, delayed execution, variable assignment, and object and array manipulation.",
   },
 
   {
@@ -93,6 +93,6 @@ export const ProjectInfo = [
     gitUrl: "https://github.com/BeckieMorton/project-news-site",
     className: "NewSite",
     skills: ["HTML", "CSS"],
-    text: "My first Technigo Pre-course project: a classic news site. This project covers basic understanding of HTML and CSS, but also some deep diving into responsive web design and how to layout using CSS Grid and Flexbox. ",
+    text: "My first Technigo Pre-course project: a classic news site. This project covers basic understanding of HTML and CSS, and inlcudes a responsive design, CSS Grid and Flexbox.",
   },
 ];
