dd5ffdc3b96a5b16e7ab4807b6053b1c8e87aa7f
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-24 12:59:11 +0100

    connecting App.jsx with other components

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import Header from "../src/components/Header.jsx"
+import Lead from "../src/components/Lead.jsx"
+import Projects from "../src/components/Projects.jsx"
+import Skills from "../src/components/Skills.jsx"
+import Contact from "../src/components/Contact.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<Lead />
+			<Projects />
+			<Skills />
+			<Contact />
+		</>
+	)
 }
 
 export default App
