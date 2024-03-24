55bf4ebeb62e475403472291ae3496ebf5541f8f
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-24 16:13:34 +0100

    added fonts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import "../styling/contact.css"
 import linkedinIcon from "../assets/linkedin-grey.svg"
+import githubIcon from "../assets/github-grey.svg"
+import instagramIcon from "../assets/instagram-grey.svg"
 
 export const Contact = () => {
 	return (
@@ -9,6 +11,12 @@ export const Contact = () => {
 			<div className='social-links'>
 				<a href='https://www.linkedin.com/in/nathalie-tu/'>
 					<img src={linkedinIcon} alt='Linkedin icon' />
+				</a>
+				<a href='https://github.com/ohitsnathalie/'>
+					<img src={githubIcon} alt='Github icon' />
+				</a>
+				<a href='https://www.instagram.com/ohitsnathalie/'>
+					<img src={instagramIcon} alt='Instagram icon' />
 				</a>
 			</div>
 		</div>
