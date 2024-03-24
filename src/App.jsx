225cb271ae1a6ea6ec30da8bb582de426fcb6e68
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-24 13:19:08 +0100

    moved files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+import Header from "./components/Header.jsx"
+import Lead from "./components/Lead.jsx"
+import Projects from "./components/Projects.jsx"
+import Skills from "./components/Skills.jsx"
+import Contact from "./components/Contact.jsx"
+
+export const App = () => {
+	return (
+		<>
+			<Header />
+			<Lead />
+			<Projects />
+			<Skills />
+			<Contact />
+		</>
+	)
+}
+
+export default App
