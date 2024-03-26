cfd0cb202607f452c30253e2078685d58016ec83
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-03-26 17:43:35 +0100

    Add anchor tag to each nav list item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,47 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <img src={logo} alt="Website Logo" className={styles.logo}></img>
       <ul className={styles.navList}>
-        <li className={styles.navItem}>About</li>
-        <li className={styles.navItem}>Projects</li>
-        <li className={styles.navItem}>My words</li>
-        <li className={styles.navItem}>Let&apos;s talk</li>
-        <li className={styles.navItem}>Language</li>
+        <li>
+          <a
+            className={styles.navItem}
+            href="#"
+            aria-label="Go to About section"
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            className={styles.navItem}
+            href="#"
+            aria-label="Go to Projects section"
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a
+            className={styles.navItem}
+            href="#"
+            aria-label="Go to My Words section"
+          >
+            My words
+          </a>
+        </li>
+        <li>
+          <a
+            className={styles.navItem}
+            href="#"
+            aria-label="Go to Let's talk section"
+          >
+            Let&apos;s talk
+          </a>
+        </li>
+        <li>
+          <a className={styles.navItem} href="#">
+            Language
+          </a>
+        </li>
       </ul>
     </nav>
   );
