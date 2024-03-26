8154bc7794ba7c85f3fc340e45c258df31611458
Alma Herrström <almaherrstrom@Almas-MacBook-Air-4.local>
2024-03-26 12:22:16 +0100

    work on adding correct fonts, colors, font size

--- src/components/sections/Articles.jsx
+++ src/components/sections/Articles.jsx
@@ -1,6 +1,6 @@
 export const MyWords = () => {
   return (
-    <div className="my-words">
+    <div className="articles">
       <h2>My Words</h2>
       <p>If I decide to write any text, I will feature it here.</p>
     </div>
