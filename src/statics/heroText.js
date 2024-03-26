e56a7ed20a9574c02f424846f579dafdb8097245
Sakura Tanaka <sito6496t@gmail.com>
2024-03-27 08:38:51 +0900

    Fix number fonts

--- src/statics/heroText.js
+++ src/statics/heroText.js
@@ -1,5 +1,5 @@
 export const heroText = {
   en: " I've transitioned from a French pastry chef to a front-end developer, started to teach myself JavaScript, React, Next.js, Astro.js and CSS/SCSS in 2021 and studied Technigo's Bootcamp to enrich my programming skills in 2023. I am passionate about writing clean, solid code, learning new things and trying new technologies. I am delving into computer science as well 💻",
 
-  ja: "元フランス菓子のパティシエのフロントエンドエンジニアです。２０２１年からプログラミングを自分で勉強し始め、2023年８月から翌年１月までスウェーデンのTechnigoというプログラミングスクールで勉強しました。プログラムが大好きで、隙間時間があればコードを書くかプロジェクトのことを考えています。私は、いかに綺麗に良いパフォーマンスのコードを書けるかを大切にしております。",
+  ja: "元フランス菓子のパティシエのフロントエンドエンジニアです。２０２３年からプログラミングを自分で勉強し始め、２０２３年８月から翌年１月までスウェーデンのTechnigoというプログラミングスクールで勉強しました。プログラムが大好きで、隙間時間があればコードを書くかプロジェクトのことを考えています。私は、いかに綺麗に良いパフォーマンスのコードを書けるかを大切にしております。",
 };
