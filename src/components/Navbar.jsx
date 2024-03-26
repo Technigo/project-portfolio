82fd03a821d499216df6e2e7e820f0ceaf622c03
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-03-26 17:33:52 +0100

    Create navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
+import styles from "./Navbar.module.css";
+import logo from "../assets/logo.svg";
+
 const Navbar = () => {
-  return <nav>this is the navbar</nav>;
+  return (
+    <nav className={styles.navbar}>
+      <img src={logo} alt="Website Logo" className={styles.logo}></img>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>About</li>
+        <li className={styles.navItem}>Projects</li>
+        <li className={styles.navItem}>My words</li>
+        <li className={styles.navItem}>Let&apos;s talk</li>
+        <li className={styles.navItem}>Language</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
