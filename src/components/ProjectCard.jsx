44b1359436f819c19141b72545c15b8e6313db6f
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-03-27 23:50:08 +0100

    Style the project card and add proptypes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,23 +1,38 @@
 import Asset from "./Asset";
 import styles from "./ProjectCard.module.css";
+import PropTypes from "prop-types";
+import { IoIosLink } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
 
 const ProjectCard = ({ project }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Asset name={project.image} format="png" folder="projects" />
       </div>
       <h3>{project.name}</h3>
-      <p>{project.description}</p>
+      <p className={styles.description}>{project.description}</p>
       <p>{project.tags.join(" ")}</p>
-      <span>
-        <a href={project.netlify}>Live Preview</a>
-      </span>
-      <span>
-        <a href={project.github}>View Code</a>
-      </span>
+      <div className={styles.reference}>
+        <span className={styles.refItem}>
+          <IoIosLink />
+          <a href={project.netlify} rel="noopener noreferrer" target="_blank">
+            Live Preview
+          </a>
+        </span>
+        <span className={styles.refItem}>
+          <FaGithub />
+          <a href={project.github} rel="noopener noreferrer" target="_blank">
+            View Code
+          </a>
+        </span>
+      </div>
     </div>
   );
 };
 
 export default ProjectCard;
+
+ProjectCard.propTypes = {
+  project: PropTypes.object.isRequired,
+};
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,20 @@
 import ProjectCard from "./ProjectCard";
 import { projects } from "../projects.json";
+import styles from "./Projects.module.css";
 
 const Projects = () => {
   return (
-    <div>
-      {projects.map((project, index) => (
-        <ProjectCard key={index} project={project} />
-      ))}
-    </div>
+    <section id="projects" className={styles.section}>
+      <div className={styles.descr}>
+        <h2 className={styles.title}>Projects</h2>
+        <p className={styles.subtitle}>Projects I&apos;ve been working on</p>
+      </div>
+      <div className={styles.container}>
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
+    </section>
   );
 };
 
