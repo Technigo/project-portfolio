d1d6d674e0e6ecc2fa421e28e1e4269f00d97f64
Frida Svensson <fridamarisvensson@gmail.com>
2024-03-27 14:12:37 +0100

    create featured projects component and child

--- my-react-portfolio/src/components/FeaturedProjects.jsx
+++ my-react-portfolio/src/components/FeaturedProjects.jsx
@@ -1,12 +1,13 @@
 import "./FeaturedProjects.css";
+import { FpChild } from "./child-components/FpChild";
 
 export const FeaturedProjects = () => {
   return (
-  <section className="featured-projects-box">
-    <h1>Featured Projects</h1>
-    <div className="featured-projects-wrapper">
-      
-    </div>
-  </section>
-  )
+    <section className="featured-projects">
+      <div className="featured-projects-box">
+        <h1>Featured Projects</h1>
+        <FpChild />
+      </div>
+    </section>
+  );
 };
