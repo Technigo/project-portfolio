98753fc66e8a06300949bc25d04c1c45afc8c9bc
unknown <linda.frischknecht@bluewin.ch>
2024-03-27 16:36:34 +0100

    adding banner

---
+++ my-react-portfolio/src/contact/Banner.jsx
@@ -0,0 +1,29 @@
+import "./Banner.css";
+import { GoDotFill } from "react-icons/go";
+
+export const Banner = () => {
+  return (
+    <div className="banner-wrapper">
+      <div className="banner">
+        <div className="banneritem">
+          <p>Linda Frischknecht</p> <GoDotFill className="dot" />{" "}
+        </div>
+        <div className="banneritem">
+          <p>Web Developer</p> <GoDotFill className="dot" />{" "}
+        </div>
+        <div className="banneritem">
+          <p>Linda Frischknecht</p> <GoDotFill className="dot" />{" "}
+        </div>
+        <div className="banneritem">
+          <p>Web Developer</p> <GoDotFill className="dot" />{" "}
+        </div>
+        <div className="banneritem">
+          <p>Linda Frischknecht</p> <GoDotFill className="dot" />{" "}
+        </div>
+        <div className="banneritem">
+          <p>Web Developer</p> <GoDotFill className="dot" />{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
--- my-react-portfolio/src/contact/ContactSection.jsx
+++ my-react-portfolio/src/contact/ContactSection.jsx
@@ -2,36 +2,40 @@ import "./ContactSection.css";
 import Linda from "../assets/FotoLinda.jpg";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import { Banner } from "./Banner";
 
 export const Contact = () => {
   return (
-    <div className="contact-section">
-      <h1 className="contact-title">Let&apos;s talk</h1>
-      <img
-        src={Linda}
-        alt="Picture of Linda Frischknecht, Frontend Developer."
-      />
-      <div className="contact">
-        <p>Linda Frischknecht</p>
-        <p>+41 (0)76 329 67 97</p>
-        <p>linda.frischknecht@gmail.com</p>
+    <div>
+      <div className="contact-section">
+        <h1 className="contact-title">Let&apos;s talk</h1>
+        <img
+          src={Linda}
+          alt="Picture of Linda Frischknecht, Frontend Developer."
+        />
+        <div className="contact">
+          <p>Linda Frischknecht</p>
+          <p>+41 (0)76 329 67 97</p>
+          <p>linda.frischknecht@gmail.com</p>
+        </div>
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/lindafrischknecht/"
+            target="_blank"
+            aria-label="Click here to get to my linkedIn."
+          >
+            <FaLinkedinIn />
+          </a>
+          <a
+            href="https://github.com/linda-f"
+            target="_blank"
+            aria-label="Click here to get to my GitHub."
+          >
+            <FaGithub />
+          </a>
+        </div>
       </div>
-      <div className="icons">
-        <a
-          href="https://www.linkedin.com/in/lindafrischknecht/"
-          target="_blank"
-          aria-label="Click here to get to my linkedIn."
-        >
-          <FaLinkedinIn />
-        </a>
-        <a
-          href="https://github.com/linda-f"
-          target="_blank"
-          aria-label="Click here to get to my GitHub."
-        >
-          <FaGithub />
-        </a>
-      </div>
+      <Banner />
     </div>
   );
 };
