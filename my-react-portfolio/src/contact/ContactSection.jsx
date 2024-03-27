637f8ba412b8da2d6ac40a43ad4c3293fdd185d1
unknown <linda.frischknecht@bluewin.ch>
2024-03-27 15:36:14 +0100

    adding contact section

--- my-react-portfolio/src/contact/ContactSection.jsx
+++ my-react-portfolio/src/contact/ContactSection.jsx
@@ -1,3 +1,32 @@
+import "./ContactSection.css";
+import Linda from "../assets/FotoLinda.jpg";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
 export const Contact = () => {
-  return <p>Here comes the Contact Section!</p>;
+  return (
+    <div className="contact-section">
+      <h1 className="contact-title">Let&apos;s talk</h1>
+      <img
+        src={Linda}
+        alt="Picture of Linda Frischknecht, Frontend Developer."
+      />
+      <div className="contact">
+        <p>Linda Frischknecht</p>
+        <p>+41 (0)76 329 67 97</p>
+        <p>linda.frischknecht@gmail.com</p>
+      </div>
+      <div className="icons">
+        <a
+          href="https://www.linkedin.com/in/lindafrischknecht/"
+          target="_blank"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a href="https://github.com/linda-f" target="_blank">
+          <FaGithub />
+        </a>
+      </div>
+    </div>
+  );
 };
