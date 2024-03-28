30106766dc579a9790d83bb9c5b18add64824b27
Caracal23 <154092424+Caracal23@users.noreply.github.com>
2024-03-28 12:04:59 +0100

    styling images with shadow in Featured project section

--- my-react-portfolio/src/components/Project.jsx
+++ my-react-portfolio/src/components/Project.jsx
@@ -9,13 +9,15 @@ export const Project = ({ projectData }) => {
 
   return (
     <>
-      <div className="projects-container">
-        <img src={projectData.imageSrc} />
-        <h1>{projectData.title}</h1>
-        <p>{projectData.text}</p>
-        {getTechnologies()}
-        <a href={projectData.demoLink}>Live Demo </a>
-        <a href={projectData.gitLink}>Git Link </a>
+      <div className="projects-element">
+        <div className="projects-container">
+          <img className="projects-image" src={projectData.imageSrc} />
+          <h1 className="projects-title">{projectData.title}</h1>
+          <p className="projects-text">{projectData.text}</p>
+          {getTechnologies()}
+          <a href={projectData.demoLink}>Live Demo </a>
+          <a href={projectData.gitLink}>Git Link </a>
+        </div>
       </div>
     </>
   );
