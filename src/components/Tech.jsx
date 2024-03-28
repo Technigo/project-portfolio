5e78e56786baf5606752743aa238a5022bc72c75
SofieFerrari <sofiestrahl@gmail.com>
2024-03-28 16:05:25 +0100

    finnished tech file

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,17 @@
 import './Heading.css'
 
 export const TechSection = () => {
-  return(
-    <>
-    <div className='color-wrapper'>
-      <h1>Tech</h1>
-    </div>
-    </>
-  )
+	return (
+		<>
+			<div className="color-wrapper">
+				<h1>Tech</h1>
+				<h2>
+					HTML, CSS, Flexbox, JavaScript, ES6, JSX, React,
+          React Hooks, Node.js,
+					Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming,
+					GitHub.
+				</h2>
+			</div>
+		</>
+	)
 }
