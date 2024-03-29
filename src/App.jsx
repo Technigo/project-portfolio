1b3c093cd80de1c062877bdcc8e27b317ef7a5cd
Mai Kanetaka <maikanetaka@gmail.com>
2024-03-29 14:44:14 +0100

    Setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import "./App.css";
-import { Introduction } from "./components/Introduction";
-import { Card } from "./components/Card";
-import { projects } from "../projects.json";
 
 export const App = () => {
   return (
     <>
-      <section>
-        <Introduction />
-      </section>
-      <section>
-        <div>
-          <h1>Featured Projects</h1>
-        </div>
-        {projects.map((project, index) => (
-          <Card key={index} project={project} />
-        ))}
-      </section>
     </>
   );
 };
