9babccec2fb2cd341ea600e9235b852a812ccd5b
izzibizz <contact@izabellind.com>
2024-03-29 15:54:48 +0100

    changed css

---
+++ my-react-portfolio/src/App.jsx
@@ -0,0 +1,24 @@
+import { Introduction } from "./components/Introduction"
+import { Tech } from "./components/Tech"
+import { Projects } from "./components/Projects"
+//import { Articles } from "./components/Articles"
+import { Skills } from "./components/Skills"
+import { Contact } from "./components/Contact"
+
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+    <Introduction />
+    <Tech />
+    <Projects />
+    <Skills />
+    <Contact />
+ 
+    </>
+  )
+}
+
+export default App
