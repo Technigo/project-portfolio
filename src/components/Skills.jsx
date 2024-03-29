47ccf13b00e0e24d1a5a88b33dca753b1ad1f647
SofieFerrari <sofiestrahl@gmail.com>
2024-03-29 17:03:41 +0100

    worked some more on skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,38 +5,39 @@ export const MySkills = () => {
 		<>
 			<div className="skill-section">
 				<h1>Skills</h1>
-				<div className="skill-titles">
+				<div className="skills">
 					<h3 className="code">Code</h3>
 					<h3 className="toolbox">Toolbox</h3>
 					<h3 className="upcomming">Upcomming</h3>
 					<h3 className="more">More</h3>
+
+					<ul className="tech-skills-code">
+						<li>HTML5</li>
+						<li>CSS3</li>
+						<li>JavaScript ES6</li>
+						<li>React</li>
+						<li>Styled Components</li>
+						<li>Github</li>
+					</ul>
 				</div>
-				<ul className="tech-skills">
-					<li>HTML5</li>
-					<li>CSS3</li>
-					<li>JavaScript ES6</li>
-					<li>React</li>
-					<li>Styled Components</li>
-					<li>Github</li>
-				</ul>
-			</div>
-			<div>
-				<ul className="tech-skills">
-					<li>Figma</li>
-					<li>Postman</li>
-					<li>Slack</li>
-					<li>Notion</li>
-				</ul>
-			</div>
-			<div>
-				<ul className="tech-skills">
-					<li>Node.js</li>
-					<li>Python</li>
-					<li>C#</li>
-				</ul>
-			</div>
-			<div>
-				<ul className="tech-skills"></ul>
+				<div>
+					<ul className="tech-skills-toolbox">
+						<li>Figma</li>
+						<li>Postman</li>
+						<li>Slack</li>
+						<li>Notion</li>
+					</ul>
+				</div>
+				<div>
+					<ul className="tech-skills-upcomming">
+						<li>Node.js</li>
+						<li>Python</li>
+						<li>C#</li>
+					</ul>
+				</div>
+				<div>
+					<ul className="tech-skills-more"></ul>
+				</div>
 			</div>
 		</>
 	)
