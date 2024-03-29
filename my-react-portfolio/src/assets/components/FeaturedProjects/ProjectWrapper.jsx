671d021dd7a74ca482926cdf42a2e10add2fb159
Trista Shan <trista8703@hotmail.com>
2024-03-29 21:17:25 +0200

    button and hover done

--- my-react-portfolio/src/assets/components/FeaturedProjects/ProjectWrapper.jsx
+++ my-react-portfolio/src/assets/components/FeaturedProjects/ProjectWrapper.jsx
@@ -1,12 +1,14 @@
-import { BtnLink } from "../smallComps/BtnLink/BtnLink"
+// import { BtnLink } from "../smallComps/BtnLink/BtnLink"
 // import { Tag } from "../smallComps/tags/Tag"
 import { Image } from "../smallComps/Image"
 import "./projects.css"
+import {Demo} from "../iconfoler/Demo"
+import {Code} from "../iconfoler/Code"
+import "./iconBtn.css"
+
 
 export const ProjectWrapper = ({projectImg, projectName, demoLink, codeLink, description, alt}) => {
 
-    const liveDemoBtn = "../../../public/icons/liveDemo.svg"
-    const viewTheCodeBtn = "../../../public/icons/viewTheCode.svg"
     return (
       <>
         <div className="card-image">
@@ -19,21 +21,21 @@ export const ProjectWrapper = ({projectImg, projectName, demoLink, codeLink, des
             <Tag projectTags={projectTags} />
           </div> */}
           
-          <div className="btn">
-            <BtnLink
-              link={demoLink}
-              src={liveDemoBtn}
-              alt={"demo link"}
-              btnContent={"Live demo"}
-              btnName={"codeBtn"}
-            />
-            <BtnLink
-              link={codeLink}
-              src={viewTheCodeBtn}
-              alt={"code link"}
-              btnContent={"View the code"}
-              btnName={"linkBtn"}
-            />
+          <div className="btn"> 
+          <div id="liveDemoBtn">
+            <a href={demoLink} >
+                <Demo />
+                Live Demo
+              </a>
+          </div>
+          <div id="viewCodeBtn">
+            <a href={codeLink} >
+                <Code />
+              View the code
+              </a>
+          </div>
+              
+              
           </div>
         </div>
       </>
--- my-react-portfolio/src/assets/components/smallComps/BtnLink/BtnLink.jsx
+++ my-react-portfolio/src/assets/components/smallComps/BtnLink/BtnLink.jsx
@@ -1,8 +1,12 @@
-
-export const BtnLink = ({link, src, alt, btnContent, btnName}) => {
+import { Demo } from "../../iconfoler/Demo";
+import "./btnLink.css"
+export const BtnLink = ({link, btnContent, btnName}) => {
     return (
-        <a href={link}className="link-btn"><img src={src} alt={alt} className={`${btnName}`}/>{btnContent}</a>
-    )
+        <a href={link} className={`${btnName}`}>
+         <Demo className= 'link-btn' />
+          <p className="btnContent">{btnContent}</p>
+        </a>
+    );
 }
 
 //use if state to change the hover svg, by the classname, so can make all the
