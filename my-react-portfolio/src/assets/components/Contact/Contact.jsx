0075ff54adae40f0a6b5570497014289008ae444
Trista Shan <trista8703@hotmail.com>
2024-03-29 23:50:33 +0200

    most styling done

--- my-react-portfolio/src/assets/components/Contact/Contact.jsx
+++ my-react-portfolio/src/assets/components/Contact/Contact.jsx
@@ -1,32 +1,49 @@
 import './Contact.css'
 import { PageTopic } from '../smallComps/PageTopic'
-import { BtnLink } from '../smallComps/BtnLink/BtnLink'
+import { Linkedin } from '../iconfoler/Linkedin'
+import { Github } from '../iconfoler/Github'
+import { Stackover } from '../iconfoler/Stackoverflow'
+import { Instagram } from '../iconfoler/ig'
 
 export const Contact = () => {
     const linkedin = "https://www.linkedin.com/in/trista-shan-62074471/"
-    const linkedinBtn = "../../../public/icons/linkedin.svg" 
-    const gitHubBtn = "../../../public/icons/gitHub.svg"
-    const instagramBtn = "../../../public/icons/instagram.svg"
     const profilePhoto ="../../../public/images/profilePhoto.jpg" 
+    const github = "https://github.com/trista1987"
+    const stackover="https://stackoverflowteams.com/c/technigo/users/598/?tab=profile"
+    const instagram = "https://www.instagram.com/trista_shan/"
 
     return (
-        <div className='contact-wrapper'>
-            <PageTopic pageTopic={"Let's Talk"} className={'contact'}/>
-            <div className='picInfo'>
-              <img src={profilePhoto} alt='profile photo' className='profilePhoto2'/>
-              <div className='info'>
-                <p className='name'>Trista Shan</p>
-                <p className='phone'>phone number</p>
-                <p className='email'>email</p>
-              </div>
-            </div>
-            <div className="contactIcons">
-            <BtnLink link={linkedin} src={linkedinBtn} alt={'linkednin button'} btnContent={''} />
-            <BtnLink link={linkedin} src={gitHubBtn} alt={'gitHub button'} btnContent={''} />
-            <BtnLink link={linkedin} src={instagramBtn} alt={'instagram button'} btnContent={''} />
-            </div>
+      <section className="contact-wrapper">
+        <PageTopic pageTopic={"Let's Talk"} className={"contact"} />
+        <div className="contactContent">
+          <img
+            src={profilePhoto}
+            alt="profile photo"
+            className="profilePhoto2"
+          />
+          <div className="info">
+            <p className="name">Trista Shan</p>
+            <p className="phone">phone number</p>
+            <p className="email">email</p>
+          </div>
         </div>
-    )
+        <div className="contactIcons">
+            <a href={linkedin}>
+              <Linkedin />
+            </a>
+            <a href={github}>
+              <Github />
+            </a>
+            <a href={stackover}>
+              <Stackover />
+            </a>
+            <a href={instagram}>
+              <Instagram />
+            </a>
+          </div>
+
+      </section>
+    );
 }
 //email need to work on for spam
 //hover svg doesn't work, need to add hovered svg 
