e35e766840f5b58dca34a30470f923f93b9854da
Mai Kanetaka <maikanetaka@gmail.com>
2024-03-29 18:07:59 +0100

    Add Tech.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { About } from "./components/About";
+import { Tech } from "./components/Tech";
 
 export const App = () => {
   return (
     <>
       <About />
+      <Tech />
     </>
   );
 };
