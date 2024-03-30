d4dff53a1261597201f2081f50a2257d4f976e40
Citronskal <yiaswift@gmail.com>
2024-03-30 18:05:54 +0100

    tech css

--- yia-portfolio/src/assets/sections/Tech.jsx
+++ yia-portfolio/src/assets/sections/Tech.jsx
@@ -2,11 +2,19 @@ import { SectionHead } from "../components/SectionHead.jsx";
 import "./Tech.css";
 
 export const Tech = () => {
-    const headlineId = 2;
-    return (
-        <div className="tech">
-        <SectionHead headlineId={headlineId}/>
-        <p>HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, MongoDB, Web Accessibility, API:s, GitHub, mob-programming, pair-programming, code review</p>
+  const headlineId = 2;
+  return (
+    <div className="tech-container">
+      <div className="tech-title">
+        <SectionHead headlineId={headlineId} />
+      </div>
+      <div className="tech-content">
+        <p>
+          HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, <br />React Hooks, Node.js,
+          MongoDB, Web Accessibility, API:s, <br /> GitHub, mob-programming,
+          pair-programming, code review
+        </p>
+      </div>
     </div>
-    );
+  );
 };
