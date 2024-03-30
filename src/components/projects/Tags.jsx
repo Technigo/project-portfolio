07d26bbf129132253493a68bcfb107182dfcc237
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-30 01:50:58 +0100

    change div to ul

--- src/components/projects/Tags.jsx
+++ src/components/projects/Tags.jsx
@@ -2,12 +2,12 @@ import "./Tags.css"
 
 export const Tags = ({ tags }) => {
 	return (
-		<div className='tag-container'>
+		<ul className='tag-container'>
 			{tags.map((topic) => (
 				<li className='tag' key={topic}>
 					{topic.toUpperCase()}
 				</li>
 			))}
-		</div>
+		</ul>
 	)
 }
