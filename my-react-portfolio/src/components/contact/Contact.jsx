eea2490608104205e1ab19cd16be4edf1b49e43d
Your El1an3 <eliane_w@yahoo.com>
2024-03-30 12:27:03 +0100

    styling contact added

--- my-react-portfolio/src/components/contact/Contact.jsx
+++ my-react-portfolio/src/components/contact/Contact.jsx
@@ -4,24 +4,27 @@ import { Heading } from "../heading/Heading";
 
 export const Contact = () => {
   return (
-    <section className="flex contact-section">
-      <div className="flex contact-container">
-        <Heading level="2" text="Let's talk" className="blue" />
-
-        <ProfileImage />
+    <section className="contact-section">
+      <div className="contact-container">
+        <div className="heading-wrapper">
+          <Heading level="2" text="Let's talk" className="dark" />
+        </div>
+        <div className="profile-wrapper">
+          <ProfileImage />
+        </div>
         <div className="flex contact-wrapper">
           <Heading level="3" text="Eliane Weber" />
           <Heading level="3" text="+41 99 111 22 33" />
           <Heading level="3" text="example@example.com" />
         </div>
         <div className="social-wrapper">
-          <button>
+          <button className="social-btn">
             <img src="../linkedin.svg" alt="linkedin-icon" />
           </button>
-          <button>
+          <button className="social-btn">
             <img src="../instagram.svg" alt="instagram-icon" />
           </button>
-          <button>
+          <button className="social-btn">
             <img src="../btngithub.svg" alt="github-icon" />
           </button>
         </div>
