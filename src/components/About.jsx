0e0d79008bf67382feb4c0221c57b5b664db1b7c
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-03-30 19:22:43 +0100

    Adjust the responsiveness and margin on About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,17 +4,23 @@ import avatar from "../assets/avatar.jpeg";
 const About = () => {
   return (
     <section className={styles.section} id="about">
-      <p>Hi I&apos;m Wen Zhao</p>
-      <h1 className={styles.frontend}>Frontend Developer</h1>
-      <div className={styles.intro}>
-        <div className={styles.avatarContainer}>
-          <img className={styles.avatar} src={avatar} alt="profile image"></img>
+      <div className={styles.container}>
+        <p>Hi I&apos;m Wen Zhao</p>
+        <h1 className={styles.frontend}>Frontend Developer</h1>
+        <div className={styles.intro}>
+          <div className={styles.avatarContainer}>
+            <img
+              className={styles.avatar}
+              src={avatar}
+              alt="profile image"
+            ></img>
+          </div>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            deleniti magnam expedita dolores ipsam blanditiis obcaecati nisi?
+            Adipisci, doloremque mollitia.
+          </p>
         </div>
-        <p className={styles.description}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo deleniti
-          magnam expedita dolores ipsam blanditiis obcaecati nisi? Adipisci,
-          doloremque mollitia.
-        </p>
       </div>
     </section>
   );
