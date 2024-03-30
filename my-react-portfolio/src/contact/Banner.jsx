3913148f450cfafcd34080c945457b38a8c1e8d6
unknown <linda.frischknecht@bluewin.ch>
2024-03-30 12:22:44 +0100

    fixed banner

--- my-react-portfolio/src/contact/Banner.jsx
+++ my-react-portfolio/src/contact/Banner.jsx
@@ -4,24 +4,24 @@ import { GoDotFill } from "react-icons/go";
 export const Banner = () => {
   return (
     <div className="banner-wrapper">
-      <marquee className="banner">
-        <div className="banneritem">
-          <p>
-            Linda Frischknecht <span className="banner-span"></span>
-            <GoDotFill className="dot" /> <span className="banner-span"></span>
-            Web Developer <span className="banner-span"></span>
-            <GoDotFill className="dot" /> <span className="banner-span"></span>
-            Linda Frischknecht <span className="banner-span"></span>
-            <GoDotFill className="dot" /> <span className="banner-span"></span>
-            Web Developer <span className="banner-span"></span>
-            <GoDotFill className="dot" /> <span className="banner-span"></span>
-            Linda Frischknecht <span className="banner-span"></span>
-            <GoDotFill className="dot" /> <span className="banner-span"></span>
-            Web Developer <span className="banner-span"></span>
-            <GoDotFill className="dot" /> <span className="banner-span"></span>
-          </p>
-        </div>
-      </marquee>
+      <span className="banneritem">
+        Linda Frischknecht <span className="banner-span"></span>
+        <GoDotFill className="dot" /> <span className="banner-span"></span>
+        Web Developer <span className="banner-span"></span>
+        <GoDotFill className="dot" /> <span className="banner-span"></span>
+      </span>
+      <span className="banneritem">
+        Linda Frischknecht <span className="banner-span"></span>
+        <GoDotFill className="dot" /> <span className="banner-span"></span>
+        Web Developer <span className="banner-span"></span>
+        <GoDotFill className="dot" /> <span className="banner-span"></span>
+      </span>
+      <span className="banneritem">
+        Linda Frischknecht <span className="banner-span"></span>
+        <GoDotFill className="dot" /> <span className="banner-span"></span>
+        Web Developer <span className="banner-span"></span>
+        <GoDotFill className="dot" /> <span className="banner-span"></span>
+      </span>
     </div>
   );
 };
