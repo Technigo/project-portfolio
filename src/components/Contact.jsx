349d121f4b782cfb58fdbb504ed93f3d3dd99b95
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2024-04-01 00:07:02 +0200

    -m

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,36 +5,37 @@ import profileImage from '../assets/original.jpg';
 import './Contact.css';
 import Button from './Button';
 
-const Contact = React.forwardRef((props, ref) => { // Use forwardRef
+const Contact = React.forwardRef((props, ref) => {
   const linkedinLink = 'https://se.linkedin.com/in/alexandra-meija-0757403b';
   const githubLink = 'https://github.com/Cocofnas';
 
   return (
-    <div className="contact" ref={ref}> {/* Attach the ref */}
+    <div className="contact" ref={ref}>
       <h2 className="classh2contact">Let's talk</h2>
       <img src={profileImage} alt="Your Name" className="profile-image" />
       <p className="contact-text">You can reach me here:</p>
       <ul className="contact-list">
-      <div className="button-container">
-        <Button
-          type="contact-link" 
-          icon={<img src={linkedinIcon} alt="LinkedIn" />} 
-          onClick={() => window.open(linkedinLink, '_blank')} 
-        />
-        </div>
-<div className="button-container">
-  <Button
-    type="contact-link"
-    icon={<img src={githubIcon} alt="GitHub" />}
-    onClick={() => window.open(githubLink, '_blank')}
-  />
-</div>
+        <li className="contact-item">
+          <Button
+            type="contact-link" 
+            icon={<img src={linkedinIcon} alt="LinkedIn" />} 
+            onClick={() => window.open(linkedinLink, '_blank')}
+          />
+        </li>
+        <li className="contact-item">
+          <Button
+            type="contact-link"
+            icon={<img src={githubIcon} alt="GitHub" />}
+            onClick={() => window.open(githubLink, '_blank')}
+          />
+        </li>
       </ul>
     </div>
   );
 });
 
 export default Contact;
+
 
 
 
