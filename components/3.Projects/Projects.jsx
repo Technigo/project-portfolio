229a19619e3d616480fdd0dc462083e03b41833f
Lovisa <lovisa.aaberg@gmail.com>
2024-03-31 16:04:52 +0200

    Articles content added with styling

--- components/3.Projects/Projects.jsx
+++
@@ -1,9 +0,0 @@
-import "./Projects.css";
-
-export const Projects = () => {
-  return (
-    <div className="projects-container">
-      <h2 className="projects-heading">Featured Projects</h2>
-    </div>
-  );
-};
