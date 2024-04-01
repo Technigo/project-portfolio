7a2fb3b04d8fb9d52c04ac067f3a96ad64979e63
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-01 13:30:04 +0200

    Update About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,33 @@
+import { Profile } from "../Image/Image";
+
 export const About = () => {
   return (
     <section>
-      <p>Hi, I am Mai Kanetaka</p>
-      <h1>Frontend Developer</h1>
       <div>
-        <img></img>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-          voluptates facere eius quaerat similique incidunt natus et alias in
-          rerum.
-        </p>
+        <p>Mai Kanetaka</p>
+        <h1>Frontend Developer</h1>
+      </div>
+      <div>
+        <div>
+          <Profile />
+        </div>
+        <div>
+          <p>
+            As a marketing professional with a deep fascination for storytelling
+            and SEO efficacy, my expertise lies in creating captivating content
+            and dynamic visuals.
+          </p>
+          <p>
+            Advancing in the digital landscape through specialized study in
+            Frontend Development, focusing on JavaScript and React, is a current
+            priority.
+          </p>
+          <p>
+            My ambition is to blend my array of skills in content generation,
+            marketing strategy, and technical innovation, striving to offer
+            strategic and inventive input to cutting-edge tech projects.
+          </p>
+        </div>
       </div>
     </section>
   );
