e8e3ff7f99033023f6b9fffd14457f2e07d30843
Paulajungaker <paula010514@hotmail.com>
2024-04-01 14:50:13 +0200

    Styling the contact part

--- my-react-portfolio/src/sections/Contact.jsx
+++ my-react-portfolio/src/sections/Contact.jsx
@@ -2,35 +2,35 @@ import myImage from "../assets/myImage.jpg";
 import githubIcon from "../assets/githubIcon.png";
 import linkedinIcon from "../assets/linkedinIcon.png";
 import "../styling/Contact.css";
-import banner from "../assets/Banner.png";
 
 const Contact = () => {
   return (
     <div className="contactContainer">
+      <h1 className="contact">Let's talk</h1>
+      <img src={myImage} alt="My picture" className="myImage" />
       <div className="contactInfo">
-        <h1 className="contact">Let's talk</h1>
-        <img src={myImage} alt="My picture" className="myImage" />
         <h2>Paula Jungåker</h2>
         <h2>+46(0)76 170 11 32</h2>
         <h2>paula010514@hotmail.com</h2>
+        <div className="contactIcons">
+          <a
+            href="https://github.com/Paulajungaker"
+            className="githubIcon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="Link to my GitHub" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/paula-jungåker-380418299/"
+            className="linkedinIcon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinIcon} alt="Link to my linkedIn" />
+          </a>
+        </div>
       </div>
-      <a
-        href="https://github.com/Paulajungaker"
-        className="githubIcon"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={githubIcon} alt="Link to my GitHub" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/paula-jungåker-380418299/"
-        className="linkedinIcon"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={linkedinIcon} alt="Link to my linkedIn" />
-      </a>
-      <img src={banner} alt="Paula Jungåker, Frontend Developer" />
     </div>
   );
 };
