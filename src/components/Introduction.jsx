f506a7d54b610bd4ff307c80de11edb95f161a60
SofieFerrari <sofiestrahl@gmail.com>
2024-04-01 11:35:48 +0200

    fixed introduction image and text to flex

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -12,17 +12,15 @@ export const Introduction = () => {
 				<div className="heading-title">
 					<h1>Frontend Developer</h1>
 				</div>
-				{/* <div className="heading-card"> */}
-					<img src={myImage} alt="Picture of Sofie" className="my-image"></img>
-					<div className="heading-text">
+				<div className="my-image">
+					<img src={myImage} alt="Picture of Sofie" ></img>
 						<p>
 							With a background in speech pathology, I combine my passion for
 							learning new skills and technology with great communication,
 							problem-solving abilities, and an analytical mind with a keen
 							attention to detail.
 						</p>
-					</div>
-				{/* </div> */}
+				</div>
 				<div className="arrow">
 					<TbArrowNarrowDown />
 				</div>
