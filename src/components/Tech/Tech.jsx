5ff2d27536f688f7c97b19e3bce3fec9b2d52e80
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-01 13:23:05 +0200

    Edit Tech

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -1,4 +1,3 @@
-
 export const Tech = () => {
   return (
     <section>
@@ -6,6 +5,9 @@ export const Tech = () => {
         <h2>Tech</h2>
       </div>
       <div>
+        <p>
+          HTML, CSS, JavaScript, ES6, JSX, React Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming
+        </p>
       </div>
     </section>
   );
