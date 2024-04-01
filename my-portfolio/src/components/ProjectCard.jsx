eb41d03c2689e73547c3367c07ee056a159b6e94
schouenkes <m.schouenke@gmail.com>
2024-04-01 19:53:07 +0200

    added and styled buttons

--- my-portfolio/src/components/ProjectCard.jsx
+++ my-portfolio/src/components/ProjectCard.jsx
@@ -1,14 +1,31 @@
 import { PropTypes } from "prop-types";
+import { Button } from "./Button.jsx";
 
 export const ProjectCard = ({ name, gitHubLink, topics, deployedLink, description, repoImage }) => {
   return (
     <li>
-      <img src={repoImage} alt="displays the frontpage of the webpage" />
-      <h2>{name}</h2>
-      <p>{description}</p>
-      {topics.map((tag) => {
-        return <p key={tag}>{tag}</p>;
-      })}
+      <img className="repo-image" src={repoImage} alt="displays the frontpage of the webpage" />
+      <div className="projectcard-textbox">
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <div className="tags-box">
+          {topics.map((tag) => {
+            return <p key={tag}>{tag}</p>;
+          })}
+        </div>
+        <Button
+          buttonIcon="src/assets/icons/live-icon.svg"
+          url={deployedLink}
+          buttonStyle="live-button"
+          buttonName="Live demo"
+        />
+        <Button
+          buttonIcon="src/assets/icons/github-icon.svg"
+          url={gitHubLink}
+          buttonStyle="github-button"
+          buttonName="View the code"
+        />
+      </div>
     </li>
   );
 };
