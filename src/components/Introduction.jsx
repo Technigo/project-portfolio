3c6c54c5b021c7437c3385e3177295f0e4eeed11
SofieFerrari <sofiestrahl@gmail.com>
2024-04-01 19:49:11 +0200

    worked on cleaning up skill section

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,6 +1,6 @@
 import './Heading.css'
 import myImage from '../assets/image.me.png'
-import { TbArrowNarrowDown } from "react-icons/tb"
+import { TbArrowNarrowDown } from 'react-icons/tb'
 
 export const Introduction = () => {
 	return (
@@ -12,14 +12,16 @@ export const Introduction = () => {
 				<div className="heading-title">
 					<h1>Frontend Developer</h1>
 				</div>
-				<div className="my-image">
-					<img src={myImage} alt="Picture of Sofie" ></img>
-						<p>
-							With a background in speech pathology, I combine my passion for
-							learning new skills and technology with great communication,
-							problem-solving abilities, and an analytical mind with a keen
-							attention to detail.
-						</p>
+				<div className="image">
+					<img src={myImage} alt="Picture of Sofie"></img>
+				<div className='image-text'>
+					<p>
+						With a background in speech pathology, I combine my passion for
+						learning new skills and technology with great communication,
+						problem-solving abilities, and an analytical mind with a keen
+						attention to detail.
+					</p>
+					</div>
 				</div>
 				<div className="arrow">
 					<TbArrowNarrowDown />
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,18 +8,7 @@ export const MySkills = () => {
 				<div className="skills">
 					<div className="code">
 						<p>Code</p>
-					</div>
-					<div className="toolbox">
-						<p>Toolbox</p>
-					</div>
-					<div className="upcomming">
-						<p>Upcomming</p>
-					</div>
-					<div className="more">
-						<p>More</p>
-					</div>
-					<div className="tech-skills-code">
-						<ul>
+						<ul className="tech-skills-code">
 							<li>HTML5</li>
 							<li>CSS3</li>
 							<li>JavaScript ES6</li>
@@ -28,23 +17,26 @@ export const MySkills = () => {
 							<li>Github</li>
 						</ul>
 					</div>
-					<div className="tech-skills-toolbox">
-						<ul>
+					<div className="toolbox">
+						<p>Toolbox</p>
+						<ul className="tech-skills-toolbox">
 							<li>Figma</li>
 							<li>Postman</li>
 							<li>Slack</li>
 							<li>Notion</li>
 						</ul>
 					</div>
-					<div className="tech-skills-upcomming">
-						<ul>
+					<div className="upcomming">
+						<p>Upcomming</p>
+						<ul className="tech-skills-upcomming">
 							<li>Node.js</li>
 							<li>Python</li>
 							<li>C#</li>
 						</ul>
 					</div>
-					<div className="tech-skills-more">
-						<ul>
+					<div className="more">
+						<p>More</p>
+						<ul className="tech-skills-more">
 							<li>Communication</li>
 							<li>Problem solver</li>
 							<li>Analytic</li>
@@ -55,3 +47,61 @@ export const MySkills = () => {
 		</>
 	)
 }
+
+// import './Skills.css'
+
+// export const MySkills = () => {
+// 	return (
+// 		<>
+// 			<div className="skill-section">
+// 				<h1>Skills</h1>
+// 				<div className="skills">
+// 					<div className="code">
+// 						<p>Code</p>
+// 					</div>
+// 					<div className="toolbox">
+// 						<p>Toolbox</p>
+// 					</div>
+// 					<div className="upcomming">
+// 						<p>Upcomming</p>
+// 					</div>
+// 					<div className="more">
+// 						<p>More</p>
+// 					</div>
+// 					<div className="tech-skills-code">
+// 						<ul>
+// 							<li>HTML5</li>
+// 							<li>CSS3</li>
+// 							<li>JavaScript ES6</li>
+// 							<li>React</li>
+// 							<li>Styled Components</li>
+// 							<li>Github</li>
+// 						</ul>
+// 					</div>
+// 					<div className="tech-skills-toolbox">
+// 						<ul>
+// 							<li>Figma</li>
+// 							<li>Postman</li>
+// 							<li>Slack</li>
+// 							<li>Notion</li>
+// 						</ul>
+// 					</div>
+// 					<div className="tech-skills-upcomming">
+// 						<ul>
+// 							<li>Node.js</li>
+// 							<li>Python</li>
+// 							<li>C#</li>
+// 						</ul>
+// 					</div>
+// 					<div className="tech-skills-more">
+// 						<ul>
+// 							<li>Communication</li>
+// 							<li>Problem solver</li>
+// 							<li>Analytic</li>
+// 						</ul>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</>
+// 	)
+// }
