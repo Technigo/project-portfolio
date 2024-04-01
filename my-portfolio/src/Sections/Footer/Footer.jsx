da20e85bc6313a44cf026c4015af922d257c4ecf
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-01 11:01:03 +0200

    Adding marquee effect to footer

--- my-portfolio/src/Sections/Footer/Footer.jsx
+++ my-portfolio/src/Sections/Footer/Footer.jsx
@@ -1,3 +1,19 @@
+import "./Footer.css";
 export const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className="marquee enable-animation marquee--fit-content">
+      <ul className="marquee__content">
+        <li>Martin Jönsson</li>
+        <li>•</li>
+        <li>Frontend Developer</li>
+        <li>•</li>
+      </ul>
+      <ul aria-hidden="true" className="marquee__content">
+        <li>Martin Jönsson</li>
+        <li>•</li>
+        <li>Frontend Developer</li>
+        <li>•</li>
+      </ul>
+    </div>
+  );
 };
--- my-portfolio/src/Sections/Talk/Talk.jsx
+++ my-portfolio/src/Sections/Talk/Talk.jsx
@@ -3,13 +3,21 @@ export const Talk = () => {
   return (
     <div className="talk">
       <h2>Let&apos;s Talk</h2>
-      <img className="profile-picture" src="../src/assets/Martin-square.jpg" alt="Martin Jönsson" />
+      <img
+        className="profile-picture"
+        src="../src/assets/Martin-square.jpg"
+        alt="Martin Jönsson"
+      />
+      <p>Martin Jönsson</p>
       <p>+46(0)73-909 11 99</p>
       <p>martin.t.joensson@gmail.com</p>
       <div className="socialmedia">
         <img src="../src/assets/Btn - linkedin.png" alt="LinkedIn"></img>
         <img src="../src/assets/Btn - github.png" alt="GitHub"></img>
-        <img src="../src/assets/Btn - stackoverflow.png" alt="StackOverflow"></img>
+        <img
+          src="../src/assets/Btn - stackoverflow.png"
+          alt="StackOverflow"
+        ></img>
         <img src="../src/assets/Btn - instagram.png" alt="Instagram"></img>
       </div>
     </div>
