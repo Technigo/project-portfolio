e4ef6ce2ee088b66a68ce3aab66676d7000d35ca
jingh999 <jinghuang0628@gmail.com>
2024-04-02 23:44:00 +0200

    edit

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -11,21 +11,23 @@ export const TopSection =()=> {
   }
 
   return (
-    <><div className="top">
+    <div className="top">
       <div className="content">
-        <img src={portrait} alt="JingPortrait" className="mobilePortrait" />
-
+        <img src={portrait} alt="JingPortrait" className="mobilePortrait"/>
+        
         <div className="frame">
           <p className="topGreeting">Hi, I'm Jing Huang</p>
           <p className="titleBlue">Frontend Developer + Designer</p>
         </div>
-
+        
         <div className="topDetails">
           <img src={portrait} alt="JingPortrait" className="topPortrait" />
-          <p className="selfIntro">Jing is a front-end developer with a background in architecture, renowned for her strong design and coding abilities. Her career is built on delivering innovative solutions that seamlessly merge aesthetics with functionality. Drawing from the design experience, Jing approach each project with a keen eye for detail and a deep understanding of structure and flow.
+          <p className="selfIntro">Jing is a front-end developer with a background in architecture, renowned for her strong design and coding abilities. Her career is built on delivering innovative solutions that seamlessly merge aesthetics with functionality. Drawing from the design experience, Jing approach each project with a keen eye for detail and a deep understanding of structure and flow. 
           </p>
         </div>
-
-      </p>
-    </div><img src={arrow} alt="arrow" className="arrow" onClick={ScrollDown} /></>
-)
+        
+        <img src={arrow} alt="arrow" className="arrow" onClick={ScrollDown}/>
+      </div>
+    </div>
+  )
+}
