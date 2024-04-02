84d4ffca95750869d4d5295c7a6eb3d3bd945c53
izzibizz <contact@izabellind.com>
2024-04-02 19:30:26 +0200

    added phone number link

--- my-react-portfolio/src/components/Contact.jsx
+++ my-react-portfolio/src/components/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <h1>Let&apos;s talk</h1>
       <img src={portrait} id="portrait" alt="portait of Izabel" />
       <h3>Izabel Lind Färnstrand</h3>
-      <h3>+46729632973</h3>
+      <h3><a href="tel:0046729632973">+46729632973</a></h3>
       <div className="email"><a href="mailto:contact@izabellind.com"><img src={email} alt="email" id="email"/><h3>contact@izabellind.com</h3></a>
       </div>
       <div className="social-media">
