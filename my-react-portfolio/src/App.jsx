b1ee1c328e2cfacd6c4911eacb4af506a969ba07
Cornelia Dahlin <cornelia.dahlin@proton.me>
2024-04-02 17:43:26 +0200

    Created Tech, done with content and design

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -1,9 +1,11 @@
 import { Intro } from "./components/Intro/Intro.jsx";
+import { Tech } from "./components/Tech/Tech.jsx";
 
 export const App = () => {
   return (
     <>
-    <Intro />
+      <Intro />
+      <Tech />
     </>
   );
 };
