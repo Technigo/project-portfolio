86fce53ab8480a082df8bef76ed72605dc1f783a
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-04-08 16:43:02 +0200

    Update Contact.jsx

--- my-react-portfolio/src/components/Contact/Contact.jsx
+++ my-react-portfolio/src/components/Contact/Contact.jsx
@@ -8,7 +8,7 @@ export const Contact = () => {
           <h1>Let&apos;s talk</h1>
         </div>
         <div className="contact-content">
-          <img src="src/assets/profilepic.jpg" alt="Profile Picture" />
+          <img src="https://media.licdn.com/dms/image/D4D03AQEHAtt3SlUKOQ/profile-displayphoto-shrink_800_800/0/1697014709384?e=1718236800&v=beta&t=Ikjt59Bq8qp6FoKDNJzLA2St8PS4ECaZKGMNb60D4OI" alt="Profile Picture" />
           <p>Cornelia Dahlin</p>
           <p>+46(0)766422217</p>
           <p>cornelia.dahlin@proton.me</p>
