956b1db395db13d787c80c8fd31a2f5a1db7425b
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-04-08 16:12:47 +0200

    Update ProjectData.jsx

--- my-react-portfolio/src/components/Projects/ProjectData.jsx
+++ my-react-portfolio/src/components/Projects/ProjectData.jsx
@@ -4,6 +4,7 @@ import weatherAppImage from "../../assets/weatherapp.png";
 import musicReleasesImage from "../../assets/musicreleases.png";
 import musicSurveyImage from "../../assets/musicsurvey.png";
 import happyThoughtsImage from "../../assets/happythoughts.png";
+import movieReleasesImage from "../../assets/moviereleases.png";
 
 export const ProjectData = [
   {
@@ -11,7 +12,7 @@ export const ProjectData = [
     heading: "DateBot",
     text: "Meet DateBot, your go-to companion for effortless date planning. Powered by JavaScript, HTML, and CSS, DateBot offers personalized suggestions based on your preferences, budget, and setting. ",
     tags: ["HTML5", "CSS3", "JavaScript"],
-    buttonText1: "Live demo",
+    buttonText1: "View on Netlify",
     buttonText2: "View the code",
     demoLink: "url_to_demo_2",
     codeLink: "https://github.com/lunek1/project-chatbot",
@@ -21,7 +22,7 @@ export const ProjectData = [
     heading: "Recipe Library",
     text: "The recipe library involves crafting a digital library, presenting an opportunity to delve into DOM manipulation, event handling, function definitions, conditional statements, and object and array manipulation.",
     tags: ["HTML5", "CSS3", "JavaScript"],
-    buttonText1: "Live demo",
+    buttonText1: "View on Netlify",
     buttonText2: "View the code",
     demoLink: "url_to_demo_2",
     codeLink: "https://github.com/lunek1/project-library",
@@ -31,7 +32,7 @@ export const ProjectData = [
     heading: "Weather App",
     text: "The weather app is using simple yet efficient Javascript which is designed to provide users with accurate and up-to-date weather information. Leveraging a weather API, the app displays today's weather condition and temperature along with a 4-day forecast.",
     tags: ["HTML5", "CSS3", "JavaScript", "API"],
-    buttonText1: "Live demo",
+    buttonText1: "View on Netlify",
     buttonText2: "View the code",
     demoLink: "url_to_demo_1",
     codeLink: "https://github.com/lunek1/project-weather-app",
@@ -41,7 +42,7 @@ export const ProjectData = [
     heading: "Music Releases",
     text: "Introducing a meticulously crafted music release library, driven by React and powered by an API. Immerse yourself in a curated selection of the latest musical releases, elegantly presented through a seamless interface. ",
     tags: ["HTML5", "CSS3", "React", "API"],
-    buttonText1: "Live demo",
+    buttonText1: "View on Netlify",
     buttonText2: "View the code",
     demoLink: "url_to_demo_2",
     codeLink:
@@ -52,7 +53,7 @@ export const ProjectData = [
     heading: "Music Survey",
     text: "The music survey is a React-powered platform that gathers insights into users' music preferences and habits through a questionnaire. Users answer questions about their favorite genres, artists, discovery methods, and the significance of music to them.",
     tags: ["HTML5", "CSS3", "React"],
-    buttonText1: "Live demo",
+    buttonText1: "View on Netlify",
     buttonText2: "View the code",
     demoLink: "url_to_demo_2",
     codeLink: "https://github.com/lunek1/project-survey-vite",
@@ -62,9 +63,19 @@ export const ProjectData = [
     heading: "Happy Thoughts Hub",
     text: "Happy Thoughts Hub allows users to share and view happy thoughts on a platform reminiscent of Twitter. Utilizing React state skills and an API, users can post their joyful moments and interact with others' posts by liking them.",
     tags: ["HTML5", "CSS3", "React", "API"],
-    buttonText1: "Live demo",
+    buttonText1: "View on Netlify",
     buttonText2: "View the code",
     demoLink: "url_to_demo_2",
     codeLink: "https://github.com/lunek1/project-happy-thoughts-vite",
   },
+  {
+    imageUrl: movieReleasesImage,
+    heading: "Top Movie Releases",
+    text: "This platform offers users access to top-rated movie releases from an external API. Users can scroll through movies, view IMDb ratings, genres, director and actor details, release dates, and summaries. It's an intuitive way to explore the latest films.",
+    tags: ["HTML5", "CSS3", "React", "API"],
+    buttonText1: "View on Netlify",
+    buttonText2: "View the code",
+    demoLink: "https://inquisitive-capybara-aede80.netlify.app/",
+    codeLink: "https://github.com/lunek1/project-movies-vite",
+  }
 ];
