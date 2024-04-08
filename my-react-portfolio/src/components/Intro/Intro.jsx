2b085ad3b959ca070b75f1a79758573ba6691bba
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-04-08 16:41:46 +0200

    Update Intro.jsx

--- my-react-portfolio/src/components/Intro/Intro.jsx
+++ my-react-portfolio/src/components/Intro/Intro.jsx
@@ -9,7 +9,7 @@ export const Intro = () => {
           <h1>Frontend Developer</h1>
         </div>
         <div className="introduction">
-          <img src="src/assets/profilepic.jpg" alt="Profile Picture" />
+          <img src="https://media.licdn.com/dms/image/D4D03AQEHAtt3SlUKOQ/profile-displayphoto-shrink_800_800/0/1697014709384?e=1718236800&v=beta&t=Ikjt59Bq8qp6FoKDNJzLA2St8PS4ECaZKGMNb60D4OI" alt="Profile Picture" />
           <p>
             Cornelia brings valuable expertise in website management, product
             handling, and customer communication from her service-oriented
