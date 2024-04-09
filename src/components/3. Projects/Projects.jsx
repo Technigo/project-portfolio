f00e84c2a2cafe324f8f1143685cb476c40321c7
fridaforser <fridaforser@gmail.com>
2024-04-09 10:04:19 +0200

    Updated project photos

--- src/components/3. Projects/Projects.jsx
+++ src/components/3. Projects/Projects.jsx
@@ -1,13 +1,42 @@
 import "./Projects.css";
-import happy from "./assets/happythoughts.png";
-import survey from "./assets/survey.png";
-import music from "./assets/musicreleases.png";
-import weather from "./assets/weather.png";
-import library from "./assets/library.png";
-import chatbot from "./assets/chatbot.png";
+import projects from "./projects.json";
+import netlifyImage from "./assets/netlify-img.png";
+import githubImage from "./assets/github-img.png";
 
 export const Projects = () => {
   return (
+    <div>
+      <h1 className="projects-header">Projects</h1>
+      {projects.projects.map((item) => (
+        <div className="projects" key={item.id}>
+          <img className="screenshot" src={item.image}/>
+          <div className="description">
+            <h2 className="projects-title">{item.name}</h2>
+            <p>{item.description}</p>
+            <div className="tags">
+              {item.tags.map((tag, index) => (
+                <p className="tag" key={index}>
+                  {tag}
+                </p>
+              ))}
+            </div>
+            <div className="links">
+              <a className="link" href={item.netlify} target="_blank">
+                <img className="icon" src={netlifyImage} width="25px"></img> Live demo
+              </a>
+              <a className="link" href={item.github} target="_blank">
+                <img className="icon" src={githubImage} width="25px"></img> View the code
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+/* 
+return (
     <div className="projects-div" style={{ backgroundColor: "#f5f0f2" }}>
       <h1>Projects</h1>
       <div className="projects">
@@ -97,6 +126,8 @@ export const Projects = () => {
             className="chatbot"
             src={chatbot}
           ></img>
+          <div>
+          </div>
           <h4>Title of project</h4>
           <h5>Information text about project</h5>
           <div className="tools">
@@ -104,10 +135,11 @@ export const Projects = () => {
             <p className="tool">CSS3</p>
             <p className="tool">React</p>
           </div>
-          <button>Live demo</button>
-          <button>View the code</button>
+          <div className="buttons">
+          <button className="button">Live demo</button>
+          <button className="button">View the code</button>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  );*/
