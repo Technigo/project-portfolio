8f3459d12e8bd8d719b0560acce24367247deaad
Looziolooz <lorenzo.dastoli@gmail.com>
2024-04-10 23:00:50 +0200

    added Tech section

---
+++ src/Sections/Tech/Tech.jsx
@@ -0,0 +1,15 @@
+import "./Tech.css";
+
+export const Tech = () => {
+  return (
+    <div className="tech">
+      <div className="tech-inner">
+        <h2>Tech</h2>
+        <p>
+          HTML, CSS, Javascript, ES6, JSX, React, React Hooks, Node.js, Mongo
+          DB, Web Accessability, APIs, mob-programming, pair-programming, GitHub
+        </p>
+      </div>
+    </div>
+  );
+};
