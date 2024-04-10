af268b3e52dc8fff73504ecc8f3b09a710b797ef
Looziolooz <lorenzo.dastoli@gmail.com>
2024-04-10 23:01:44 +0200

    added projects section

---
+++ src/Sections/Projects/ProjectList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import { Project } from "../../components/Project/Project";
+import "./ProjectList.css";
+import { projects } from "../../components/project.json";
+
+export const Projectlist = () => {
+  return (
+    <div className="projectlist">
+      <h2>Featured Projects</h2>
+      <div className="project-container">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            name={project.name}
+            image={project.image}
+            description={project.description}
+            tags={project.tags}
+            netlify={project.netlify}
+            github={project.github}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+Projectlist.propTypes = {
+  projects: PropTypes.array,
+};
