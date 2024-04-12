4082809e13b8e47a8f64aaec862ebb43df1f4f63
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-04-12 12:27:48 +0200

    intro layout

--- src/components/sections/Intro.jsx
+++ src/components/sections/Intro.jsx
@@ -15,15 +15,20 @@ export const Intro = () => {
           <ProfilePicture />
         </div>
         <div className="intro-text">
-          <h3>Hi, I'm Alma Herrström</h3>
-          <h1>Frontend Developer</h1>
-          <p className="hind">
-            I'm merging my background in financial audit with a passion for web
-            development. Currently in a fast-paced web development boot camp
-            learning tools like JavaScript, React and Node.js, I'm
-            blending my attention to detail and problem-solving skills to create
-            user-friendly digital experiences.
-          </p>
+          <div className="presentation">
+            <h3>Hi, I'm Alma Herrström</h3>
+            <h1>Frontend Developer</h1>
+          </div>
+          <div className="tablet-layout">
+            {/* <ProfilePicture className="tablet-profpic"/> */}
+            <p className="hind">
+              I'm merging my background in financial audit with a passion for
+              web development. Currently in a fast-paced web development boot
+              camp learning tools like JavaScript, React and Node.js, I'm
+              blending my attention to detail and problem-solving skills to
+              create user-friendly digital experiences.
+            </p>
+          </div>
         </div>
       </div>
       <a onClick={scrollToTechSection}>
