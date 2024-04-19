21c779e350e4bf2e4c9544de46642053a05bf934
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-04-19 23:02:58 +0200

    Update Contact.jsx

--- my-react-portfolio/src/components/Contact/Contact.jsx
+++ my-react-portfolio/src/components/Contact/Contact.jsx
@@ -8,10 +8,30 @@ export const Contact = () => {
           <h1>Let&apos;s talk</h1>
         </div>
         <div className="contact-content">
-          <img src="https://media.licdn.com/dms/image/D4D03AQEHAtt3SlUKOQ/profile-displayphoto-shrink_800_800/0/1697014709384?e=1718236800&v=beta&t=Ikjt59Bq8qp6FoKDNJzLA2St8PS4ECaZKGMNb60D4OI" alt="Profile Picture" />
+          <img
+            src="https://media.licdn.com/dms/image/D4D03AQEHAtt3SlUKOQ/profile-displayphoto-shrink_800_800/0/1697014709384?e=1718236800&v=beta&t=Ikjt59Bq8qp6FoKDNJzLA2St8PS4ECaZKGMNb60D4OI"
+            alt="Profile Picture"
+          />
           <p>Cornelia Dahlin</p>
           <p>+46(0)766422217</p>
           <p>cornelia.dahlin@proton.me</p>
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/in/cornelia-dahlin-940684295/" target="_blank">
+              <i className="fab fa-linkedin" style={{ color: "#bababa" }}></i>
+            </a>
+            <a href="https://github.com/lunek1" target="_blank">
+              <i className="fab fa-github" style={{ color: "#bababa" }}></i>
+            </a>
+            <a href="https://stackoverflow.com/users/23121233/cornelia" target="_blank">
+              <i
+                className="fab fa-stack-overflow"
+                style={{ color: "#bababa" }}
+              ></i>
+            </a>
+            <a href="https://www.instagram.com/prettycultivated/" target="_blank">
+              <i className="fab fa-instagram" style={{ color: "#bababa" }}></i>
+            </a>
+          </div>
         </div>
       </div>
       <footer className="footer">
