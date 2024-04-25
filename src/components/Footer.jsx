b7e8e1577525242f476f043b94550962744162b3
SofieFerrari <sofiestrahl@gmail.com>
2024-04-25 12:23:08 +0200

    added projects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,12 @@
 import './Heading.css'
 
-const footerBorder = 'Sofie Ferrari Strahl ∙ Frontend Developer ∙ '
+const footerBorder = 'Sofie Ferrari Strahl ∙ Frontend Developer '
 
 export const Footer = () => {
 	return (
 		<>
 			<div className="footer">
-				<h3>
-					{footerBorder} {footerBorder}
-					{footerBorder}
-				</h3>
+				<h3>{footerBorder}</h3>
 			</div>
 		</>
 	)
