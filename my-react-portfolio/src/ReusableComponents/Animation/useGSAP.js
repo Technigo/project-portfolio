cfdda2c7c48d13ae9db2d656abf431e3e6c24f80
hamdiolad <oladhamdi@gmail.com>
2024-04-26 17:16:29 +0200

    annimation

--- my-react-portfolio/src/ReusableComponents/Animation/useGSAP.js
+++ my-react-portfolio/src/ReusableComponents/Animation/useGSAP.js
@@ -1,5 +1,35 @@
 import { useEffect } from "react";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export const useGSAP = (ref, animationOptions, scrollTriggerOptions = null) => {
+  useEffect(() => {
+    if (ref.current) {
+      gsap.registerPlugin(ScrollTrigger);
+
+      const animation = gsap.to(ref.current, {
+        ...animationOptions,
+        scrollTrigger: scrollTriggerOptions
+          ? {
+              trigger: ref.current,
+              ...scrollTriggerOptions,
+            }
+          : null,
+      });
+
+      // Clean up function to kill animations and ScrollTriggers when component unmounts or ref changes
+      return () => {
+        if (scrollTriggerOptions) {
+          animation.scrollTrigger.kill();
+        }
+        animation.kill();
+      };
+    }
+  }, [ref, animationOptions, scrollTriggerOptions]); // Include scrollTriggerOptions in dependencies array
+};
+
+/*import { useEffect } from "react";
+import { gsap } from "gsap";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -28,3 +58,4 @@ export const useGSAP = (ref, animationOptions, scrollTriggerOptions = null) => {
     }
   }, [ref, animationOptions, scrollTriggerOptions]); // Include scrollTriggerOptions in dependencies array
 };
+*/
