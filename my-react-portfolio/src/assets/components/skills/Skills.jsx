557cdfe327393fd6fdcc9a26936b820cdee31d81
Trista Shan <trista8703@hotmail.com>
2024-05-03 23:03:37 +0300

    skills done

--- my-react-portfolio/src/assets/components/skills/Skills.jsx
+++ my-react-portfolio/src/assets/components/skills/Skills.jsx
@@ -1,35 +1,47 @@
-import {PageTopic} from "../PageTopic"
-import "./skills.css"
+import { PageTopic } from "../PageTopic";
+import "./skills.css";
 
 export const Skills = () => {
-    return (
-      <>
-        <section className="skill-wrapper">
-          <PageTopic className={"skills"} pageTopic={"Skills"} />
-          <div className="skill-container" aria-label="Tech skills"> 
-            <div className="code">
-              <h4 className="code-head">Code</h4>
+  return (
+    <>
+      <section className="skill-wrapper">
+        <PageTopic className={"skills"} pageTopic={"Skills"} />
+        <div className="skill-container" aria-label="Tech skills">
+          <div className="skillbox">
+            <h4 className="code-head">Code</h4>
+            <div className="listbox">
               <p>HTML5</p>
               <p>CSS3</p>
               <p>Javascript ES6</p>
               <p>React</p>
               <p>Styled Components</p>
+              <p>Tailwind</p>
+              <p>Express.js</p>
               <p>GitHub</p>
+              <p>Node.js</p>
             </div>
-            <div className="toolbox">
-              <h4 className="toolbox-head">Toolbox</h4>
+          </div>
+          <div className="skillbox">
+            <h4 className="toolbox-head">Toolbox</h4>
+            <div className="listbox">
               <p>Postman</p>
+              <p>Adobe Photoshop</p>
+              <p>Adobe Illustrator</p>
               <p>Figma</p>
               <p>Slack</p>
               <p>APIs</p>
               <p>Git</p>
             </div>
-            <div className="upcoming">
-              <h4 className="upcoming-head">Upcoming</h4>
-              <p>Node.js</p>
+          </div>
+          <div className="skillbox">
+            <h4 className="upcoming-head">Upcoming</h4>
+            <div className="listbox">
+              <p>Mongo DB</p>
             </div>
-            <div className="more">
-              <h4 className="more-head">More</h4>
+          </div>
+          <div className="skillbox">
+            <h4 className="more-head">More</h4>
+            <div className="listbox">
               <p>Concept development</p>
               <p>Agile methodology</p>
               <p>Accessibility</p>
@@ -37,7 +49,8 @@ export const Skills = () => {
               <p>Pair programming</p>
             </div>
           </div>
-        </section>
-      </>
-    );
-}
+        </div>
+      </section>
+    </>
+  );
+};
