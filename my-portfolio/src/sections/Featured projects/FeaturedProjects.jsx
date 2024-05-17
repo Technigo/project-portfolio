0b7a468d721c9576b9939584438dafde10343e9d
Erica Mechler <erica.mechler1@gmail.com>
2024-05-17 10:00:11 +0200

    added imagepath etc for studio-mama project

--- my-portfolio/src/sections/Featured projects/FeaturedProjects.jsx
+++ my-portfolio/src/sections/Featured projects/FeaturedProjects.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 
 import { Button } from "../../components/FormElements/Button";
 import { MainHeading } from "../../components/TextElements/MainHeading";
