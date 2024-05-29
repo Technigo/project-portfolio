52890d30f087edd3ef1c7557ae21d2b603b08612
Linnea Johansson <Linneajoh@gmail.com>
2024-05-29 20:28:29 +0200

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Headline from './Components/Headline'; 
 import Tech from './Components/Tech';
 import Projects from './Components/Projects';
-import Words from './Components/Words';
 import Skills from './Components/Skills';
 import Talk from './Components/Talk';
 import Footer from './Components/Footer';
@@ -14,7 +13,6 @@ const App = () => {
       <Headline />
       <Tech />
       <Projects />
-      <Words />
       <Skills />
       <Talk />
       <Footer />
