2e83f31d6f3eb4530a906a761cefdb4f6727e1cb
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-06-16 14:37:45 +0200

    added new project and fixed fetch

--- my-react-portfolio/src/Components/ArticleSection/ArticlesSection.jsx
+++ my-react-portfolio/src/Components/ArticleSection/ArticlesSection.jsx
@@ -1,37 +1,48 @@
 import { useEffect, useState } from 'react'
 import { ArticleCard } from '../ArticleCard/ArticleCard'
-
 import articleImages from '../article-images.json'
 import './articleSection.css'
+
 export const ArticlesSection = () => {
   const [articles, setArticles] = useState([])
+
   useEffect(() => {
-    // fetching medium api👇🏻
-    fetch(
-      'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/@lavicafra/feed'
-    )
-      .then((resp) => resp.json())
-      .then((data) => {
-        const sortedList = data.items.sort(
-          (a, b) => new Date(b.pubDate) - new Date(a.pubDate)
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch(
+          'https://v1.nocodeapi.com/vittoriam/medium/WjrrDaitUqQiqAzo'
+        )
+        if (!response.ok) {
+          throw new Error('Failed to fetch articles')
+        }
+        const data = await response.json()
+
+        // Sort articles by publication date (assuming 'published' is the date field)
+        const sortedArticles = data.sort(
+          (a, b) => new Date(b.published) - new Date(a.published)
         )
 
-        // matching the images of the json file trought the article title
-        const pairedArticles = sortedList.map((article) => {
-          const matchingImage = articleImages.find(
+        // Match articles with corresponding images and descriptions
+        const matchedArticles = sortedArticles.map((article) => {
+          const matchedImage = articleImages.find(
             (img) => img.articleTitle === article.title
           )
-          // Assigning image and description properties to each article, if found
           return {
             ...article,
-            image: matchingImage?.image || '',
-            description: matchingImage?.description || '',
+            image: matchedImage ? matchedImage.image : '',
+            description: matchedImage ? matchedImage.description : '',
           }
         })
-        setArticles(pairedArticles)
-      })
-      .catch((error) => console.error(error))
+
+        setArticles(matchedArticles)
+      } catch (error) {
+        console.error('Error fetching articles:', error)
+      }
+    }
+
+    fetchArticles()
   }, [])
+
   return (
     <div className="articleSection">
       <h1>My Words</h1>
