0192c6b5df36742b96c3403cc018826c90a9057a
Erica Mechler <erica.mechler1@gmail.com>
2024-07-05 16:19:01 +0200

    added some skills & updated the text to be web dev instead of only frontdev

--- my-portfolio/src/components/TickerTape/TickerTape.jsx
+++ my-portfolio/src/components/TickerTape/TickerTape.jsx
@@ -1,12 +1,13 @@
-import "./TickerTape.css";
 import { Subheading } from "../TextElements/Subheading";
+
+import "./TickerTape.css";
 
 export const TickerTape = () => {
   return (
     <div aria-hidden="true" className="ticker-tape">
       <Subheading
         text={
-          "Erica Mechler · Frontend Developer · Erica Mechler· Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer · Erica Mechler · Frontend Developer"
+          "Erica Mechler · Web Developer · Erica Mechler· Frontend Developer · Erica Mechler ·  Web Developer · Erica Mechler · Frontend Developer · Erica Mechler · Web Developer · Erica Mechler · Frontend Developer · Erica Mechler · Web Developer · Erica Mechler · Frontend Developer · Erica Mechler · Web Developer · Erica Mechler · Frontend Developer · Erica Mechler · Web Developer · Erica Mechler · Frontend Developer"
         }
       />
     </div>
--- my-portfolio/src/sections/Introduction/Introduction.jsx
+++ my-portfolio/src/sections/Introduction/Introduction.jsx
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 
 export const Introduction = ({ avatarUrl }) => {
   const introText =
-    "As a Front End Developer with a background in project management, I blend technical expertise with leadership skills to create exceptional digital solutions. I specialize in crafting intuitive user interfaces and driving projects forward. Let's collaborate to bring your vision to life!";
+    "As a Web Developer with a background in project management, I blend technical expertise with leadership skills to create exceptional digital solutions. I specialize in crafting intuitive user interfaces and driving projects forward. Let's collaborate to bring your vision to life!";
 
   return (
     <section className="introduction-container">
@@ -18,7 +18,7 @@ export const Introduction = ({ avatarUrl }) => {
       />
       <div className="headings">
         <Subheading text={"Hi, I'm Erica Mechler"} />
-        <h1 className="introduction-heading">Frontend Developer</h1>
+        <h1 className="introduction-heading">Web Developer</h1>
       </div>
       <div className="intro-text">
         <NormalText text={introText} />
