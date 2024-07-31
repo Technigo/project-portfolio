0f6049ba6a9762d4ec40229c499aaaaa168934de
Lovisa <lovisa.aaberg@gmail.com>
2024-08-01 00:32:01 +0200

    Added project

--- src/components/4.Articles/ArticlesContainer.jsx
+++ src/components/4.Articles/ArticlesContainer.jsx
@@ -1,16 +1,16 @@
-import { Articles } from "./Articles";
-import "./ArticlesContainer.css";
+// import { Articles } from "./Articles";
+// import "./ArticlesContainer.css";
 
-export const ArticlesContainer = () => {
-  return (
-    <>
-      <div className="wavy-divider"></div>
-      <div className="articles-container">
-        <div className="article-box">
-          <h2 className="articles-heading">My words</h2>
-          <Articles />
-        </div>
-      </div>
-    </>
-  );
-};
+// export const ArticlesContainer = () => {
+//   return (
+//     <>
+//       <div className="wavy-divider"></div>
+//       <div className="articles-container">
+//         <div className="article-box">
+//           <h2 className="articles-heading">My words</h2>
+//           <Articles />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
