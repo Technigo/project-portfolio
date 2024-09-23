2bef895882aeb2272f0d79dd6cc5befa51d17298
Lovisa <lovisa.aaberg@gmail.com>
2024-09-23 13:56:12 +0200

    Changes to text

--- src/components/1.Introduction/Introduction.jsx
+++ src/components/1.Introduction/Introduction.jsx
@@ -14,9 +14,9 @@ export const Introduction = () => {
         <h1 className="introduction-heading">Hi 👋, I&apos;m Lovisa Åberg</h1>
         <h2 className="introduction-subheading">Frontend Developer</h2>
         <p className="introduction-text">
-          By combining my UX design background with front-end development, I
-          ensure that every interface I build is not only visually appealing but
-          also highly functional, accessible, and user-friendly.
+          By integrating my UX design background with front-end development, I
+          create functional, accessible, and user-centered interfaces that
+          deliver seamless digital experiences.
         </p>
       </section>
       <section className="arrow-container">
