fb24ed1896419ef3f30d0e4437d1614a4b82f6b4
HeleneWestrin <helene.westrin@alphadev.se>
2024-10-29 15:19:29 +0100

    Added fonts. Created variables for typography, spacing, etc.

--- helene-portfolio/src/App.jsx
+++ helene-portfolio/src/App.jsx
@@ -1,14 +1,43 @@
+import { useState, useEffect } from "react";
+
+/* Components */
 import Hero from "./components/layout/Hero";
 import TechWords from "./components/layout/TechWords";
 import Projects from "./components/layout/Projects";
 import SkillLists from "./components/layout/SkillLists";
 import Articles from "./components/layout/Articles";
 import Footer from "./components/layout/Footer";
-import "./App.css";
+
+const getPreferredTheme = () => {
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme) return storedTheme;
+
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  return prefersDark ? "dark" : "light";
+};
 
 function App() {
+  const [theme, setTheme] = useState(getPreferredTheme);
+
+  // Update the data-theme attribute on the html element whenever the theme changes
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme); // Save user choice in local storage
+  }, [theme]);
+
   return (
     <>
+      <nav>
+        Helene
+        <button
+          onClick={() =>
+            setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
+          }
+          aria-label={`Switch to ${theme === "light" ? "dark" : "light"} mode`}
+        >
+          Toggle Theme
+        </button>
+      </nav>
       <main>
         <Hero />
         <TechWords />
