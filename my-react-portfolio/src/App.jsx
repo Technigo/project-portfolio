f66db0ceb9f65dd82bf4fd9251088d4b4b8efcf7
JoyceKuode <joyce.kuo@me.com>
2024-10-31 15:33:32 +0100

    Create basic structure for main content

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
+import { HeaderSection } from "./sections/HeaderSection"
+import { ImageSection } from "./sections/ImageSection"
+import { BioSection } from "./sections/BioSection"
+import { ProjectSection } from "./sections/ProjectSection"
+import { TechSection } from "./sections/TechSection"
+import { SkillSection } from "./sections/SkillSection"
+import { ArticleSection } from "./sections/ArticleSection"
+import { ContactSection } from "./sections/ContactSection"
+
+const App = () => {
 
   return (
-    <>
-      <div>
+    <main>
+      <HeaderSection />
+      <ImageSection />
+      <BioSection />
+      <ProjectSection />
+      <TechSection />
+      <SkillSection />
+      <ArticleSection />
+      <ContactSection />
+
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,8 +41,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </p> */}
+    </main>
   )
 }
 
