1fff0f68afd46651617f64946e0627c72c76d028
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-01 11:29:02 +0100

    First commit, structure components

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      {Header}
+      < Header text="Welcome to my page" />
+      {/* <TopSection /> */}
+    </div>
+  )
+}
+
+export default App
+
