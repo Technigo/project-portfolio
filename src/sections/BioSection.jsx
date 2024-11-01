e6bd82a9442ce67daeb0f0669f8c7059f283180b
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-11-01 21:26:31 +0100

    - added bio Image

--- src/sections/BioSection.jsx
+++ src/sections/BioSection.jsx
@@ -1,4 +1,5 @@
 import { BioCard } from "../components/BioCard/BioCard"
+import { BioImage } from "../ui/BioImage/BioImage"
 import { Grid } from "../ui/Grid/Grid"
 import "../ui/Grid/Grid.css"
 
@@ -6,6 +7,7 @@ export const BioSection = () => {
     return (
         <Grid background="neutral">
             <BioCard />
+            <BioImage />
         </Grid>
     )
 }
--- src/ui/BioImage/BioImage.jsx
+++ src/ui/BioImage/BioImage.jsx
@@ -1,9 +1,18 @@
-export const BioImage = () => {
+import PropTypes from 'prop-types'
+import BioImageDown from "../../assets/BioImageDown.png"
+import BioImageUp from "../../assets/BioImageUp.png"
+import "./BioImage.css"
+
+export const BioImage = ({ src, alt }) => {
   return (
     <div className="bio-image">
-      <image></image>
-      <image></image>
-      <image></image>
+      <img src={BioImageDown} alt={alt} className='image-down' />
+      <img src={BioImageUp} alt={alt} className='image-up' />
     </div>
   )
+}
+
+BioImage.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
 }
