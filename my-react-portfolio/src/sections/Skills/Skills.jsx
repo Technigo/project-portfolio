b797f7e96955de648dd9a46eae87d438e71abf02
xingyin2024 <xingyin.thu@gmail.com>
2024-11-03 00:43:37 +0800

    update readme

--- my-react-portfolio/src/sections/Skills/Skills.jsx
+++ my-react-portfolio/src/sections/Skills/Skills.jsx
@@ -4,9 +4,8 @@ import { SkillCard } from "../../components/SkillCard";
 export const Skills = () => { 
   return (
     <section className="skills-section">
-              <h2 className="skills-heading">Skills</h2>
+      <h2 className="skills-heading">Skills</h2>
       <div className="skills-container">
-
         <SkillCard />
       </div>
     </section>    
