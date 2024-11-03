c7e78b85b68e7de4b08bb919638368c3f2333a8a
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-11-03 20:47:19 +0100

    - css

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -3,37 +3,13 @@ import { CardImage } from "../../ui/CardImage/CardImage"
 import { ProjectButton } from "../../ui/ProjectButton/ProjectButton"
 import { HeaderThree } from "../../ui/Typography/Typography"
 import "./ProjectCard.css"
-import repoImages from "../../../src/projects.json"
+import projectData from "../../../src/projects.json"
 
 export const ProjectCard = () => {
   const [projects, setProjects] = useState([])
-  const [repos, setRepos] = useState([])
-
   useEffect(() => {
-
-    const fetchProjects = async () => {
-      const response = await fetch("../../../src/projects.json")
-      const projectData = await response.json()
-      setProjects(projectData)
-    }
-
-    fetchProjects()
-  }, [])
-
-  useEffect(() => {
-    const fetchRepos = async () => {
-      try {
-        const response = await fetch("https://api.github.com/users/gittebe/repos")
-        if (!response.ok) {
-          throw new Error("Network response was not ok" + response.statusText);
-        }
-        const data = await response.json();
-        setRepos(data);
-      } catch (error) {
-        console.error("There has been a problem with your fetch operation:", error);
-      }
-    };
-    fetchRepos()
+    setProjects(projectData)
+    console.log(projectData)
   }, [])
 
   return (
@@ -47,20 +23,6 @@ export const ProjectCard = () => {
           <div className="button-container">
             <ProjectButton label="Live demo" background="primary"></ProjectButton>
             <ProjectButton label="View Code" background="neutral"></ProjectButton>
-          </div>
-          <div className="github-repos">
-            <h4>GitHub Repositories:</h4>
-            <ul>
-              {repos.map((repo) => {
-                const repoImage = repoImages.find((img) => img.repoName === repo.name);
-                return (
-                  <li key={repo.id}>
-                    <CardImage cardImage={repoImage?.imageUrl} alt={repo.name} />
-                    {repo.name}
-                  </li>
-                );
-              })}
-            </ul>
           </div>
         </div>
       ))}
