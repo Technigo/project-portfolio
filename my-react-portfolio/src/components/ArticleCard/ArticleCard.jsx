32c128b8b3e23375d9bcc68b6f249d3456b4d0af
JoyceKuode <joyce.kuo@me.com>
2024-11-03 17:44:26 +0100

    Add article section styling for desktop

--- my-react-portfolio/src/components/ArticleCard/ArticleCard.jsx
+++ my-react-portfolio/src/components/ArticleCard/ArticleCard.jsx
@@ -9,15 +9,17 @@ export const ArticleCard = ({ image, title, date, description, link }) => {
       <div className="article-card-image-container">
         <img src={image} alt={title} className="article-card-image" />
       </div>
-      <div className="article-card-info">
-        <Tag text={date} variant="white-border" className="article-tag" />
-        <header>
-          <h3 className="article-card-title">{title}</h3>
-        </header>
-        <p className="article-card-description">
-          {description}</p>
+      <div className="article-card-content">
+        <div className="article-card-info">
+          <Tag text={date} variant="white-border" className="article-tag" />
+          <header>
+            <h3 className="article-card-title">{title}</h3>
+          </header>
+          <p className="article-card-description">
+            {description}</p>
+        </div>
+        <Button link={link} text="Read article" icon={webIcon} variant="black-on-white" />
       </div>
-      <Button link={link} text="Read article" icon={webIcon} variant="black-on-white" />
     </article>
   )
 }
