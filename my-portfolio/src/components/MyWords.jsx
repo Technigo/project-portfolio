66226c49faa2e9b51bcf49c60ec37056fd22b592
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-03 14:10:43 +0100

    MyWords component update

--- my-portfolio/src/components/MyWords.jsx
+++ my-portfolio/src/components/MyWords.jsx
@@ -8,10 +8,34 @@ const MyWords = () => {
   return (
     <div className="my-words-container">
       <h2>My Words</h2>
-      <div className="images-container">
-        <img src={ImagePink} alt="Pink themed image" className="image-pink" />
-        <img src={ImageGrey} alt="Grey themed image" className="image-grey" />
-        <img src={ImageRed} alt="Red themed image" className="image-red" />
+
+      <div className="words-images">
+        <div className="words-item">
+          <img src={ImagePink} alt="Pink themed image" className="words-image" />
+          <div className="word-text">
+            <h3>How to stop being scared:
+            Learning to love code</h3>
+            <p>The chat bot app is a conversational AI-powered tool designed to enhance user experience by providing instant, personalized, and automated responses to user inquiries.</p>          
+          </div>
+        </div>
+
+        <div className="words-item">
+          <img src={ImageGrey} alt="Grey themed image" className="words-image" />
+          <div className="word-text">
+            <h3>How to stop being scared:
+            Learning to love code</h3>
+            <p>The chat bot app is a conversational AI-powered tool designed to enhance user experience by providing instant, personalized, and automated responses to user inquiries.</p>  
+          </div>
+        </div>
+
+        <div className="words-item">
+          <img src={ImageRed} alt="Red themed image" className="words-image" />
+          <div className="word-text">
+            <h3>How to stop being scared:
+            Learning to love code</h3>
+            <p>The chat bot app is a conversational AI-powered tool designed to enhance user experience by providing instant, personalized, and automated responses to user inquiries.</p>  
+          </div>
+        </div>
       </div>
     </div>
   );
