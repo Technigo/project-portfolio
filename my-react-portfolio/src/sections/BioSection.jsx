a68657f9d53be559424e8de64e36e00c78dc4040
JoyceKuode <joyce.kuo@me.com>
2024-11-03 23:41:04 +0100

    Update bio section with new bio

--- my-react-portfolio/src/sections/BioSection.jsx
+++ my-react-portfolio/src/sections/BioSection.jsx
@@ -9,7 +9,7 @@ export const BioSection = () => {
         <div className="bio-container">
           <SectionTitle>Bio</SectionTitle>
           <BodyText>
-            Joyce is an exceptional developer known for her innovative solutions and exceptional coding abilities. She creates user-friendly applications and solves complex issues with ease. Her drive for excellence makes her a valuable asset to any project and a standout in the technology field.
+            Joyce is an exceptional developer, with a unique blend of creativity and analytical thinking shaped by her unique experiences in the music industry. She excels at creating user-friendly applications and enjoys tackling complex problems with attention to detail. Known for her strong communication skills and collaborative spirit, Joyce brings a fresh perspective to every project, making her a valuable asset and a standout in the tech industry.
           </BodyText>
         </div>
       </section>
