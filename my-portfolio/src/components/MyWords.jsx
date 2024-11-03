180d5f8e8690c59c37cc0e90c16d6a22b1b59747
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-11-03 11:48:17 +0100

    Update ProjectInfoCard styling

--- my-portfolio/src/components/MyWords.jsx
+++ my-portfolio/src/components/MyWords.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./MyWords.css";
+import ImagePink from "../assets/ImagePink.jpeg";
+import ImageGrey from "../assets/ImageGrey.jpeg";
+import ImageRed from "../assets/ImageRed.jpeg";
 
 const MyWords = () => {
   return (
-    <h2 className="my-words-container">My Words</h2>
+    <div className="my-words-container">
+      <h2>My Words</h2>
+      <div className="images-container">
+        <img src={ImagePink} alt="Pink themed image" className="image-pink" />
+        <img src={ImageGrey} alt="Grey themed image" className="image-grey" />
+        <img src={ImageRed} alt="Red themed image" className="image-red" />
+      </div>
+    </div>
   );
 };
 
