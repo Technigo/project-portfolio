e7785364b878fde66de6ecb9aa1a062881fb584c
Cholpon Abdyrakhmanova <cholpoun@gmail.com>
2024-11-03 21:54:28 +0100

    n

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  root: '.', // This ensures the root directory is the correct level
-  base: './', // Serve files relative to the project root
-});
