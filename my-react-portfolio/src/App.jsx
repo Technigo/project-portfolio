cece01a6b143c468356596e9e21d63c2282fdab9
Cholpon Abdyrakhmanova <cholpoun@gmail.com>
2024-11-03 18:40:25 +0100

    added hero section

--- my-react-portfolio/src/App.jsx
+++ my-react-portfolio/src/App.jsx
@@ -1,16 +1,22 @@
-import "./App.css"
-import { ArticleSection } from "./sections/ArticleSection"
-import { ProjectSection } from "./sections/ProjectSection"
+import "./App.css";
+import HeroSection from "./sections/HeroSection";
+import { ArticleSection } from "./sections/ArticleSection";
+import { ProjectSection } from "./sections/ProjectSection";
 
 const App = () => {
-
   return (
-  <main>
-    <h1> My portfolio</h1>
-    <ArticleSection/>
-    <ProjectSection/>
-  </main>
-  )
-}
+    <main>
+      <HeroSection
+        name="Sara Svensson"
+        title="Frontend Developer"
+        description="Sara is an exceptional developer known for her innovative solutions and exceptional coding abilities. She creates user-friendly applications and solves complex issues with ease. Her drive for excellence makes her a valuable asset to any project and a standout in the technology field."
+        imageSrc="/path/to/image.jpg" // Replace with the correct path to the image
+      />
+      <h1>My portfolio</h1>
+      <ArticleSection />
+      <ProjectSection />
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ my-react-portfolio/src/sections/HeroSection.jsx
@@ -0,0 +1,24 @@
+import './HeroSection.css'; 
+import scrollArrow from '../assets/Arrow.svg';
+
+const HeroSection = ({ name, title, description, imageSrc }) => {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Hi, I’m {name}</h1>
+        <h2 className="hero-title">{title}</h2>
+        <div className="hero-description">
+          <img src={imageSrc} alt={`${name}'s photo`} className="hero-image" />
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="hero-scroll">
+        <img src={scrollArrow} alt="Scroll down arrow" className="scroll-arrow-image" />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
+
+
