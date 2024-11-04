7a42e1131a84089f85148ef7be1eb16cfab577f9
jacquelinekellyhunt <kelly@cosafe.com>
2024-11-04 11:42:50 -0400

    Start Projects component

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import Bio from './components/Introduction/Bio';
 import Contact from './components/Contact/Contact'; 
 import Tech from './components/Tech/Tech';
 import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
 import './App.css';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <Img />
       <Bio />
+      <Projects />
       <Skills /> 
       <Tech /> 
       <Contact /> 
