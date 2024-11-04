031e6667459a440dc4ebd9c7fc266f9155376624
Jonash189 <jonas.hellstrom89@gmail.com>
2024-11-04 10:06:53 +0100

    Adding last styling

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -5,33 +5,33 @@ import './Articles.css';
 
 export const Articles = () => {
     return (
+        <div>
+            <h2 className="articles-title">My words</h2>
+            <div className="articles-container">
+                {articlesData.articles.map((article, index) => (
+                    <div key={index} className="article-card">
+                        <img src={article.image} alt={article.title} className="article-image" />
 
-        <div className="articles-container">
-            <h2 className="featured-title">My words</h2>
+                        {/* Placera tags direkt efter bilden */}
+                        <div className="article-tags">
+                            {article.tags.map((tag, tagIndex) => (
+                                <span key={tagIndex} className="article-tag">{tag}</span>
+                            ))}
+                        </div>
 
-            {articlesData.articles.map((article, index) => (
-                <div key={index} className="article-card">
-                    <img src={article.image} alt={article.title} className="article-image" />
+                        <div className="article-content">
+                            <h1 className="article-title">{article.title}</h1>
+                            <p className="article-body">{article.body}</p>
 
-                    {/* Placera tags direkt efter bilden */}
-                    <div className="article-tags">
-                        {article.tags.map((tag, tagIndex) => (
-                            <span key={tagIndex} className="article-tag">{tag}</span>
-                        ))}
-                    </div>
-
-                    <div className="article-content">
-                        <h1 className="article-title">{article.title}</h1>
-                        <p className="article-body">{article.body}</p>
-
-                        <div className="article-buttons">
-                            <button className="article-button">
-                                <a href={article.article} target="_blank" rel="noopener noreferrer" className="article-link"><MdArticle />Read article</a>
-                            </button>
+                            <div className="article-buttons">
+                                <button className="article-button">
+                                    <a href={article.article} target="_blank" rel="noopener noreferrer" className="article-link"><MdArticle />Read article</a>
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 };
