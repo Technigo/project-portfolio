7ea4b33e2ad624dee7bbab6cb0f5c6f2aa250bf0
Zoe <zoe.lindqvist@gmail.com>
2024-11-07 11:28:35 +0100

    change my words

--- src/sections/myWords/articlesData.js
+++ src/sections/myWords/articlesData.js
@@ -9,13 +9,13 @@ export const myWords = [
     link: "https://www.linkedin.com/pulse/from-teaching-tech-my-career-transition-journey-zoe-lindqvist-ywr8f/?trackingId=zqwZMpPzSN6nfHgb9hugSg%3D%3D",
   },
 
-  {
-    title: `From Teaching to Tech: My Career Transition`,
-    imageUrl: "/assets/article-img/career-transition.jpg",
-    tagText: "October, 2024",
-    description:
-      " For anyone considering a career change, remember: It's never too late to pivot 🚀 Real growth happens when you step out of your comfort zone 💫",
+  // {
+  //   title: `From Teaching to Tech: My Career Transition`,
+  //   imageUrl: "/assets/article-img/career-transition.jpg",
+  //   tagText: "October, 2024",
+  //   description:
+  //     " For anyone considering a career change, remember: It's never too late to pivot 🚀 Real growth happens when you step out of your comfort zone 💫",
 
-    link: "https://www.linkedin.com/pulse/from-teaching-tech-my-career-transition-journey-zoe-lindqvist-ywr8f/?trackingId=zqwZMpPzSN6nfHgb9hugSg%3D%3D",
-  },
+  //   link: "https://www.linkedin.com/pulse/from-teaching-tech-my-career-transition-journey-zoe-lindqvist-ywr8f/?trackingId=zqwZMpPzSN6nfHgb9hugSg%3D%3D",
+  // },
 ];
