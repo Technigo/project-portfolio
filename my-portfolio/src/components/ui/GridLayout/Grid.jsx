7396213ab567644602c83a16d36164e1e1f3290c
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-11-11 17:13:56 +0100

    added props in gridsection.jsx

--- my-portfolio/src/components/ui/GridLayout/Grid.jsx
+++ my-portfolio/src/components/ui/GridLayout/Grid.jsx
@@ -1,9 +1,35 @@
+/* eslint-disable react/prop-types */
 import "./Grid.css"
 
-export const Grid = ({ children, background }) => {
-  return (
-    <section className={`grid-section ${background}-background`}>
-      {children}
-    </section>
-  )
-}
+  const spacing = {
+    xsmall: "16px",
+    small: "24px",
+    medium: "32px",
+    large: "64px",
+    xlarge: "128px",
+    unique: "24px",
+  };
+
+
+  export const Grid = ({ sectionType, gap, paddingTop, paddingBottom, paddingLeft, paddingRight, children }) => {
+    const gapValue = sectionType === "article" ? spacing.xlarge : spacing.large;
+    const paddingTopValue = sectionType === "article" ? spacing.large : spacing.xlarge;
+    const paddingBottomValue = sectionType === "article" ? spacing.xlarge : spacing.large;
+    const paddingLeftValue = sectionType === "article" ? spacing.small : spacing.xsmall;
+    const paddingRightValue = sectionType === "article" ? spacing.small : spacing.xsmall;
+
+    return (
+      <div
+        style={{
+          display: "grid",
+          gap: spacing[gap] || gapValue,
+          paddingTop: spacing[paddingTop] || paddingTopValue,
+          paddingBottom: spacing[paddingBottom] || paddingBottomValue,
+          paddingLeft: spacing[paddingLeft] || paddingLeftValue,
+          paddingRight: spacing[paddingRight] || paddingRightValue,
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
