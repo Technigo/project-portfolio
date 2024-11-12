32c6d539540b1eeea9cda72647e9f5397b64fb2f
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-11-12 10:56:36 +0100

    - deleted empty css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BioSection } from "./sections/BioSection/BioSection"
 import { SkillsSection } from "./sections/SkillsSection/SkillsSection"
 import { ProjectsSection } from "./sections/ProjectsSection/ProjectsSection"
