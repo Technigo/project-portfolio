a6827637277e52270753428c1f58d02dec960a49
Anna <annah80@gmail.com>
2024-11-12 16:02:33 +0100

    updated intro-text

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
       </h1>
       <h1 className="name">Anna Hansen</h1>
       <p className="intro-text">
-        Creative Frontend Developer with a background in Finance and Business Operations.
+        Creative Frontend Developer with a Background in Finance and Business Operations.
       </p>
     </div>
   );
