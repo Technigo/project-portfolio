74d4602e2930633572dd806f0c1997b94d0cbb25
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-11-12 10:16:18 +0100

    - worked on desktop layout of the bio section

--- src/sections/BioSection/BioSection.jsx
+++ src/sections/BioSection/BioSection.jsx
@@ -1,19 +1,16 @@
 import { BioCard } from "../../components/BioCard/BioCard"
 import { BioImage } from "../../ui/BioImage/BioImage"
-import { Grid } from "../../ui/Grid/Grid"
 import "./BioSection.css"
 
 export const BioSection = () => {
   return (
-    <Grid background="neutral">
-      <section className="bio-section">
-        <div className="bio-header">
-          <BioCard />
-        </div>
-        <div className="bio-body">
-          <BioImage />
-        </div>
-      </section>
-    </Grid>
+    <section className="bio-section">
+      <div className="bio-header">
+        <BioCard />
+      </div>
+      <div className="bio-body">
+        <BioImage />
+      </div>
+    </section>
   )
 }
