32e1c0db0973926cf3f759083920e5d9b5300ca2
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-11-12 03:04:11 +0100

    added styling in gallary for the images

--- my-portfolio/src/components/sections/LibrarySection/Library.jsx
+++ my-portfolio/src/components/sections/LibrarySection/Library.jsx
@@ -7,9 +7,11 @@ export const Library = () => {
   return (
     <div className="library-container">
       <div className="image-container">
+        <div className="image-wrapper"> 
         <img className="img1" src={images.img1} alt="Image 1" />
         <img className="img2" src={images.img2} alt="Image 2" />
         <img className="img3" src={images.img3} alt="Image 3" />
+        </div>
       </div>
     </div>
   );
