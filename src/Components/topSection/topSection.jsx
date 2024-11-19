19ca01046c44438e2e134c4ce07649e0483d8bcf
Maryyy-ux <maria.fernandez703@yahoo.com>
2024-11-19 22:42:38 +0100

    Add files via upload

--- src/Components/topSection/topSection.jsx
+++ src/Components/topSection/topSection.jsx
@@ -1,9 +1,9 @@
 // src/components/topSection/topSection.jsx
 
 import styled from 'styled-components';
-import rectangle18black from '/public/images/rectangle-18black.jpg';
-import rectangle16miscelaneous from '/public/images/rectangle-16miscelaneous.jpg';
-import rectangle17red from '/public/images/rectangle-17red.jpg';
+import rectangle18black from '/public/images/miscelaneousblack.jpg';
+import rectangle16miscelaneous from '/public/images/purpleorso.jpg';
+import rectangle17red from '/public/images/red.jpg';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
@@ -59,51 +59,51 @@ const AdditionalText = styled.div`
 `;
 
 const TopSection = () => {
-    // Array de imágenes para el carousel
-    const carouselImages = [
-        rectangle18black,
-        rectangle16miscelaneous,
-        rectangle17red,
+  // Array de imágenes para el carousel
+  const carouselImages = [
+    rectangle18black,
+    rectangle16miscelaneous,
+    rectangle17red,
 
-    ];
+  ];
 
-    return (
-        <TopSectionContainer>
-            {/* Texto de introducción */}
-            <IntroText>
-                <h3>Hi there! I&apos;m</h3>
-                <h1>MARÍA FERNÁNDEZ HUERTA</h1>
-                <h3>Front-end developer with an ICT & cibersecurity legal background</h3>
-            </IntroText>
+  return (
+    <TopSectionContainer>
+      {/* Texto de introducción */}
+      <IntroText>
+        <h3>Hi there! I&apos;m</h3>
+        <h1>MARÍA FERNÁNDEZ HUERTA</h1>
+        <h3>Front-end developer with an ICT & cibersecurity legal background</h3>
+      </IntroText>
 
-            {/* Carousel de imágenes */}
-            <CarouselContainer>
-                <StyledCarousel
-                    showArrows={true}
-                    autoPlay={true}
-                    infiniteLoop={true}
-                    showThumbs={false}
-                    showStatus={false}
-                    centerMode={true}
-                    centerSlidePercentage={33.33} // Muestra 3 imágenes a la vez
-                    emulateTouch={true}
-                    interval={3000} // Intervalo de 3 segundos entre imágenes
-                >
-                    {carouselImages.map((image, index) => (
-                        <div key={index}>
-                            <img src={image} alt={`Imagen Carousel ${index + 1} `} />
-                        </div>
-                    ))}
-                </StyledCarousel>
-            </CarouselContainer>
+      {/* Carousel de imágenes */}
+      <CarouselContainer>
+        <StyledCarousel
+          showArrows={true}
+          autoPlay={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
+          centerMode={true}
+          centerSlidePercentage={33.33} // Muestra 3 imágenes a la vez
+          emulateTouch={true}
+          interval={3000} // Intervalo de 3 segundos entre imágenes
+        >
+          {carouselImages.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt={`Imagen Carousel ${index + 1} `} />
+            </div>
+          ))}
+        </StyledCarousel>
+      </CarouselContainer>
 
-            {/* Texto adicional */}
-            <AdditionalText>
-                <h2>BIO</h2>
-                <p>Although my predominant background is on legal, as a labour, commercial and ICT lawyer also have a background in Cybersecurity and data protection. A few years ago, I founded out my passion for tech and that I would mix my two experiences. After a Master program in ICT law _emergent technologies law_ started my studies on Software enginering, fulfilled with the bootcamp con front end development.</p>
-            </AdditionalText>
-        </TopSectionContainer >
-    );
+      {/* Texto adicional */}
+      <AdditionalText>
+        <h2>BIO</h2>
+        <p>Although my predominant background is on legal, as a labour, commercial and ICT lawyer also have a background in Cybersecurity and data protection. A few years ago, I founded out my passion for tech and that I would mix my two experiences. After a Master program in ICT law _emergent technologies law_ started my studies on Software enginering, fulfilled with the bootcamp con front end development.</p>
+      </AdditionalText>
+    </TopSectionContainer >
+  );
 };
 
 export default TopSection;
