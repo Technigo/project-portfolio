caf3d4442dde86e26f917916bafc7cf4c0024fe9
Anna <annah80@gmail.com>
2024-12-18 11:12:32 +0100

    changed images

--- src/sections/ProjectsGallery.jsx
+++ src/sections/ProjectsGallery.jsx
@@ -7,7 +7,7 @@ export const ProjectsGallery = () => {
       <div className="gallery">
         <img src="/Thoughts.png" alt="gallery-image-thoughts" className="image left" />
         <img src="/Weather.png" alt="gallery-image-weather" className="image middle" />
-        <img src="/Business.png" alt="gallery-image-business" className="image right" />
+        <img src="/Music.png" alt="gallery-image-music" className="image right" />
       </div>
     </section>
   );
