a14c29257f87beb57b3505c239a24abca7e2f2f8
Nella <nellax@me.com>
2024-12-30 17:16:07 +0100

    css updates, padding etc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export const HeaderIntro = () => (
   <Grid background="black">
     <div className="intro-text">
       <h3>Hi there, I&apos;m</h3>
-      <h2>Nella Edvardsson</h2>
+      <h1>Nella</h1>
       <h3>
         Creative Frontend Developer with a Background in HR & Teaching
       </h3></div>
