72dbc0f73aadbff94bae69c1439f888226c436f2
Sherry Isola-Gbenla <sherry.isolagbenla@hotmail.com>
2025-02-09 11:12:12 +0100

    added project component and started with mywords component

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -5,6 +5,10 @@ import { Introduction } from "./Components/Introduction";
 import { Tech } from "./Components/Tech";
 import { Skills } from "./Components/Skills";
 import { Marquee } from "./Components/Marquee";
+import { LetsTalk } from "./Components/LetsTalk";
+import theWeatherApp from "./assets/the-weather-app.jpeg";
+import { FeaturedProjects } from "./Components/FeaturedProjects";
+import { MyWords } from "./Components/MyWords";
 
 
 
@@ -15,8 +19,27 @@ function App() {
     <div>
       <Introduction />
       <Tech />
+      <FeaturedProjects
+        // this is an array. the square brackets are the array. the curly braces are the object.
+        projects={[
+          {
+            title: "The Weather App Project",
+            image: theWeatherApp,
+            alt: "picture of the weather app project",
+            description: "This is a weather app that I created using external API.",
+            demoUrl: "",
+            codeUrl: "",
+            tech: ["HTML", "CSS", "JavaScript"],
+          },
+        ]}
+      />
+
+      <MyWords />
+
       <Skills />
+      <LetsTalk />
       <Marquee />
+
 
 
     </div>
